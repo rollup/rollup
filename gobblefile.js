47130007048e75d1bc512862290df4e368962f31
Rich Harris <richard.a.harris@gmail.com>
2015-05-22 11:44:20 -0400

    SELF HOSTING!!!!

--- gobblefile.js
+++ gobblefile.js
@@ -1,9 +1,19 @@
 var gobble = require( 'gobble' );
 
-module.exports = gobble( 'src' )
-	.transform( 'babel' )
-	.transform( 'esperanto-bundle', {
-		entry: 'rollup',
-		type: 'cjs',
-		strict: true
-	});
+var selfhost = 1;
+
+module.exports = selfhost ?
+	gobble( 'src' )
+		.transform( 'babel' )
+		.transform( 'rollup', {
+			entry: 'rollup',
+			format: 'cjs'
+		}) :
+
+	gobble( 'src' )
+		.transform( 'babel' )
+		.transform( 'esperanto-bundle', {
+			entry: 'rollup',
+			type: 'cjs',
+			strict: true
+		});
