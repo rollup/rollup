6a905dbd99dae2c84da7e93d0cdac7643226ff54
Rich Harris <richard.a.harris@gmail.com>
2015-05-22 11:40:32 -0400

    test for computed property rewrites

--- test/function/shadowed-external-export/main.js
+++ test/function/shadowed-external-export/main.js
@@ -1,7 +1,10 @@
 import { relative } from 'path';
 
+var paths = {};
 function getRelativePath ( path, path2 ) {
+	paths[ path ] = true;
 	return relative( path, path2 );
 }
 
 assert.equal( getRelativePath( 'foo/bar/baz', 'foo/baz/bar' ), '../../baz/bar' );
+assert.deepEqual( paths, { 'foo/bar/baz': true });
