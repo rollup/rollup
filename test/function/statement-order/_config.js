ae48cc1691433e588636543218c492cb6fc99265
Rich Harris <richard.a.harris@gmail.com>
2015-06-16 17:48:21 -0400

    failing test for #26

---
+++ test/function/statement-order/_config.js
@@ -0,0 +1,15 @@
+var assert = require( 'assert' );
+
+module.exports = {
+	description: 'correct statement order is preserved even in weird edge cases',
+	context: {
+		getAnswer: function ( obj ) {
+			return obj.answer;
+		}
+	},
+	exports: function ( exports ) {
+		assert.equal( exports, 'right' );
+	},
+	solo: true,
+	show: true
+};
---
+++ test/function/statement-order/main.js
@@ -0,0 +1,3 @@
+import answer from './answer';
+var answer2 = answer;
+export default answer2;
