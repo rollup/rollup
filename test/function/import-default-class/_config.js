21a12658e867120986057a7428e33ce771d6fa5d
Rich-Harris <richard.a.harris@gmail.com>
2015-06-17 22:59:59 -0400

    try and get tests to pass in node 0.10 and 0.12

--- test/function/import-default-class/_config.js
+++ test/function/import-default-class/_config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	description: 'imports a default class',
-	babel: [ 'es6.classes' ]
+	babel: [ 'es6.classes', 'es6.blockScoping' ]
 };
 
 // test copied from https://github.com/esnext/es6-module-transpiler/tree/master/test/examples/export-default-class
