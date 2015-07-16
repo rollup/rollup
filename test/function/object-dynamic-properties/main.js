15d541f6753300b8fe032c07865e9c7427098c5a
Oskar Segersvärd <victorystick@gmail.com>
2015-07-16 18:20:15 +0200

    Removed  from tests.

--- test/function/object-dynamic-properties/main.js
+++ test/function/object-dynamic-properties/main.js
@@ -1,3 +1,4 @@
 import tags from './tags';
 
 assert(tags['[object Object]'], true);
+assert(Object.keys(tags).length, 6);
--- test/function/object-dynamic-properties/tags.js
+++ test/function/object-dynamic-properties/tags.js
@@ -1,13 +1,13 @@
 // Emulates a piece of Lodash source code.
 // The tag strings are not imported, and a runtime error ensues.
 
-let objTag = '[object Object]';
-let arrTag = '[object Array]';
-let fnTag = '[object Function]';
-let dateTag = '[object Date]';
+var objTag = '[object Object]';
+var arrTag = '[object Array]';
+var fnTag = '[object Function]';
+var dateTag = '[object Date]';
 
-let strTag = '[object String]';
-let numTag = '[object Number]';
+var strTag = '[object String]';
+var numTag = '[object Number]';
 
 // Only the code below is included in the bundle.
 
