36be9196f5cc694a9aa25ab7ca2bd44561ec8ee1
Rich-Harris <richard.a.harris@gmail.com>
2015-08-16 14:24:57 -0400

    test for #77

---
+++ test/form/dedupes-external-imports/_expected/es6.js
@@ -0,0 +1,26 @@
+import { Component } from 'external';
+
+class Foo extends Component {
+	constructor () {
+		super();
+		this.isFoo = true;
+	}
+}
+
+class Bar extends Component {
+	constructor () {
+		super();
+		this.isBar = true;
+	}
+}
+
+class Baz extends Component {
+	constructor () {
+		super();
+		this.isBaz = true;
+	}
+}
+
+const foo = new Foo();
+const bar = new Bar();
+const baz = new Baz();
---
+++ test/form/dedupes-external-imports/baz.js
@@ -0,0 +1,8 @@
+import { Component as Comp } from 'external';
+
+export default class Baz extends Comp {
+	constructor () {
+		super();
+		this.isBaz = true;
+	}
+}
