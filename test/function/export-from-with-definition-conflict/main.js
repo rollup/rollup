b09bce28b57cdd4a7d26a8fe95c84e0415d51e60
Rich-Harris <richard.a.harris@gmail.com>
2015-08-17 20:00:46 -0400

    fix typo in test

--- test/function/export-from-with-definition-conflict/main.js
+++ test/function/export-from-with-definition-conflict/main.js
@@ -5,5 +5,5 @@ export { foo as bar } from './a';
 export { bar as foo } from './a';
 export { baz } from './a';
 
-assert.equal( foo, 'b-foo' );
-assert.equal( baz, 'b-baz' );
+assert.equal( foo, 'local-foo' );
+assert.equal( baz, 'local-baz' );
