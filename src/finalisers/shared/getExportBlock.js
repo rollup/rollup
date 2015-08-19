199d3ad4e429927cbc84f06e566f51d91c389b23
Rich Harris <richard.a.harris@gmail.com>
2015-08-19 09:13:05 -0400

    simplify export block logic

--- src/finalisers/shared/getExportBlock.js
+++ src/finalisers/shared/getExportBlock.js
@@ -11,10 +11,7 @@ export default function getExportBlock ( bundle, exportMode, mechanism = 'return
 	return bundle.toExport
 		.map( name => {
 			const prop = name === 'default' ? `['default']` : `.${name}`;
-			const reexport = bundle.entryModule.reexports[ name ];
-			name = reexport ?
-				bundle.traceExport( bundle.entryModule, name ) :
-				bundle.trace( bundle.entryModule, name );
+			name = bundle.traceExport( bundle.entryModule, name );
 			return `exports${prop} = ${name};`;
 		})
 		.join( '\n' );
