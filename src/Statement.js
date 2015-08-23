840d1dc21d8442a27681883b3a317185cce89042
Rich-Harris <richard.a.harris@gmail.com>
2015-08-22 20:46:49 -0400

    handle re-exports in entry module

--- src/Statement.js
+++ src/Statement.js
@@ -263,21 +263,21 @@ export default class Statement {
 		this.isIncluded = true;
 
 		// `export { name } from './other'` is a special case
-		// if ( this.isReexportDeclaration ) {
-		// 	return this.module.bundle.fetchModule( this.node.source.value, this.module.id )
-		// 		.then( otherModule => {
-		// 			return sequence( this.node.specifiers, specifier => {
-		// 				const reexport = this.module.reexports[ specifier.exported.name ];
-		//
-		// 				reexport.isUsed = true;
-		// 				reexport.module = otherModule;
-		//
-		// 				return otherModule.isExternal ?
-		// 					null :
-		// 					otherModule.markExport( specifier.local.name, specifier.exported.name, this.module );
-		// 			});
-		// 		});
-		// }
+		if ( this.isReexportDeclaration ) {
+			const id = this.module.resolvedIds[ this.node.source.value ];
+			const otherModule = this.module.bundle.moduleById[ id ];
+
+			this.node.specifiers.forEach( specifier => {
+				const reexport = this.module.reexports[ specifier.exported.name ];
+
+				reexport.isUsed = true;
+				reexport.module = otherModule; // TODO still necessary?
+
+				if ( !otherModule.isExternal ) otherModule.markExport( specifier.local.name, specifier.exported.name, this.module );
+			});
+
+			return;
+		}
 
 		Object.keys( this.dependsOn ).forEach( name => {
 			if ( this.defines[ name ] ) return; // TODO maybe exclude from `this.dependsOn` in the first place?
