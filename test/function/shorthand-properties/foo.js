ab8fc095aaba38eb7ff3378d7fd09e1747663527
Oskar Segersvärd <victorystick@gmail.com>
2015-09-10 11:31:56 +0200

    Changed shorthand properties test

--- test/function/shorthand-properties/foo.js
+++ test/function/shorthand-properties/foo.js
@@ -1,7 +1,10 @@
+import baz from './baz.js';
+
 function bar () {
 	return 'foo-bar';
 }
 
 export var foo = {
-	bar
+	bar,
+	baz
 };
--- test/function/shorthand-properties/main.js
+++ test/function/shorthand-properties/main.js
@@ -1,8 +1,5 @@
+import bar from './baz.js';
 import { foo } from './foo';
-
-function bar () {
-	return 'main-bar';
-}
 
 assert.equal( bar(), 'main-bar' );
 assert.equal( foo.bar(), 'foo-bar' );
