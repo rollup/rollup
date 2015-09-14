886e6f7edc647db4fe57e27a5b76af2aebd5ca8e
Oskar Segersvärd <victorystick@gmail.com>
2015-09-14 17:32:52 +0200

    Added tests

---
+++ test/form/export-all-from-internal/_expected/umd.js
@@ -0,0 +1,13 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	factory((global.exposedInternals = {}));
+}(this, function (exports) { 'use strict';
+
+	const a = 1;
+	const b = 2;
+
+	exports.a = a;
+	exports.b = b;
+
+}));
