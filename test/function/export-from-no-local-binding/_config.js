94de9accfba4db8c37869f0514a800b04c1a6694
Oskar Segersvärd <victorystick@gmail.com>
2015-09-29 10:36:39 +0200

    Enable test

--- test/function/export-from-no-local-binding/_config.js
+++ test/function/export-from-no-local-binding/_config.js
@@ -1,13 +1,5 @@
-var assert = require( 'assert' );
-
 module.exports = {
-	description: 'export from does not create a local binding',
-
-	error: function ( err ) {
-		assert.ok( false, 'TODO: assertion is skipped because it is not used... we need to implement something like /*rollup: include */')
-	},
-
-	skip: true
+	description: 'export from does not create a local binding'
 };
 
 // test copied from https://github.com/esnext/es6-module-transpiler/tree/master/test/examples/export-from
