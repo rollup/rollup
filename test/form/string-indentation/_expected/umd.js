e5120f392886c2f35d63e0c54af2622f0cdf3730
Rich-Harris <richard.a.harris@gmail.com>
2015-10-05 07:26:12 -0400

    failing test for #164

---
+++ test/form/string-indentation/_expected/umd.js
@@ -0,0 +1,24 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory();
+}(this, function () { 'use strict';
+
+	var a = '1\
+  2';
+
+	var b = '1\
+	2';
+
+	var c = `1
+  2`;
+
+	var d = `1
+	2`;
+
+	assert.equal( a, '1\n  2' );
+	assert.equal( b, '1\n\t2' );
+	assert.equal( c, '1\n  2' );
+	assert.equal( d, '1\n\t2' );
+
+}));
