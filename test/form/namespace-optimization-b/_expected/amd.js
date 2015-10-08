e006d07a0bb6d4f0d0c430b1afcbde9a93b82707
Rich-Harris <richard.a.harris@gmail.com>
2015-10-07 21:03:48 -0400

    consistent naming scbeme

---
+++ test/form/namespace-optimization-b/_expected/amd.js
@@ -0,0 +1,15 @@
+define(function () { 'use strict';
+
+  function foo() {
+  }
+
+  function a() {
+    foo();
+    foo();
+    var a;
+    if (a.b) {
+    }
+  }
+  a();
+
+});
---
+++ test/form/namespace-optimization-b/_expected/umd.js
@@ -0,0 +1,19 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  factory();
+}(this, function () { 'use strict';
+
+  function foo() {
+  }
+
+  function a() {
+    foo();
+    foo();
+    var a;
+    if (a.b) {
+    }
+  }
+  a();
+
+}));
