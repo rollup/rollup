92676f5f74d4953d085ea4742e10978302ecb00b
Rich-Harris <richard.a.harris@gmail.com>
2015-10-07 20:59:57 -0400

    update a test (#161)

--- test/function/consistent-renaming-f/main.js
+++ test/function/consistent-renaming-f/main.js
@@ -3,7 +3,9 @@ import bar from './bar';
 export default function foo () {}
 
 foo.prototype.a = function ( foo ) {
+	var foo = foo;
+	foo();
 	return bar();
 };
 
-assert.equal( new foo().a(), 'consistent' );
+assert.equal( new foo().a(function () {}), 'consistent' );
