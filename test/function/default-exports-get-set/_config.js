aa02b8d7c22a69e3b86ed880f0bd9cce9e82966f
Rich-Harris <richard.a.harris@gmail.com>
2015-10-31 12:40:17 -0400

    failing test for #251

---
+++ test/function/default-exports-get-set/_config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	solo: true,
+	show: true,
+	description: 'handles get/set keywords when converting `export default` to object'
+};
---
+++ test/function/default-exports-get-set/foo.js
@@ -0,0 +1,15 @@
+let foo = 'FOO';
+
+function get () {
+	return foo;
+}
+
+function set ( x ) {
+	foo = x;
+}
+
+function jet () {}
+function net () {}
+function wet () {}
+
+export default { get, set, jet, net, wet };
