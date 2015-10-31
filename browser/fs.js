9e14a05aa1a6b35a7afac15aa89725a40d9ae65a
Rich-Harris <richard.a.harris@gmail.com>
2015-10-31 12:25:04 -0400

    fix browser build

--- browser/fs.js
+++ browser/fs.js
@@ -1,5 +1,6 @@
 const nope = method => `Cannot use fs.${method} inside browser`;
 
+export const isFile = () => false;
 export const readdirSync = nope( 'readdirSync' );
 export const readFileSync = nope( 'readFileSync' );
 export const writeFile = nope( 'writeFile' );
