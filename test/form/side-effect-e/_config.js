37f79e2836455fd986fa36f6b6a6c1e9f821ea63
Rich-Harris <richard.a.harris@gmail.com>
2015-10-31 21:35:12 -0400

    add test

---
+++ test/form/side-effect-e/_config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	solo: true,
+	description: 'disregards side-effects that are contained within a function',
+	options: {
+		moduleName: 'myBundle'
+	}
+};
---
+++ test/form/side-effect-e/main.js
@@ -0,0 +1,8 @@
+function foo () {
+	var a, b, c;
+	b = 1;
+}
+
+foo();
+
+export default 42;
