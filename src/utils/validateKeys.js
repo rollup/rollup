3c5d099cc7000fdf85995d382809a6fb774059a4
Rich-Harris <richard.a.harris@gmail.com>
2015-12-22 10:37:18 -0500

    avoid for...of

--- src/utils/validateKeys.js
+++ src/utils/validateKeys.js
@@ -3,8 +3,12 @@ import { keys } from './object.js';
 export default function validateKeys ( object, allowedKeys ) {
 	const actualKeys = keys( object );
 
-	for ( let key of actualKeys ) {
-		if ( allowedKeys.indexOf( key ) < 0 ) {
+	let i = actualKeys.length;
+
+	while ( i-- ) {
+		const key = actualKeys[i];
+
+		if ( allowedKeys.indexOf( key ) === -1 ) {
 			return new Error(
 				`Unexpected key '${ key }' found, expected one of: ${ allowedKeys.join( ', ' ) }`
 			);
