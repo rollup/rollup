28eecffeefa6071e71d7c77dc27385f6bf915875
Eric Ferraiuolo <eferraiuolo@gmail.com>
2015-12-30 15:13:52 -0500

    Fix UMD global export
    This wraps the comma expression generated when using a namespaced
    global module name; e.g.: `Foo.bar`. Previously the generated code
    would fail in a CommonJS or AMD module environment.
    
    See: #378

--- test/form/exports-at-end-if-possible/_expected/umd.js
+++ test/form/exports-at-end-if-possible/_expected/umd.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	factory((global.myBundle = {}));
+	(factory((global.myBundle = {})));
 }(this, function (exports) { 'use strict';
 
 	var FOO = 'foo';
--- test/form/external-imports-custom-names/_expected/umd.js
+++ test/form/external-imports-custom-names/_expected/umd.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('jquery')) :
 	typeof define === 'function' && define.amd ? define(['jquery'], factory) :
-	factory(global.jQuery);
+	(factory(global.jQuery));
 }(this, function ($) { 'use strict';
 
 	$ = 'default' in $ ? $['default'] : $;
