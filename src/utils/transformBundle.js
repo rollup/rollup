f2e950914e93bb60f6ebc0679b1cce0a7588e47c
Bogdan Chadkin <trysound@yandex.ru>
2015-12-30 05:25:30 +0300

    Remove promise version comment

--- src/utils/transformBundle.js
+++ src/utils/transformBundle.js
@@ -1,5 +1,3 @@
-// import Promise from 'es6-promise/lib/es6-promise/promise.js';
-
 export default function transformBundle ( source, transformers ) {
 	if ( typeof source === 'string' ) {
 		source = {
@@ -27,27 +25,4 @@ export default function transformBundle ( source, transformers ) {
 		return result;
 
 	}, source );
-
-	// Promisified version
-	// return transformers.reduce( ( promise, transformer ) => {
-	// 	return promise.then( previous => {
-	// 		return Promise.resolve( transformer( previous ) ).then( result => {
-	// 			if ( result == null ) return previous;
-
-	// 			if ( typeof result === 'string' ) {
-	// 				result = {
-	// 					code: result,
-	// 					map: null
-	// 				};
-	// 			}
-	// 			// `result.map` can only be a string if `result` isn't
-	// 			else if ( typeof result.map === 'string' ) {
-	// 				result.map = JSON.parse( result.map );
-	// 			}
-
-	// 			return result;
-	// 		});
-	// 	});
-
-	// }, Promise.resolve( source ) );
 }
