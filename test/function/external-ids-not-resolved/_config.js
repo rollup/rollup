19c56b87ac734a6cf79d9f2b2553a3f7bbe62f17
Rich-Harris <richard.a.harris@gmail.com>
2016-01-02 15:04:14 -0500

    prevent resolution of external IDs (#410)

---
+++ test/function/external-ids-not-resolved/_config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	description: 'does not attempt to resolve external IDs',
+	options: {
+		external: [ 'external' ],
+		plugins: [
+			{
+				resolveId: function ( importee ) {
+					if ( importee === 'external' ) {
+						throw new Error( 'Attempted to resolve external module ID' );
+					}
+				}
+			}
+		]
+	}
+};
