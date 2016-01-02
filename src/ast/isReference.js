a7de73fbe94d1c33ca71bf2bd8becdaea27fe73b
Rich-Harris <richard.a.harris@gmail.com>
2016-01-02 10:26:22 -0500

    add comment re arrow function expression special case

--- src/ast/isReference.js
+++ src/ast/isReference.js
@@ -4,6 +4,9 @@ export default function isReference ( node, parent ) {
 	}
 
 	if ( node.type === 'Identifier' ) {
+		// the only time we could have an identifier node without a parent is
+		// if it's the entire body of a function without a block statement –
+		// i.e. an arrow function expression like `a => a`
 		if ( !parent ) return true;
 
 		// TODO is this right?
