d41060182175a5bd899875861c6b962c04024eed
Rich-Harris <richard.a.harris@gmail.com>
2016-01-17 17:28:57 -0500

    fix browser version

--- browser/fs.js
+++ browser/fs.js
@@ -1,6 +1,7 @@
 const nope = method => `Cannot use fs.${method} inside browser`;
 
-export const isFile = () => false;
+export const lstatSync = nope( 'lstatSync' );
 export const readdirSync = nope( 'readdirSync' );
 export const readFileSync = nope( 'readFileSync' );
+export const realpathSync = nope( 'realpathSync' );
 export const writeFile = nope( 'writeFile' );
