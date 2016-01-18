29f7732f5f8ee64c75eb6400c5911945eb9a5228
Rich-Harris <richard.a.harris@gmail.com>
2016-01-18 13:18:11 -0500

    ugh windows

--- test/function/cannot-call-internal-namespace/_config.js
+++ test/function/cannot-call-internal-namespace/_config.js
@@ -5,7 +5,7 @@ module.exports = {
 	description: 'errors if code calls an internal namespace',
 	error: function ( err ) {
 		assert.equal( err.message, 'Cannot call a namespace (\'foo\')' );
-		assert.equal( err.file, path.resolve( __dirname, 'main.js' ) );
+		assert.equal( err.file, path.resolve( __dirname, 'main.js' ).replace( /\//g, path.sep ) );
 		assert.equal( err.pos, 33 );
 		assert.deepEqual( err.loc, { line: 2, column: 0 });
 	}
