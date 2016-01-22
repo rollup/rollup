1a0011485e912295df28bea534fd69abc823b701
Brian Donovan <me@brian-donovan.com>
2016-01-22 15:27:15 -0500

    Skip dead branches of a conditional expression.
    This allows replacement of inline conditions such as in `export const resolve = isWindows ? win32Resolve : posixResolve;`.

--- test/function/skips-dead-branches-f/_config.js
+++ test/function/skips-dead-branches-f/_config.js
@@ -1,7 +1,7 @@
 var assert = require( 'assert' );
 
 module.exports = {
-	description: 'skips a dead branch (g)',
+	description: 'skips a dead branch (f)',
 	code: function ( code ) {
 		assert.equal( code.indexOf( 'obj.foo = function' ), -1, code );
 	}
