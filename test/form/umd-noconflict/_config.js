66f838ad9d92b7ca3af294e48c34d5d4a5ce1ec2
Chris Reeves <chris@ev-soft.net>
2016-03-29 17:27:44 -0400

    add an option to add noConflict method to umd global

---
+++ test/form/umd-noconflict/_config.js
@@ -0,0 +1,10 @@
+var assert = require( 'assert' );
+
+module.exports = {
+	description: 'exports noConflict method for default umd when requested',
+	options: {
+		noConflict: true,
+		moduleName: 'FooBar'
+	}
+};
+
---
+++ test/form/umd-noconflict/_expected/umd.js
@@ -0,0 +1,24 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(function() {
+		var current = global.FooBar;
+		var exports = factory((global.FooBar = global.FooBar || {}));
+		global.FooBar = exports;
+		exports.noConflict = function() { global.FooBar = current; return exports; };
+	})();
+}(this, function (exports) { 'use strict';
+
+	function doThings() {
+		console.log( 'doing things...' );
+	}
+
+	const number = 42;
+
+	var setting = 'no';
+
+	exports.doThings = doThings;
+	exports.number = number;
+	exports.setting = setting;
+
+}));
