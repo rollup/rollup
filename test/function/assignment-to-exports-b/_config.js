a6d3436392314079b863982caafb2417dd3c2871
Rich Harris <richard.a.harris@gmail.com>
2016-04-28 13:50:20 -0400

    fix test title

--- test/function/assignment-to-exports-b/_config.js
+++ test/function/assignment-to-exports-b/_config.js
@@ -1,7 +1,7 @@
 const assert = require( 'assert' );
 
 module.exports = {
-	description: 'exports are kept up-to-date',
+	description: 'exports are rewritten inside a variable init',
 	exports: exports => {
 		assert.equal( exports.b, 42 );
 	}
