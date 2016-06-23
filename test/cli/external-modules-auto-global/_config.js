ec5b11f76b019ebf7418c78ffdb7f0d41ea13d61
Rich-Harris <richard.a.harris@gmail.com>
2016-06-22 20:42:12 -0400

    failing test for #743

---
+++ test/cli/external-modules-auto-global/_config.js
@@ -0,0 +1,7 @@
+const assert = require( 'assert' );
+
+module.exports = {
+	description: 'populates options.external with --global keys',
+	command: 'rollup main.js --format iife --globals mathematics:Math',
+	execute: true
+};
