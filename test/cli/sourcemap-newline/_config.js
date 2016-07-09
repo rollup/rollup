035f66e8d92dae33ee950cbaad0929a5f2c6dead
Rich-Harris <richard.a.harris@gmail.com>
2016-07-09 15:51:26 -0400

    reinstate all tests

--- test/cli/sourcemap-newline/_config.js
+++ test/cli/sourcemap-newline/_config.js
@@ -1,7 +1,6 @@
 const assert = require( 'assert' );
 
 module.exports = {
-	solo: true,
 	description: 'adds a newline after the sourceMappingURL comment (#756)',
 	command: 'rollup -i main.js -m inline',
 	result: code => {
