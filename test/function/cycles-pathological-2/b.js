088d12bf9f206fe00d64a3f5cd332a07fadaf9d2
ph.ter <philipp.weinfurter@gmail.com>
2016-07-28 11:28:54 +0200

    Avoid infinite recursion in Bundle.sort()

---
+++ test/function/cycles-pathological-2/b.js
@@ -0,0 +1,11 @@
+import { a, av } from './main';
+import { d, dv } from './d';
+import { c, cv } from './c';
+
+export function b() {
+    a(av);
+    d(dv);
+    c(cv);
+};
+
+export const bv = av + dv + cv;
---
+++ test/function/cycles-pathological-2/c.js
@@ -0,0 +1,9 @@
+import { b, bv } from './b';
+import { a, av } from './main';
+
+export function c() {
+    a(av);
+    b(bv);
+};
+
+export const cv = av + bv;
