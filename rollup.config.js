0913247901b566af2e8e73ef60e5d0cb26b107d6
Rich-Harris <richard.a.harris@gmail.com>
2016-09-06 20:19:35 -0400

    transpile acorn for now, pending new release

--- rollup.config.js
+++ rollup.config.js
@@ -22,7 +22,11 @@ export default {
 	entry: 'src/rollup.js',
 	plugins: [
 		buble({
-			include: [ 'src/**', 'node_modules/acorn/**' ],
+			include: [ 'node_modules/acorn/**' ]
+		}),
+
+		buble({
+			include: [ 'src/**' ],
 			target: {
 				node: 4
 			}
