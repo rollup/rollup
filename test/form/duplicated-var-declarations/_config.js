7df2f00b57869f6cd247f191b59f8a614b08e65f
Rich Harris <richard.a.harris@gmail.com>
2016-09-07 17:59:58 -0400

    add test for #716 but skip it for now

---
+++ test/form/duplicated-var-declarations/_config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	skip: true,
+	description: 'does not remove duplicated var declarations (#716)'
+};
---
+++ test/form/duplicated-var-declarations/main.js
@@ -0,0 +1,10 @@
+var a = 1, b = 2;
+
+assert.equal( a, 1 );
+assert.equal( b, 2 );
+
+var a = 3, b = 4, c = 5;
+
+assert.equal( a, 3 );
+assert.equal( b, 4 );
+assert.equal( c, 5 );
