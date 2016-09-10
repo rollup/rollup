7819d7b68822e7a767e227480b08598492b14bf9
Rich-Harris <richard.a.harris@gmail.com>
2016-09-10 12:26:31 -0400

    update to acorn 4

--- test/function/double-named-export/_config.js
+++ test/function/double-named-export/_config.js
@@ -1,8 +1,9 @@
+const path = require( 'path' );
 const assert = require( 'assert' );
 
 module.exports = {
 	description: 'throws on duplicate named exports',
 	error: err => {
-		assert.equal( err.message, `A module cannot have multiple exports with the same name ('foo')` );
+		assert.equal( err.message, `Duplicate export 'foo' (3:9) in ${path.resolve(__dirname, 'foo.js')}` );
 	}
 };
