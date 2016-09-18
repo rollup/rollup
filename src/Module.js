e36a57527b490a748a6e13e7952c8a8d382fa774
Rich-Harris <richard.a.harris@gmail.com>
2016-09-18 17:45:09 -0400

    prevent {export foo as default} from creating a live binding (#860)

--- src/Module.js
+++ src/Module.js
@@ -170,7 +170,13 @@ export default class Module {
 						throw new Error( `A module cannot have multiple exports with the same name ('${exportedName}')` );
 					}
 
-					this.exports[ exportedName ] = { localName };
+					// `export { default as foo }` – special case. We want importers
+					// to use the UnboundDefaultExport proxy, not the original declaration
+					if ( exportedName === 'default' ) {
+						this.exports[ exportedName ] = { localName: 'default' };
+					} else {
+						this.exports[ exportedName ] = { localName };
+					}
 				});
 			} else {
 				this.bundle.onwarn( `Module ${this.id} has an empty export declaration` );
--- test/function/export-as-default/_config.js
+++ test/function/export-as-default/_config.js
@@ -1,6 +1,5 @@
 var assert = require( 'assert' );
 
 module.exports = {
-	solo: true,
 	description: 'export { foo as default } does not create a live binding'
 };
