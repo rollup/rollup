ed35cef98f86591086ce1a4610d4c7734023f592
Daniel K <FredyC@users.noreply.github.com>
2016-10-01 22:15:24 +0200

    Add module id when warning about top level this
    Hard to fix the problem when not knowing which module actually has it.

--- src/ast/nodes/ThisExpression.js
+++ src/ast/nodes/ThisExpression.js
@@ -7,7 +7,7 @@ export default class ThisExpression extends Node {
 		if ( lexicalBoundary.isModuleScope ) {
 			this.alias = this.module.bundle.context;
 			if ( this.alias === 'undefined' ) {
-				this.module.bundle.onwarn( 'The `this` keyword is equivalent to `undefined` at the top level of an ES module, and has been rewritten' );
+				this.module.bundle.onwarn( `The 'this' keyword is equivalent to 'undefined' at the top level of an ES module, and has been rewritten (in ${this.module.id})` );
 			}
 		}
 	}
