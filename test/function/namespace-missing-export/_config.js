54124770cd94a0a9e6095031e8e835f080c7b64b
Rich-Harris <richard.a.harris@gmail.com>
2016-10-12 09:26:01 -0400

    fix missing namespace member warnings (closes #1045)

--- test/function/namespace-missing-export/_config.js
+++ test/function/namespace-missing-export/_config.js
@@ -3,7 +3,7 @@ var assert = require( 'assert' );
 module.exports = {
 	options: {
 		onwarn: function ( msg ) {
-			assert.ok( /Export 'foo' is not defined by/.test( msg ) );
+			assert.equal( msg, `main.js (3:21) 'foo' is not exported by 'empty.js'. See https://github.com/rollup/rollup/wiki/Troubleshooting#name-is-not-exported-by-module` );
 		}
 	}
 };
