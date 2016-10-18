0006ea1e1c7e9b0e41d178ab3959cd0a27259c54
kzc <zaxxon2011@gmail.com>
2016-10-18 09:02:44 -0400

    use buble for test to resolve node 0.12 failure

--- test/function/braceless-arrow-function-returning-function/_config.js
+++ test/function/braceless-arrow-function-returning-function/_config.js
@@ -1,3 +1,4 @@
 module.exports = {
-	description: 'arrow function without braces returning a function (#1032)'
+	description: 'arrow function without braces returning a function (#1032)',
+	buble: true
 };
