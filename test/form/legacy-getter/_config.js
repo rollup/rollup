ea704835ba6cec265f4f6d20a794463668bdf7ab
Iván Sánchez Ortega <ivan@mazemap.no>
2016-10-20 13:22:47 +0200

    Add testing of getter defs in legacy mode.

---
+++ test/form/legacy-getter/_config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	description: 'Does not output getters when in legacy',
+	options: {
+		legacy: true,
+		moduleName: 'foo'
+	}
+};
---
+++ test/form/legacy-getter/_expected/umd.js
@@ -0,0 +1,29 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.foo = global.foo || {})));
+}(this, (function (exports) { 'use strict';
+
+	var browserSpecificThing;
+
+	if ('ActiveXObject' in window) {
+		browserSpecificThing = "InternetExplorerThing";
+	} else {
+		browserSpecificThing = "DecentBrowserThing";
+	}
+
+	function foo() {}
+
+
+	var browserStuff = (Object.freeze || Object)({
+		browserSpecificThing: browserSpecificThing,
+		foo: foo
+	});
+
+	console.log(browserSpecificThing);
+
+	exports.B = browserStuff;
+
+	Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
