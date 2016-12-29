708fb9821b877ddf36ac58ec928b2df285e7ccfd
Rich-Harris <richard.a.harris@gmail.com>
2016-12-29 17:43:51 -0500

    warn on missing format (fixes #1197)

--- test/cli/sourcemap-newline/_config.js
+++ test/cli/sourcemap-newline/_config.js
@@ -2,7 +2,7 @@ const assert = require( 'assert' );
 
 module.exports = {
 	description: 'adds a newline after the sourceMappingURL comment (#756)',
-	command: 'rollup -i main.js -m inline',
+	command: 'rollup -i main.js -f es -m inline',
 	result: code => {
 		assert.equal( code.slice( -1 ), '\n' );
 	}
