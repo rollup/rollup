f2d0851f120f602a9d008d7f33213bbca8320070
kzc <zaxxon2011@gmail.com>
2017-01-05 23:53:16 -0500

    `throw` always considered to be a side effect

--- test/form/side-effect-h/_expected/amd.js
+++ test/form/side-effect-h/_expected/amd.js
@@ -1,5 +1,13 @@
 define(function () { 'use strict';
 
+	function foo ( ok ) {
+		if ( !ok ) {
+			throw new Error( 'this will be ignored' );
+		}
+	}
+
+	foo();
+
 	var main = 42;
 
 	return main;
--- test/form/side-effect-h/_expected/iife.js
+++ test/form/side-effect-h/_expected/iife.js
@@ -1,6 +1,14 @@
 var myBundle = (function () {
 	'use strict';
 
+	function foo ( ok ) {
+		if ( !ok ) {
+			throw new Error( 'this will be ignored' );
+		}
+	}
+
+	foo();
+
 	var main = 42;
 
 	return main;
