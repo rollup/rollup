176e9a64ff6b8114eb4a1c07112fe83908340727
Oskar Segersvärd <victorystick@gmail.com>
2017-04-24 15:12:47 +0200

    Consolidate all non-null, `resolveId` return statements

--- src/utils/defaults.js
+++ src/utils/defaults.js
@@ -23,16 +23,16 @@ function addJsExtensionIfNecessary ( file ) {
 export function resolveId ( importee, importer ) {
 	if ( typeof process === 'undefined' ) throw new Error( `It looks like you're using Rollup in a non-Node.js environment. This means you must supply a plugin with custom resolveId and load functions. See https://github.com/rollup/rollup/wiki/Plugins for more information` );
 
-	// absolute paths are left untouched
-	if ( isAbsolute( importee ) ) return addJsExtensionIfNecessary( resolve( importee ) );
+	// external modules (non-entry modules that start with neither '.' or '/')
+	// are skipped at this stage.
+	if ( importer !== undefined && !/^[\/\.]/.test( importee[0] ) ) return null;
 
-	// if this is the entry point, resolve against cwd
-	if ( importer === undefined ) return addJsExtensionIfNecessary( resolve( process.cwd(), importee ) );
-
-	// external modules are skipped at this stage
-	if ( importee[0] !== '.' ) return null;
-
-	return addJsExtensionIfNecessary( resolve( dirname( importer ), importee ) );
+	// `resolve` processes paths from right to left, prepending them until an
+	// absolute path is created. Absolute importees therefore shortcircuit the
+	// resolve call and require no special handing on our part.
+	// See https://nodejs.org/api/path.html#path_path_resolve_paths
+	return addJsExtensionIfNecessary(
+		resolve( importer ? dirname( importer ) : resolve(), importee ) );
 }
 
 
