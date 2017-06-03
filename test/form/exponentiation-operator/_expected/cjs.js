9b49affd45e19e1f9431819dc5056e4f59710a87
Rich Harris <richard.a.harris@gmail.com>
2017-06-03 11:57:51 -0400

    support exponentiation operator (#1416)

---
+++ test/form/exponentiation-operator/_expected/cjs.js
@@ -0,0 +1,5 @@
+'use strict';
+
+{
+	console.log('8 is larger than 7');
+}
---
+++ test/form/exponentiation-operator/main.js
@@ -0,0 +1,5 @@
+if (2 ** 3 > 7) {
+	console.log('8 is larger than 7');
+} else {
+	console.log('8 is... not larger than 7?');
+}
