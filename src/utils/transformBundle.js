05fb8001a0d9fff9f282d7f5e168183e1261695d
caccialdo <caccialdo@gmail.com>
2017-07-07 09:49:32 +0100

    Support for async transformBundle

--- src/utils/transformBundle.js
+++ src/utils/transformBundle.js
@@ -2,37 +2,38 @@ import { decode } from 'sourcemap-codec';
 import error from './error.js';
 
 export default function transformBundle ( code, plugins, sourceMapChain, options ) {
-	return plugins.reduce( ( code, plugin ) => {
-		if ( !plugin.transformBundle ) return code;
+	return plugins.reduce( ( promise, plugin ) => {
+		if ( !plugin.transformBundle ) return promise;
 
-		let result;
+		return promise.then( code => {
+			return Promise.resolve().then( () => {
+				return plugin.transformBundle( code, { format : options.format } );
+			}).then( result => {
+				if ( result == null ) return code;
 
-		try {
-			result = plugin.transformBundle( code, { format : options.format } );
-		} catch ( err ) {
-			error({
-				code: 'BAD_BUNDLE_TRANSFORMER',
-				message: `Error transforming bundle${plugin.name ? ` with '${plugin.name}' plugin` : ''}: ${err.message}`,
-				plugin: plugin.name
+				if ( typeof result === 'string' ) {
+					result = {
+						code: result,
+						map: null
+					};
+				}
+
+				const map = typeof result.map === 'string' ? JSON.parse( result.map ) : result.map;
+				if ( map && typeof map.mappings === 'string' ) {
+					map.mappings = decode( map.mappings );
+				}
+
+				sourceMapChain.push( map );
+
+				return result.code;
+			}).catch( err => {
+				error({
+					code: 'BAD_BUNDLE_TRANSFORMER',
+					message: `Error transforming bundle${plugin.name ? ` with '${plugin.name}' plugin` : ''}: ${err.message}`,
+					plugin: plugin.name
+				});
 			});
-		}
+		});
 
-		if ( result == null ) return code;
-
-		if ( typeof result === 'string' ) {
-			result = {
-				code: result,
-				map: null
-			};
-		}
-
-		const map = typeof result.map === 'string' ? JSON.parse( result.map ) : result.map;
-		if ( map && typeof map.mappings === 'string' ) {
-			map.mappings = decode( map.mappings );
-		}
-
-		sourceMapChain.push( map );
-
-		return result.code;
-	}, code );
+	}, Promise.resolve( code ) );
 }
