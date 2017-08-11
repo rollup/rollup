492c228b99588a72b00c60c4c19ebea362272408
Rich Harris <richard.a.harris@gmail.com>
2017-08-11 11:30:00 -0400

    gah, try now

--- test/cli/samples/node-config/_config.js
+++ test/cli/samples/node-config/_config.js
@@ -1,3 +1,8 @@
+const sander = require('sander');
+
+sander.rimrafSync(__dirname, 'node_modules');
+sander.copydirSync(__dirname, 'node_modules_rename_me').to(__dirname, 'node_modules');
+
 module.exports = {
 	description: 'uses config file installed from npm',
 	command: 'rollup --config node:foo',
