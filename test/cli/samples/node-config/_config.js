b9d07793310ef3aee71cd0582965374a12915c01
Rich Harris <richard.a.harris@gmail.com>
2017-08-11 12:28:08 -0400

    losing my goddamn mind over here

--- test/cli/samples/node-config/_config.js
+++ test/cli/samples/node-config/_config.js
@@ -1,3 +1,10 @@
+const glob = require('glob');
+
+console.error(
+	`contents of ${__dirname}`,
+	glob.sync('**', {cwd: __dirname})
+);
+
 module.exports = {
 	description: 'uses config file installed from npm',
 	command: 'rollup --config node:foo',
