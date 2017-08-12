f8f2fa7ad4fc206cfcbdee89874041b7af2ba688
Rich Harris <richard.a.harris@gmail.com>
2017-08-11 21:27:08 -0400

    init for loop scopes correctly

--- src/ast/nodes/ForStatement.js
+++ src/ast/nodes/ForStatement.js
@@ -6,9 +6,13 @@ export default class ForStatement extends Statement {
 		if ( this.init ) this.init.initialise( this.scope );
 		if ( this.test ) this.test.initialise( this.scope );
 		if ( this.update ) this.update.initialise( this.scope );
-		this.body.initialiseAndReplaceScope ?
-			this.body.initialiseAndReplaceScope( this.scope ) :
+
+		if ( this.body.type === 'BlockStatement' ) {
+			this.body.initialiseScope( this.scope );
+			this.body.initialiseChildren();
+		} else {
 			this.body.initialise( this.scope );
+		}
 	}
 
 	initialiseScope ( parentScope ) {
---
+++ test/function/samples/for-in-loop-head-dependencies/_config.js
@@ -0,0 +1,3 @@
+module.exports = {
+	description: 'uses parent scope for right-hand-side in for-in statement (#1480)'
+};
