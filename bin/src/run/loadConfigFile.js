0c03d28e93153315fc72d90683550a6f40a3268f
Rich Harris <richard.a.harris@gmail.com>
2017-08-11 23:02:35 -0400

    watch config file, restart on changes (#1535)

---
+++ bin/src/run/loadConfigFile.js
@@ -0,0 +1,52 @@
+import path from 'path';
+import chalk from 'chalk';
+import * as rollup from 'rollup';
+import batchWarnings from './batchWarnings.js';
+import relativeId from '../../../src/utils/relativeId.js';
+import { handleError, stderr } from '../logging.js';
+
+export default function loadConfigFile (configFile, silent) {
+	const warnings = batchWarnings();
+
+	return rollup.rollup({
+		entry: configFile,
+		external: id => {
+			return (id[0] !== '.' && !path.isAbsolute(id)) || id.slice(-5,id.length) === '.json';
+		},
+		onwarn: warnings.add
+	})
+		.then( bundle => {
+			if ( !silent && warnings.count > 0 ) {
+				stderr( chalk.bold( `loaded ${relativeId( configFile )} with warnings` ) );
+				warnings.flush();
+			}
+
+			return bundle.generate({
+				format: 'cjs'
+			});
+		})
+		.then( ({ code }) => {
+			// temporarily override require
+			const defaultLoader = require.extensions[ '.js' ];
+			require.extensions[ '.js' ] = ( m, filename ) => {
+				if ( filename === configFile ) {
+					m._compile( code, filename );
+				} else {
+					defaultLoader( m, filename );
+				}
+			};
+
+			const configs = require( configFile );
+			if ( Object.keys( configs ).length === 0 ) {
+				handleError({
+					code: 'MISSING_CONFIG',
+					message: 'Config file must export an options object, or an array of options objects',
+					url: 'https://github.com/rollup/rollup/wiki/Command-Line-Interface#using-a-config-file'
+				});
+			}
+
+			require.extensions[ '.js' ] = defaultLoader;
+
+			return Array.isArray( configs ) ? configs : [configs];
+		});
+}
