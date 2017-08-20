224e192856699d9b67e0626fd6b4e0c3432158f4
Rich Harris <richard.a.harris@gmail.com>
2017-08-20 18:45:56 -0400

    modernise build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,53 +1,106 @@
 import { readFileSync } from 'fs';
 import buble from 'rollup-plugin-buble';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
+import string from 'rollup-plugin-string';
+import pkg from './package.json';
 
-var pkg = JSON.parse( readFileSync( 'package.json', 'utf-8' ) );
-
-var commitHash = (function () {
+const commitHash = (function() {
 	try {
-		return readFileSync( '.commithash', 'utf-8' );
-	} catch ( err ) {
+		return readFileSync('.commithash', 'utf-8');
+	} catch (err) {
 		return 'unknown';
 	}
 })();
 
-var banner = readFileSync( 'src/banner.js', 'utf-8' )
-	.replace( '${version}', pkg.version )
-	.replace( '${time}', new Date() )
-	.replace( '${commitHash}', commitHash );
+const banner = readFileSync('src/banner.js', 'utf-8')
+	.replace('${version}', pkg.version)
+	.replace('${time}', new Date())
+	.replace('${commitHash}', commitHash);
 
-export default {
-	entry: 'src/node-entry.js',
-	plugins: [
-		json(),
+export default [
+	/* rollup.js and rollup.es.js */
+	{
+		input: 'src/node-entry.js',
+		plugins: [
+			json(),
+			buble({
+				include: ['src/**', 'node_modules/acorn/**'],
+				target: {
+					node: '4'
+				}
+			}),
+			resolve(),
+			commonjs()
+		],
+		external: ['fs', 'path', 'events', 'module'],
+		banner,
+		sourcemap: true,
+		output: [
+			{ file: 'dist/rollup.js', format: 'cjs' },
+			{ file: 'dist/rollup.es.js', format: 'es' }
+		]
+	},
 
-		buble({
-			include: [ 'src/**', 'node_modules/acorn/**' ],
-			target: {
-				node: '0.12'
+	/* rollup.browser.js */
+	{
+		input: 'src/browser-entry.js',
+		plugins: [
+			json(),
+			buble({
+				include: ['src/**', 'node_modules/acorn/**'],
+				target: {
+					node: '4'
+				}
+			}),
+			resolve(),
+			commonjs(),
+			{
+				load: id => {
+					if ( ~id.indexOf( 'fs.js' ) ) return readFileSync( 'browser/fs.js', 'utf-8' );
+					if ( ~id.indexOf( 'path.js' ) ) return readFileSync( 'browser/path.js', 'utf-8' );
+				}
 			}
-		}),
+		],
+		banner,
+		sourcemap: true,
+		output: [
+			{
+				file: 'dist/rollup.browser.js',
+				format: 'umd',
+				name: 'rollup'
+			}
+		]
+	},
 
-		nodeResolve({
-			jsnext: true
-		}),
-
-		commonjs()
-	],
-	external: [
-		'fs',
-		'path',
-		'events',
-		'module'
-	],
-	banner,
-	sourceMap: true,
-	moduleName: 'rollup',
-	targets: [
-		{ dest: 'dist/rollup.js', format: 'cjs' },
-		{ dest: 'dist/rollup.es.js', format: 'es' }
-	]
-};
+	/* bin/rollup */
+	{
+		input: 'bin/src/index.js',
+		plugins: [
+			string({ include: '**/*.md' }),
+			json(),
+			buble({ target: { node: 4 } }),
+			commonjs({
+				include: 'node_modules/**'
+			}),
+			resolve()
+		],
+		external: [
+			'fs',
+			'path',
+			'module',
+			'events',
+			'rollup',
+			'os'
+		],
+		output: {
+			file: 'bin/rollup',
+			format: 'cjs',
+			banner: '#!/usr/bin/env node',
+			paths: {
+				rollup: '../dist/rollup.js'
+			}
+		}
+	}
+];
