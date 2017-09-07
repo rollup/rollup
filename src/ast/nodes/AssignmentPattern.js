f8ca69d3a6208e30a97e19b1378a9d60f8b94c0a
Lukas Taegert <lukas.taegert@tngtech.com>
2017-09-07 18:15:26 +0200

    Test and fix a bug where assignments to patterns with defaults would not always register all side effects.

--- src/ast/nodes/AssignmentPattern.js
+++ src/ast/nodes/AssignmentPattern.js
@@ -1,6 +1,10 @@
 import Node from '../Node.js';
 
 export default class AssignmentPattern extends Node {
+	bindAssignment ( expression ) {
+		this.left.bindAssignment( expression );
+	}
+
 	hasEffectsWhenAssigned ( options ) {
 		return this.left.hasEffectsWhenAssigned( options );
 	}
