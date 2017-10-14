d53dcb15c604d1bdb80d9907ef7c94c7acfcd0c7
Lukas Taegert <lukas.taegert@tngtech.com>
2017-10-14 22:37:33 +0200

    Rework logical expressions to provide partial evaluation where possible and understand all relevant effects.

--- src/ast/nodes/LogicalExpression.js
+++ src/ast/nodes/LogicalExpression.js
@@ -1,43 +1,55 @@
 import Node from '../Node.js';
 import { UNKNOWN_VALUE } from '../values.js';
 
-const operators = {
-	'&&': ( left, right ) => left && right,
-	'||': ( left, right ) => left || right
-};
-
 export default class LogicalExpression extends Node {
 	getValue () {
 		const leftValue = this.left.getValue();
 		if ( leftValue === UNKNOWN_VALUE ) return UNKNOWN_VALUE;
+		if ( (leftValue && this.operator === '||') || (!leftValue && this.operator === '&&') ) {
+			return leftValue;
+		}
+		return this.right.getValue();
+	}
 
-		const rightValue = this.right.getValue();
-		if ( rightValue === UNKNOWN_VALUE ) return UNKNOWN_VALUE;
-
-		return operators[ this.operator ]( leftValue, rightValue );
+	hasEffects ( options ) {
+		const leftValue = this.left.getValue();
+		return this.left.hasEffects( options )
+			|| (
+				( leftValue === UNKNOWN_VALUE
+					|| (!leftValue && this.operator === '||')
+					|| (leftValue && this.operator === '&&') )
+				&& this.right.hasEffects( options )
+			);
 	}
 
 	hasEffectsWhenAccessedAtPath ( path, options ) {
-		const leftValue = this.left.getValue();
-		if ( leftValue === UNKNOWN_VALUE ) {
-			return this.left.hasEffectsWhenAccessedAtPath( path, options )
-				|| this.right.hasEffectsWhenAccessedAtPath( path, options );
-		}
-		if ( (leftValue && this.operator === '||') || (!leftValue && this.operator === '&&') ) {
-			return this.left.hasEffectsWhenAccessedAtPath( path, options );
-		}
-		return this.right.hasEffectsWhenAccessedAtPath( path, options );
+		return this._someRelevantBranch( node =>
+			node.hasEffectsWhenAccessedAtPath( path, options ) );
 	}
 
 	hasEffectsWhenAssignedAtPath ( path, options ) {
+		return this._someRelevantBranch( node =>
+			node.hasEffectsWhenAssignedAtPath( path, options ) );
+	}
+
+	hasEffectsWhenCalledAtPath ( path, callOptions, options ) {
+		return this._someRelevantBranch( node =>
+			node.hasEffectsWhenCalledAtPath( path, callOptions, options ) );
+	}
+
+	someReturnExpressionWhenCalledAtPath ( path, callOptions, predicateFunction, options ) {
+		return this._someRelevantBranch( node =>
+			node.someReturnExpressionWhenCalledAtPath( path, callOptions, predicateFunction, options ) );
+	}
+
+	_someRelevantBranch ( predicateFunction ) {
 		const leftValue = this.left.getValue();
 		if ( leftValue === UNKNOWN_VALUE ) {
-			return this.left.hasEffectsWhenAssignedAtPath( path, options )
-				|| this.right.hasEffectsWhenAssignedAtPath( path, options );
+			return predicateFunction( this.left ) || predicateFunction( this.right );
 		}
 		if ( (leftValue && this.operator === '||') || (!leftValue && this.operator === '&&') ) {
-			return this.left.hasEffectsWhenAssignedAtPath( path, options );
+			return predicateFunction( this.left );
 		}
-		return this.right.hasEffectsWhenAssignedAtPath( path, options );
+		return predicateFunction( this.right );
 	}
 }
