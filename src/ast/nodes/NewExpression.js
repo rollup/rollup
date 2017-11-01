503b8aaf95dbe5118268a38c92534d7dbf7999ad
Lukas Taegert <lukas.taegert@tngtech.com>
2017-11-01 11:19:23 +0100

    Bind calls to ES5 constructors

--- src/ast/nodes/NewExpression.js
+++ src/ast/nodes/NewExpression.js
@@ -2,6 +2,10 @@ import Node from '../Node.js';
 import CallOptions from '../CallOptions';
 
 export default class NewExpression extends Node {
+	bindNode () {
+		this.callee.bindCallAtPath( [], this._callOptions );
+	}
+
 	hasEffects ( options ) {
 		return this.arguments.some( child => child.hasEffects( options ) )
 			|| this.callee.hasEffectsWhenCalledAtPath( [], this._callOptions, options.getHasEffectsWhenCalledOptions() );
@@ -12,6 +16,6 @@ export default class NewExpression extends Node {
 	}
 
 	initialiseNode () {
-		this._callOptions = CallOptions.create( { withNew: true } );
+		this._callOptions = CallOptions.create( { withNew: true, args: this.arguments } );
 	}
 }
