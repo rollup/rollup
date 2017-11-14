4ad48c30bc931e83b43c816ccf21ffd015fda77d
Soo Jae Hwang <misoguy1985@gmail.com>
2017-11-14 14:23:46 +0900

    Add test to verify error object after transform not showing correct location

---
+++ test/function/samples/error-after-transform-should-throw-correct-location/_config.js
@@ -0,0 +1,39 @@
+var path = require( 'path' );
+var assert = require( 'assert' );
+var MagicString = require( 'magic-string' );
+
+module.exports = {
+	description: 'error after transform should throw with correct location of file',
+	options: {
+		plugins: [
+			{
+				transform: function ( source, id ) {
+					var s = new MagicString( source );
+					s.prepend( "import _assign from 'object-assign';\n" );
+
+					return {
+						code: s.toString(),
+						map: s.generateMap({ hires: true })
+					};
+				}
+			}
+		]
+	},
+	error: {
+		code: 'MISSING_EXPORT',
+		message: `'default' is not exported by empty.js`,
+		pos: 44,
+		loc: {
+			file: path.resolve( __dirname, 'main.js' ),
+			line: 1,
+			column: 7
+		},
+		frame: `
+			1: import a from './empty.js';
+			          ^
+			2:
+			3: Object.assign({}, a);
+		`,
+		url: `https://github.com/rollup/rollup/wiki/Troubleshooting#name-is-not-exported-by-module`
+	}
+};
