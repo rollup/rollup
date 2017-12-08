862b2ca0db13bc304261d403f89fb32d5b70f407
Guy Bedford <guybedford@gmail.com>
2017-12-08 10:40:50 +0200

    update self tests to slightly more realistic scenarios

--- test/function/samples/can-import-self/_config.js
+++ test/function/samples/can-import-self/_config.js
@@ -3,14 +3,6 @@ var assert = require( 'assert' );
 module.exports = {
 	description: 'a module importing its own bindings',
 	exports: function ( exports ) {
-		assert.equal(exports.default, 'me');
-		assert.equal(exports.another, 'me');
-	},
-	warnings: [
-		{
-			code: 'MIXED_EXPORTS',
-			message: `Using named and default exports together. Consumers of your bundle will have to use bundle['default'] to access the default export, which may not be what you want. Use \`exports: 'named'\` to disable this warning`,
-			url: `https://github.com/rollup/rollup/wiki/JavaScript-API#exports`
-		}
-	]
+		assert.equal(exports.result, 4);
+	}
 };
