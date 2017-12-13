6bfb9c4a13450fb4bbbd9e0c8fe8ebde1da969e1
Guy Bedford <guybedford@gmail.com>
2017-12-13 14:53:18 +0200

    formatting fixes

--- test/function/samples/can-import-self/_config.js
+++ test/function/samples/can-import-self/_config.js
@@ -3,6 +3,6 @@ var assert = require( 'assert' );
 module.exports = {
 	description: 'a module importing its own bindings',
 	exports: function ( exports ) {
-		assert.equal(exports.result, 4);
+		assert.equal( exports.result, 4 );
 	}
 };
--- test/function/samples/can-import-self/lib.js
+++ test/function/samples/can-import-self/lib.js
@@ -1,3 +1,3 @@
 import * as lib from './lib';
 export const op = x => x * x;
-export const run = () => lib.op(2);
+export const run = () => lib.op( 2 );
