a716235c509c0f0625b93151307b5efa634e4736
Guy Bedford <guybedford@gmail.com>
2017-12-13 14:47:40 +0200

    external function parent and resolved argument

---
+++ test/function/samples/external-alias-parent/_config.js
@@ -0,0 +1,36 @@
+var assert = require( 'assert' );
+var path = require( 'path' );
+
+module.exports = {
+	description: 'includes an external module included dynamically by an alias',
+	options: {
+		input: path.join( __dirname, 'first', 'main.js' ),
+		external: function ( id, parentId, isResolved ) {
+			if ( isResolved === false || !parentId )
+				return;
+			if ( parentId.endsWith( 'main.js' ) ) {
+				return id === 'lodash';
+			} else {
+				return id === 'underscore';
+			}
+		},
+
+		// Define a simple alias plugin for underscore
+		plugins: [
+			{
+				resolveId: function ( id, parentId ) {
+					if ( id === 'underscore' && parentId && parentId.endsWith( 'main.js' ) ) {
+						return 'lodash';
+					}
+				}
+			}
+		]
+	},
+
+	context: {
+		require: function ( required ) {
+			assert( required === 'lodash' || required === 'underscore' );
+			return 1;
+		}
+	}
+};
