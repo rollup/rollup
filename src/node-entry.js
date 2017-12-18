fe2aa7df1245236e32a7b423959b9e7ac762c899
Guy Bedford <guybedford@gmail.com>
2017-12-18 23:09:48 +0200

    export direct Bundle interface

--- src/node-entry.js
+++ src/node-entry.js
@@ -1,3 +1,4 @@
 export { default as rollup } from './rollup/index.js';
 export { default as watch } from './watch/index.js';
 export { version as VERSION } from '../package.json';
+export { default as Bundle } from './Bundle.js';
