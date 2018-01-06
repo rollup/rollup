d7213d0747ada162b43d46305aacf661bb3dde85
Lukas Taegert <lukas.taegert@tngtech.com>
2018-01-06 17:41:47 +0100

    Forgot to move assignment check out of Node

--- src/ast/nodes/shared/Pattern.ts
+++ src/ast/nodes/shared/Pattern.ts
@@ -6,5 +6,9 @@ import ExecutionPathOptions from '../../ExecutionPathOptions';
 export interface PatternNode extends WritableEntity, Node {}
 
 export class GenericPatternNode extends GenericNode implements PatternNode {
+	hasEffectsWhenAssignedAtPath (_path: ObjectPath, _options: ExecutionPathOptions) {
+		return true;
+	}
+
 	reassignPath (_path: ObjectPath, _options: ExecutionPathOptions) {}
 }
