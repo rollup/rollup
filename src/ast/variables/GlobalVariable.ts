57a527cfb0119897dd6bfaa57a35c78403a54266
Lukas Taegert <lukas.taegert@tngtech.com>
2018-01-09 09:23:25 +0100

    Quick fix to enable tree-shaking babel-helpers again.

--- src/ast/variables/GlobalVariable.ts
+++ src/ast/variables/GlobalVariable.ts
@@ -21,18 +21,19 @@ export default class GlobalVariable extends Variable {
 	hasEffectsWhenAccessedAtPath (path: ObjectPath) {
 		// path.length == 0 can also have an effect but we postpone this for now
 		return (
-			path.length > 0 &&
-			!pureFunctions[[this.name, ...path].join('.')] &&
-			!pureFunctions[[this.name, ...path.slice(0, -1)].join('.')] &&
-			!(
-				path.length > 1 &&
-				pureFunctions[[this.name, ...path.slice(0, -2)].join('.')] &&
-				path[path.length - 2] === 'prototype'
-			)
+			path.length > 0
+				&& !this.isPureFunctionMember(path)
+				&& !(this.name === 'Reflect' && path.length === 1)
 		);
 	}
 
 	hasEffectsWhenCalledAtPath (path: ObjectPath) {
 		return !pureFunctions[[this.name, ...path].join('.')];
+	}
+
+	private isPureFunctionMember (path: ObjectPath) {
+		return pureFunctions[[this.name, ...path].join('.')]
+			|| (path.length >= 1 && pureFunctions[[this.name, ...path.slice(0, -1)].join('.')])
+			|| (path.length >= 2 && pureFunctions[[this.name, ...path.slice(0, -2)].join('.')] && path[path.length - 2] === 'prototype');
 	}
 }
