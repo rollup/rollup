43e6dc004f8b2efe5cb21e832b184e1f94de93c7
Lukas Taegert <lukas.taegert@tngtech.com>
2018-01-15 07:22:53 +0100

    Add failing test for namespace imports from chunks

---
+++ test/chunking-form/samples/namespace-imports-from-chunks/_config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	description: 'simple chunking',
+	options: {
+		input: ['main1.js', 'main2.js']
+	},
+	solo: true
+};
---
+++ test/chunking-form/samples/namespace-imports-from-chunks/main2.js
@@ -0,0 +1,3 @@
+import * as main1 from './main1.js';
+
+console.log( main1.p );
