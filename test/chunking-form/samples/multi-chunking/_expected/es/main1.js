6213e9a0dd708ea7df714094e3b555801ae18a2b
Guy Bedford <guybedford@gmail.com>
2018-01-21 01:54:05 +0200

    fix up import binding bug

--- test/chunking-form/samples/multi-chunking/_expected/es/main1.js
+++ test/chunking-form/samples/multi-chunking/_expected/es/main1.js
@@ -1,4 +1,4 @@
-import { num, num as num$1 } from './chunk1.js';
-import { num, num as num$1 } from './chunk2.js';
+import { num } from './chunk1.js';
+import { num as num$1 } from './chunk2.js';
 
 console.log(num + num$1);
--- test/chunking-form/samples/multi-chunking/_expected/es/main3.js
+++ test/chunking-form/samples/multi-chunking/_expected/es/main3.js
@@ -1,4 +1,4 @@
-import { num, num as num$1 } from './chunk1.js';
-import { num, num as num$1 } from './chunk3.js';
+import { num } from './chunk1.js';
+import { num as num$1 } from './chunk3.js';
 
 console.log(num + num$1);
