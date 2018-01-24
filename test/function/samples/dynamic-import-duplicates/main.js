684fa64cfea2e0764b228ee51d3121b68a821b20
Guy Bedford <guybedford@gmail.com>
2018-01-24 23:09:06 +0200

    Node 4 test case fix

--- test/function/samples/dynamic-import-duplicates/main.js
+++ test/function/samples/dynamic-import-duplicates/main.js
@@ -1,5 +1,5 @@
 export var x = 41;
 
-export var promise = Promise.all([import('./foo'), import('./foo')]).then(([foo]) => {
-  return foo.x;
+export var promise = Promise.all([import('./foo'), import('./foo')]).then(foos => {
+  return foos[0].x;
 });
