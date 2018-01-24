66b4e677734259ecd9e2f9a8cd85b561a379f17b
Erik <erikdesjardins@users.noreply.github.com>
2018-01-24 11:40:31 -0500

    failing test for plugin load returning empty string

---
+++ test/function/samples/load-returns-empty-string/_config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	description: 'loaders are permitted to return the empty string',
+	options: {
+		plugins: [
+			{
+				load: function ( id ) {
+					if ( /foo\.js/.test( id ) ) {
+						return '';
+					}
+				}
+			},
+			{
+				load: function ( id ) {
+					if ( /bar\.js/.test( id ) ) {
+						return { code: '', map: null };
+					}
+				}
+			}
+		]
+	}
+};
