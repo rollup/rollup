03225ddc5f0312b195ecd823ad10cae6752adebb
Guy Bedford <guybedford@gmail.com>
2018-02-08 11:23:01 +0200

    feedback and #1951 case fix

---
+++ test/form/samples/external-namespace-and-named/_config.js
@@ -0,0 +1,3 @@
+module.exports = {
+	description: 'Correctly handles external namespace tracing with both namespace and named exports'
+};
---
+++ test/form/samples/external-namespace-and-named/_expected/es.js
@@ -0,0 +1,6 @@
+import * as foo from 'foo';
+import { blah, bar } from 'foo';
+
+console.log(foo);
+console.log(blah);
+console.log(bar);
