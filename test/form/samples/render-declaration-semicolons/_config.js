0ce0eb4add7a964bb471666b8f14c8c82959c19f
Lukas Taegert <lukas.taegert@tngtech.com>
2018-02-21 07:33:46 +0100

    Resolve #1993

---
+++ test/form/samples/render-declaration-semicolons/_config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	description: 'properly inserts semi-colons after declarations (#1993)',
+	options: { output: { name: 'bundle' } }
+};
---
+++ test/form/samples/render-declaration-semicolons/_expected/iife.js
@@ -0,0 +1,35 @@
+var bundle = (function (exports) {
+	'use strict';
+
+	var a, b;
+	console.log(a, b);
+
+	var c, d;
+	console.log(c, d);
+
+	const e = 1, f = 2;
+	console.log(e, f);
+
+	const g = 3, h = 4;
+	console.log(g, h);
+
+	var i, j;
+
+	var k, l;
+
+	const m = 1, n = 2;
+
+	const o = 3, p = 4;
+
+	exports.i = i;
+	exports.j = j;
+	exports.k = k;
+	exports.l = l;
+	exports.m = m;
+	exports.n = n;
+	exports.o = o;
+	exports.p = p;
+
+	return exports;
+
+}({}));
