004c8c9a358f74d65449c61fb09ca18f833feeb6
Lukas Taegert <lukas.taegert@tngtech.com>
2018-03-12 08:53:48 +0100

    Add tests and code-splitting support

---
+++ test/function/samples/adds-timings-to-bundle-when-codesplitting/_config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const assert = require('assert');
+
+module.exports = {
+	description: 'Adds timing information to bundle when bundling with perf=true',
+	options: {
+		input: ['main1.js', 'main2.js'],
+		experimentalCodeSplitting: true,
+		perf: true
+	},
+	bundle(bundle) {
+		const timings = bundle.getTimings();
+		const timers = Object.keys(timings);
+		assert.ok(timers.indexOf('# BUILD') >= 0, '# BUILD time is not measured.');
+		assert.ok(timers.indexOf('# GENERATE') >= 0, '# GENERATE time is not measured.');
+		timers.forEach(timer => {
+			assert.equal(typeof timings[timer], 'number');
+			assert.ok(timings[timer] >= 0, 'Timer is not non-negative.');
+		})
+	}
+};
