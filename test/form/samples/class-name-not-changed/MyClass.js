2473c500a7826ea6660f9c4cbeceb47031b1c708
Guy Bedford <guybedford@gmail.com>
2018-03-12 13:33:28 +0200

    Revert "Have rollup emit `let $safeClassName = class $originalClassName { ...` instead of"
    This reverts commit 9e91088a6653f5655ae5bff8847bc2d098f67374.

--- test/form/samples/class-name-not-changed/MyClass.js
+++
@@ -1,3 +0,0 @@
-export class MyClass {
-	constructor() { }
-}
--- test/form/samples/class-name-not-changed/main.js
+++
@@ -1,5 +0,0 @@
-export class MyClass {
-	constructor() { }
-}
-assert.equal(MyClass.name, "MyClass") // oops
-export { MyClass as MyClass2 } from "./MyClass"
