b7ca117b04cd09fa66ebe468970eb7ec3a35cac7
Vladimir Agafonkin <agafonkin@gmail.com>
2018-03-14 20:01:20 +0200

    remove segment coercions for now

--- src/Module.ts
+++ src/Module.ts
@@ -476,18 +476,18 @@ export default class Module {
 
 		while (filteredSourcemapChain.length > 0) {
 			const sourcemap = filteredSourcemapChain.pop();
-			const line = sourcemap.mappings[location.line - 1];
+			const line: any = sourcemap.mappings[location.line - 1];
 			let locationFound = false;
 
 			if (line !== undefined) {
 				for (const segment of line) {
-					if (+segment[0] >= location.column) {
+					if (segment[0] >= location.column) {
 						if (segment.length < 4) break;
 						location = {
-							line: +segment[2] + 1,
-							column: +segment[3],
-							source: sourcemap.sources[+segment[1]],
-							name: sourcemap.names[+segment[4]]
+							line: segment[2] + 1,
+							column: segment[3],
+							source: sourcemap.sources[segment[1]],
+							name: sourcemap.names[segment[4]]
 						};
 						locationFound = true;
 						break;
