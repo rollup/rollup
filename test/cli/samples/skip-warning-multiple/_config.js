4e552e1939db393a225aae9634a4e4fee42d67de
Greg Skiano <gregory.skiano@nytimes.com>
2018-03-29 07:57:21 -0700

    removing solo

--- test/cli/samples/skip-warning-multiple/_config.js
+++ test/cli/samples/skip-warning-multiple/_config.js
@@ -1,5 +1,4 @@
 module.exports = {
-	solo: true,
 	description: 'ignores warnings by code',
 	command: 'rollup --config rollup.config.js --format es --skipWarning CIRCULAR_DEPENDENCY -s UNRESOLVED_IMPORT'
 };
--- test/cli/samples/skip-warning-single/_config.js
+++ test/cli/samples/skip-warning-single/_config.js
@@ -1,5 +1,4 @@
 module.exports = {
-	solo: true,
 	description: 'ignores warnings by code',
 	command: 'rollup --config rollup.config.js --format es -s UNRESOLVED_IMPORT'
 };
