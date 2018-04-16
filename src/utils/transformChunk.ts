5e61a2eb1ae746a9afd4cef9dae915bd99828652
Guy Bedford <guybedford@gmail.com>
2018-04-16 11:20:57 +0200

    unified plugin interface, pluginContext

--- src/utils/transformChunk.ts
+++ src/utils/transformChunk.ts
@@ -1,29 +1,38 @@
 import { decode } from 'sourcemap-codec';
 import error from './error';
-import { OutputOptions, Plugin, RawSourceMap } from '../rollup/types';
+import { OutputOptions, RawSourceMap } from '../rollup/types';
+import Graph from '../Graph';
 
 export default function transformChunk(
+	graph: Graph,
 	code: string,
-	plugins: Plugin[],
 	sourcemapChain: RawSourceMap[],
 	options: OutputOptions
 ) {
-	return plugins.reduce((promise, plugin) => {
+	return graph.plugins.reduce((promise, plugin) => {
 		if (!plugin.transformBundle) return promise;
 
 		return promise.then(code => {
 			return Promise.resolve()
-				.then(() => (plugin.transformChunk || plugin.transformBundle)(code, options))
+				.then(() =>
+					(plugin.transformChunk || plugin.transformBundle).call(graph.pluginContext, code, options)
+				)
 				.then(result => {
 					if (result == null) return code;
 
-					if (typeof result === 'string' && plugin.transformBundle) {
-						result = {
-							code: result,
-							map: undefined
-						};
-					} else if (typeof result !== 'string' || typeof result.code !== 'string' || !result.map) {
-						throw new Error('transformChunk');
+					if (plugin.transformBundle) {
+						if (typeof result === 'string') {
+							result = {
+								code: result,
+								map: undefined
+							};
+						}
+					} else if (typeof result === 'string') {
+						throw new Error('transformChunk must return a { code, map } object, not a string.');
+					} else if (!result.map && options.sourcemap) {
+						throw new Error(
+							'transformChunk must return a "map" sourcemap property when sourcemaps are enabled.'
+						);
 					}
 
 					const map = typeof result.map === 'string' ? JSON.parse(result.map) : result.map;
@@ -41,9 +50,8 @@ export default function transformChunk(
 				.catch(err => {
 					error({
 						code: plugin.transformChunk ? 'BAD_CHUNK_TRANSFORMER' : 'BAD_BUNDLE_TRANSFORMER',
-						message: `Error transforming bundle${
-							plugin.name ? ` with '${plugin.name}' plugin` : ''
-						}: ${err.message}`,
+						message: `Error transforming ${(plugin.transformChunk ? 'chunk' : 'bundle') +
+							(plugin.name ? ` with '${plugin.name}' plugin` : '')}: ${err.message}`,
 						plugin: plugin.name
 					});
 				});
