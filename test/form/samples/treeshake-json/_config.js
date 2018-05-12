4bb67cb87028ce13a6ad5209198d973a04a0b07d
Lukas Taegert <lukas.taegert@tngtech.com>
2018-05-12 20:06:51 +0200

    Unskip JSON test

--- test/form/samples/treeshake-json/_config.js
+++ test/form/samples/treeshake-json/_config.js
@@ -1,8 +1,6 @@
 const json = require('rollup-plugin-json');
 
 module.exports = {
-	// skipped until rollup-plugin-json properly fills the "value" field of Literals
-	skip: true,
 	description: 'removes unusued json keys',
 	options: { plugins: [json()] }
 };
