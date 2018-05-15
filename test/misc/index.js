60653880da906482f4a5b2874560e1cf2b7b0cf0
Lukas Taegert <lukas.taegert@tngtech.com>
2018-05-15 07:57:53 +0200

    Fix code style

--- test/misc/index.js
+++ test/misc/index.js
@@ -2,8 +2,8 @@ const assert = require('assert');
 const path = require('path');
 const rollup = require('../../dist/rollup');
 const { executeBundle, loader } = require('../utils.js');
-const { SourceMapConsumer } = require( 'source-map' );
-const { getLocator } = require( 'locate-character' );
+const { SourceMapConsumer } = require('source-map');
+const { getLocator } = require('locate-character');
 
 describe('sanity checks', () => {
 	it('exists', () => {
@@ -162,30 +162,30 @@ describe('sanity checks', () => {
 });
 
 describe('in-memory sourcemaps', () => {
-	it( 'generates an in-memory sourcemap', () => {
-		return rollup.rollup({
-			input: 'main',
-			plugins: [loader({ main: `console.log( 42 );` })],
-		})
-		.then(bundle => {
-			return bundle.generate({
-				format: 'cjs',
-				sourcemap: true,
-				sourcemapFile: path.resolve( 'bundle.js' )
-			});
-		})
-		.then(generated => {
-			const smc = new SourceMapConsumer( generated.map );
-			const locator = getLocator( generated.code, { offsetLine: 1 });
-	
-			let generatedLoc = locator( '42' );
-			let loc = smc.originalPositionFor( generatedLoc ); // 42
-			assert.equal( loc.source, 'main' );
-			assert.equal( loc.line, 1 );
-			assert.equal( loc.column, 13 );
-		});
-	});
+	it('generates an in-memory sourcemap', () => {
+		return rollup
+			.rollup({
+				input: 'main',
+				plugins: [loader({ main: `console.log( 42 );` })]
+			})
+			.then(bundle => {
+				return bundle.generate({
+					format: 'cjs',
+					sourcemap: true,
+					sourcemapFile: path.resolve('bundle.js')
+				});
+			})
+			.then(generated => {
+				const smc = new SourceMapConsumer(generated.map);
+				const locator = getLocator(generated.code, { offsetLine: 1 });
 
+				const generatedLoc = locator('42');
+				const loc = smc.originalPositionFor(generatedLoc); // 42
+				assert.equal(loc.source, 'main');
+				assert.equal(loc.line, 1);
+				assert.equal(loc.column, 13);
+			});
+	});
 });
 
 describe('deprecations', () => {
