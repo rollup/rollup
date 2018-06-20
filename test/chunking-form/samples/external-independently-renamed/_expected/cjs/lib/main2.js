724270af225230da38190936d592666355fb61c1
Kelly Selden <kellyselden@gmail.com>
2018-06-20 16:20:36 +0100

    imports of the same external can be independently renamed

---
+++ test/chunking-form/samples/external-independently-renamed/_expected/cjs/lib/main2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var placeholder = require('../internal');
+
+
+
+Object.keys(placeholder).forEach(function (key) { exports[key] = placeholder[key]; });
