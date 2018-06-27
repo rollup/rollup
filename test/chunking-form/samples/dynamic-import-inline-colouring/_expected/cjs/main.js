c3228cde9f4358ec08c349bef123b3289e33b8d3
Guy Bedford <guybedford@gmail.com>
2018-06-27 06:11:49 +0200

    Inline dynamic imports that are also imported statically and only used in a single chunk (#2295)

---
+++ test/chunking-form/samples/dynamic-import-inline-colouring/_expected/cjs/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var foo = "FOO";
+
+var foo$1 = /*#__PURE__*/Object.freeze({
+	default: foo
+});
+
+var main = Promise.resolve().then(function () { return foo$1; });
+
+module.exports = main;
