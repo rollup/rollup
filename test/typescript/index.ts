e26caa496c048c053cc529c87f3cfdce6d0bff7e
Martin Hochel <hochelmartin@gmail.com>
2018-07-27 14:14:14 +0200

    types: add plugin implementation type for plugin authors

--- test/typescript/index.ts
+++ test/typescript/index.ts
@@ -1,1 +1,11 @@
-import * as rollup from './dist/rollup';
+// Plugin API
+// interface Options {
+// 	extensions?: string | string[];
+// }
+// const plugin: rollup.PluginImpl<Options> = (options = {}) => {
+// 	const extensions = options.extensions || ['.js'];
+// 	return { name: 'my-plugin' };
+// };
+
+// plugin();
+// plugin({ extensions: ['.js', 'json'] });
