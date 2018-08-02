f0ef1941a1b279ba85176dcaf1ce53c5b5b88726
Bogdan Chadkin <trysound@yandex.ru>
2018-08-03 02:54:10 +0300

    Add memory perf metric along with time metrics

--- bin/src/run/timings.ts
+++ bin/src/run/timings.ts
@@ -1,3 +1,4 @@
+import prettyBytes from 'pretty-bytes';
 import tc from 'turbocolor';
 import { SerializedTimings } from '../../../src/rollup/types';
 
@@ -5,6 +6,7 @@ export function printTimings(timings: SerializedTimings) {
 	Object.keys(timings).forEach(label => {
 		const color =
 			label[0] === '#' ? (label[1] !== '#' ? tc.underline : tc.bold) : (text: string) => text;
-		console.info(color(`${label}: ${timings[label].toFixed(0)}ms`));
+		const [time, memory] = timings[label];
+		console.info(color(`${label}: ${time.toFixed(0)}ms, ${prettyBytes(memory)}`));
 	});
 }
