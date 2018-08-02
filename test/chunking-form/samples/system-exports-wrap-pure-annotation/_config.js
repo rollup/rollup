d975fe971b1fbdebc084a13d97c3c22bd8736113
Bogdan Chadkin <trysound@yandex.ru>
2018-08-02 16:17:06 +0300

    System exports() should wrap expressions with pure annotation
    This took me a day to figure out why parts of my vendors
    are just not exported across chunks.
    
    Take this module
    https://unpkg.com/react-head@2.2.0/dist/index.esm.js
    
    `HeadCollector` will be wrapped with `exports` this way
    so uglify will consider exports is pure function and will
    happily remove it as unused.
    
    ```js
    var HeadCollector =
    /*#__PURE__*/
    exports('id', function (_Component) {
      _inheritsLoose(HeadCollector, _Component);
    ...
    ```

---
+++ test/chunking-form/samples/system-exports-wrap-pure-annotation/_config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  solo: true,
+	description: 'system exports should wrap pure annotations',
+	options: {
+		input: ['main1.js', 'main2.js'],
+	}
+};
---
+++ test/chunking-form/samples/system-exports-wrap-pure-annotation/_expected/system/main1.js
@@ -0,0 +1,14 @@
+System.register(['./chunk-85f39e10.js'], function (exports, module) {
+	'use strict';
+	var dep;
+	return {
+		setters: [function (module) {
+			dep = module.a;
+		}],
+		execute: function () {
+
+			console.log('1', dep);
+
+		}
+	};
+});
