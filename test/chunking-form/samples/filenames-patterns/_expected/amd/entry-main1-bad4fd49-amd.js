4e67254cfb6b45d86e55a5a7ab5f9662c7dd7715
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-08-11 12:09:55 +0200

    Allow [format] pattern for ID generation (#2387)

---
+++ test/chunking-form/samples/filenames-patterns/_expected/amd/entry-main1-bad4fd49-amd.js
@@ -0,0 +1,5 @@
+define(['./chunk-main2-bff49b9a-amd.js'], function (main2) { 'use strict';
+
+	main2.log(main2.dep);
+
+});
---
+++ test/chunking-form/samples/filenames-patterns/_expected/es/entry-main1-1a6a749d-esm.js
@@ -0,0 +1,3 @@
+import { a as dep, b as log } from './chunk-main2-e628225d-esm.js';
+
+log(dep);
