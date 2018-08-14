9add3e35924cf1abb6b0bfc641b73aaafb2711af
Lukas Taegert <lukastaegert@users.noreply.github.com>
2018-08-14 08:00:46 +0200

    Rename option cacheExpiry -> experimentalCacheExpiry (#2401)

--- test/hooks/index.js
+++ test/hooks/index.js
@@ -274,19 +274,20 @@ describe('hooks', () => {
 					`var input = new URL('../assets/test-19916f7d.ext', import.meta.url).href;\n\nexport default input;\n`
 				);
 
-				return rollup.rollup({
-					cache,
-					input: 'input',
-					experimentalCodeSplitting: true,
-					plugins: [
-						loader({ input: '' }),
-						{
-							transform() {
-								assert.fail('Should cache transform');
+				return rollup
+					.rollup({
+						cache,
+						input: 'input',
+						experimentalCodeSplitting: true,
+						plugins: [
+							loader({ input: '' }),
+							{
+								transform () {
+									assert.fail('Should cache transform');
+								}
 							}
-						}
-					]
-				});
+						]
+					});
 			})
 			.then(bundle => {
 				return bundle.generate({ format: 'es' });
@@ -331,21 +332,22 @@ describe('hooks', () => {
 					`var input = new URL('../assets/test-19916f7d.ext', import.meta.url).href;\n\nexport default input;\n`
 				);
 
-				return rollup.rollup({
-					cache,
-					input: 'input',
-					experimentalCodeSplitting: true,
-					plugins: [
-						loader({ input: '' }),
-						{
-							name: 'x',
-							transform() {
-								runs++;
-								return `alert('hello world')`;
+				return rollup
+					.rollup({
+						cache,
+						input: 'input',
+						experimentalCodeSplitting: true,
+						plugins: [
+							loader({ input: '' }),
+							{
+								name: 'x',
+								transform () {
+									runs++;
+									return `alert('hello world')`;
+								}
 							}
-						}
-					]
-				});
+						]
+					});
 			})
 			.then(bundle => {
 				return bundle.generate({ format: 'es' });
@@ -878,12 +880,12 @@ module.exports = input;
 		return rollup
 			.rollup({
 				input: 'input',
-				cacheExpiry: 5,
+				experimentalCacheExpiry: 5,
 				plugins: [
 					loader({ input: `alert('hello')` }),
 					{
 						name: 'x',
-						buildStart() {
+						buildStart () {
 							this.cache.set('first', 'first');
 							this.cache.set('second', 'second');
 						}
@@ -897,13 +899,14 @@ module.exports = input;
 						return rollup.rollup({
 							cache: bundle.cache,
 							input: 'input',
-							cacheExpiry: 5,
+							experimentalCacheExpiry: 5,
 							plugins: [
 								loader({ input: `alert('hello')` }),
 								{
 									name: 'x',
-									buildStart() {
-										if (i === 4) assert.equal(this.cache.has('second'), true);
+									buildStart () {
+										if (i === 4)
+											assert.equal(this.cache.has('second'), true);
 									}
 								}
 							]
@@ -916,12 +919,12 @@ module.exports = input;
 				return rollup.rollup({
 					cache: bundle.cache,
 					input: 'input',
-					cacheExpiry: 5,
+					experimentalCacheExpiry: 5,
 					plugins: [
 						loader({ input: `alert('hello')` }),
 						{
 							name: 'x',
-							buildStart() {
+							buildStart () {
 								assert.equal(this.cache.has('first'), false);
 								assert.equal(this.cache.get('first'), undefined);
 								assert.equal(this.cache.get('second'), 'second');
