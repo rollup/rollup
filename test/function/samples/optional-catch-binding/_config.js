1a7c34aea03125535e984bb42b3f981ebf0ad0cf
bathos <valentinium@gmail.com>
2018-09-12 04:27:54 -0400

    switch default ecmaVersion to 2019; remove acorn options from option catch param test

--- test/function/samples/optional-catch-binding/_config.js
+++ test/function/samples/optional-catch-binding/_config.js
@@ -3,11 +3,6 @@ const assert = require('assert');
 module.exports = {
 	description: 'allows optional catch binding with appropriate acorn settings',
 	minNodeVersion: 10,
-	options: {
-		acorn: {
-			ecmaVersion: 2019
-		}
-	},
 	exports(exports) {
 		assert.equal(exports.foo, true);
 	}
