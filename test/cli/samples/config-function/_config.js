78527619b8d217dce944062f03e1ce6a74215d49
Lukas Taegert <lukas.taegert@tngtech.com>
2018-09-12 07:57:22 +0200

    Test that the command object can be modified in the config file

--- test/cli/samples/config-function/_config.js
+++ test/cli/samples/config-function/_config.js
@@ -1,5 +1,6 @@
 module.exports = {
-	description: 'if config returns a function then this will be called with command args',
-	command: 'rollup --config rollup.config.js --silent --some-option="foo" --another-option=42',
+	description:
+		'if the config file returns a function then this will be called with the command args',
+	command: 'rollup --config rollup.config.js --silent',
 	execute: true
 };
