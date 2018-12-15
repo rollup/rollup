551654c4467ae7913450c2ce9219c414874a9c31
Lukas Taegert-Atkinson <lukastaegert@users.noreply.github.com>
2018-12-15 11:51:10 +0100

    Sanitizes chunk names to allow for virtually created chunks (#2590)

---
+++ test/chunking-form/samples/sanitize-chunk-names/_expected/amd/_virtual:entry-2.js
@@ -0,0 +1,7 @@
+define(function () { 'use strict';
+
+	var _virtual_entry2 = "\u0000virtual:entry-2";
+
+	return _virtual_entry2;
+
+});
---
+++ test/chunking-form/samples/sanitize-chunk-names/_expected/system/_virtual:entry-2.js
@@ -0,0 +1,10 @@
+System.register([], function (exports, module) {
+	'use strict';
+	return {
+		execute: function () {
+
+			var _virtual_entry2 = exports('default', "\u0000virtual:entry-2");
+
+		}
+	};
+});
