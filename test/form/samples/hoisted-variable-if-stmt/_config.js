f8600a927675e0f6c64803e9dee28af27545934a
lye <unpantsu@gmail.com>
2018-12-23 08:29:57 -0800

    Hoisting fixes (#2607)
    When hoisting a var, use UNKNOWN_EXPRESSION as the initializer instead
    of the actual one. This prevents the initializer from being used during
    the optimization step when it hasn't yet been assigned at runtime.

---
+++ test/form/samples/hoisted-variable-if-stmt/_config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	description:
+		'Properly renders branches which refer to hoisted variables from other lexical scopes'
+};
---
+++ test/form/samples/hoisted-variable-if-stmt/main.js
@@ -0,0 +1,15 @@
+if (false) {
+    var foo = true;
+}
+
+if (foo) {
+    console.log("nope");
+}
+
+if (true) {
+    var bar = true;
+}
+
+if (bar) {
+    console.log("ok");
+}
