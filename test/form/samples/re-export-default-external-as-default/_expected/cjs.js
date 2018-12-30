80ad3b918b12b039d268557d700e85825c688b07
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-12-30 21:18:02 +0100

    Fixed issue with reexporting default as default with {exports: named, interop: true} options.

---
+++ test/form/samples/re-export-default-external-as-default/_expected/cjs.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var external = require('external');
+var external__default = _interopDefault(external);
+
+
+
+Object.keys(external).forEach(function (key) { exports[key] = external[key]; });
+exports.default = external__default;
