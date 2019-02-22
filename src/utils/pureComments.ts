07ef28498fee5da8b5be6549c9bd52417f32c383
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2019-02-22 07:26:59 +0100

    Slightly simplify comment handling as I do not expect us to want to iterate more than once through all comments even if other annotation types are added

--- src/utils/pureComments.ts
+++ src/utils/pureComments.ts
@@ -4,20 +4,18 @@ import { base as basicWalker } from 'acorn-walk';
 import * as ESTree from 'estree';
 import { CommentDescription } from '../Module';
 
-type NodeHandler = (node: ESTree.Node, comment: CommentDescription) => void;
-
-function checkCommentsBeforeNode(
+function handlePureAnnotationsOfNode(
 	node: ESTree.Node & acorn.Node,
-	state: { handleNode: NodeHandler; commentIndex: number; commentNodes: CommentDescription[] },
+	state: { commentIndex: number; commentNodes: CommentDescription[] },
 	type: string = node.type
 ) {
 	let commentNode = state.commentNodes[state.commentIndex];
 	while (commentNode && node.start >= commentNode.end) {
-		state.handleNode(node, commentNode);
+		markPureNode(node, commentNode);
 		commentNode = state.commentNodes[++state.commentIndex];
 	}
 	if (commentNode && commentNode.end <= node.end) {
-		basicWalker[type](node, state, checkCommentsBeforeNode);
+		basicWalker[type](node, state, handlePureAnnotationsOfNode);
 	}
 }
 
@@ -42,9 +40,8 @@ const pureCommentRegex = /[@#]__PURE__/;
 const isPureComment = (comment: CommentDescription) => pureCommentRegex.test(comment.text);
 
 export function markPureCallExpressions(comments: CommentDescription[], esTreeAst: ESTree.Program) {
-	checkCommentsBeforeNode(<any>esTreeAst, {
+	handlePureAnnotationsOfNode(<any>esTreeAst, {
 		commentNodes: comments.filter(isPureComment),
-		commentIndex: 0,
-		handleNode: markPureNode
+		commentIndex: 0
 	});
 }
