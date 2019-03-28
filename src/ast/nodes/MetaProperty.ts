4b1cff18c866b4344e9279d730849a72cf66f1d2
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2019-03-28 13:23:11 +0100

    Merge compact and non-compact import.meta.url mechanisms

--- src/ast/nodes/MetaProperty.ts
+++ src/ast/nodes/MetaProperty.ts
@@ -7,48 +7,33 @@ import MemberExpression from './MemberExpression';
 import * as NodeType from './NodeType';
 import { NodeBase } from './shared/Node';
 
-// TODO Lukas unite compact and non-compact similar to relative mechanism
+// TODO Lukas extract more common elements from mechanisms
 // TODO Lukas reference absolute mechanism in relative mechanism
 
-const globalImportMetaUrlMechanism = `(typeof document !== 'undefined' ? document.currentScript && document.currentScript.src || document.baseURI : new (typeof URL !== 'undefined' ? URL : require('ur'+'l').URL)('file:' + __filename).href)`;
-const importMetaUrlMechanisms: Record<string, string> = {
-	amd: `new URL((typeof process !== 'undefined' && process.versions && process.versions.node ? 'file:' : '') + module.uri).href`,
-	cjs: `new (typeof URL !== 'undefined' ? URL : require('ur'+'l').URL)((process.browser ? '' : 'file:') + __filename, process.browser && document.baseURI).href`,
+const globalImportMetaUrlMechanism = (_: string) =>
+	`(typeof document${_}!==${_}'undefined'${_}?${_}document.currentScript${_}&&${_}document.currentScript.src${_}||${_}document.baseURI${_}:${_}new${_}(typeof URL${_}!==${_}'undefined'${_}?${_}URL${_}:${_}require('ur'+'l').URL)('file:'${_}+${_}__filename).href)`;
+
+const importMetaUrlMechanisms: Record<string, (_: string) => string> = {
+	amd: (_: string) =>
+		`new URL((typeof process${_}!==${_}'undefined'${_}&&${_}process.versions${_}&&${_}process.versions.node${_}?${_}'file:'${_}:${_}'')${_}+${_}module.uri).href`,
+	cjs: (_: string) =>
+		`new${_}(typeof URL${_}!==${_}'undefined'${_}?${_}URL${_}:${_}require('ur'+'l').URL)((process.browser${_}?${_}''${_}:${_}'file:')${_}+${_}__filename,${_}process.browser${_}&&${_}document.baseURI).href`,
 	iife: globalImportMetaUrlMechanism,
 	umd: globalImportMetaUrlMechanism
 };
 
-const globalImportMetaUrlMechanismCompact = `(typeof document!=='undefined'?document.currentScript&&document.currentScript.src||document.baseURI:new(typeof URL!=='undefined'?URL:require('ur'+'l').URL)('file:'+__filename).href)`;
-const importMetaUrlMechanismsCompact: Record<string, string> = {
-	amd: `new URL((typeof process!=='undefined'&&process.versions&&process.versions.node?'file:':'')+module.uri).href`,
-	cjs: `new(typeof URL!=='undefined'?URL:require('ur'+'l').URL)((process.browser?'':'file:')+__filename,process.browser&&document.baseURI).href`,
-	iife: globalImportMetaUrlMechanismCompact,
-	umd: globalImportMetaUrlMechanismCompact
-};
-
-const globalRelUrlMechanism = (relPath: string, compact: boolean) => {
-	const _ = compact ? '' : ' ';
+const globalRelUrlMechanism = (relPath: string, _: string) => {
 	return `new${_}(typeof URL${_}!==${_}'undefined'${_}?${_}URL${_}:${_}require('ur'+'l').URL)((typeof document${_}!==${_}'undefined'${_}?${_}document.currentScript${_}&&${_}document.currentScript.src${_}||${_}document.baseURI${_}:${_}'file:'${_}+${_}__filename)${_}+${_}'/../${relPath}').href`;
 };
 
-const relUrlMechanisms: Record<string, (relPath: string, compact: boolean) => string> = {
-	amd: (relPath: string, compact: boolean) => {
-		const _ = compact ? '' : ' ';
-		return `new URL((typeof process${_}!==${_}'undefined'${_}&&${_}process.versions${_}&&${_}process.versions.node${_}?${_}'file:'${_}:${_}'')${_}+${_}module.uri${_}+${_}'/../${relPath}').href`;
-	},
-	cjs: (relPath: string, compact: boolean) => {
-		const _ = compact ? '' : ' ';
-		return `new${_}(typeof URL${_}!==${_}'undefined'${_}?${_}URL${_}:${_}require('ur'+'l').URL)((process.browser${_}?${_}''${_}:${_}'file:')${_}+${_}__dirname${_}+${_}'/${relPath}',${_}process.browser${_}&&${_}document.baseURI).href`;
-	},
-	es: (relPath: string, compact: boolean) => {
-		const _ = compact ? '' : ' ';
-		return `new URL('../${relPath}',${_}import.meta.url).href`;
-	},
+const relUrlMechanisms: Record<string, (relPath: string, _: string) => string> = {
+	amd: (relPath: string, _: string) =>
+		`new URL((typeof process${_}!==${_}'undefined'${_}&&${_}process.versions${_}&&${_}process.versions.node${_}?${_}'file:'${_}:${_}'')${_}+${_}module.uri${_}+${_}'/../${relPath}').href`,
+	cjs: (relPath: string, _: string) =>
+		`new${_}(typeof URL${_}!==${_}'undefined'${_}?${_}URL${_}:${_}require('ur'+'l').URL)((process.browser${_}?${_}''${_}:${_}'file:')${_}+${_}__dirname${_}+${_}'/${relPath}',${_}process.browser${_}&&${_}document.baseURI).href`,
+	es: (relPath: string, _: string) => `new URL('../${relPath}',${_}import.meta.url).href`,
 	iife: globalRelUrlMechanism,
-	system: (relPath: string, compact: boolean) => {
-		const _ = compact ? '' : ' ';
-		return `new URL('../${relPath}',${_}module.url).href`;
-	},
+	system: (relPath: string, _: string) => `new URL('../${relPath}',${_}module.url).href`,
 	umd: globalRelUrlMechanism
 };
 
@@ -77,8 +62,10 @@ export default class MetaProperty extends NodeBase {
 		format: string,
 		compact: boolean
 	): boolean {
+		// TODO Lukas why?
 		if (!this.rendered) return false;
 
+		const _ = compact ? '' : ' ';
 		if (this.parent instanceof MemberExpression === false) return false;
 
 		const parent = <MemberExpression>this.parent;
@@ -93,17 +80,16 @@ export default class MetaProperty extends NodeBase {
 		if (importMetaProperty.startsWith('ROLLUP_ASSET_URL_')) {
 			const assetFileName = this.context.getAssetFileName(importMetaProperty.substr(17));
 			const relPath = normalize(relative(dirname(chunkId), assetFileName));
-			code.overwrite(parent.start, parent.end, relUrlMechanisms[format](relPath, compact));
+			code.overwrite(parent.start, parent.end, relUrlMechanisms[format](relPath, _));
 			return true;
 		}
 
 		if (format === 'system') {
 			code.overwrite(this.meta.start, this.meta.end, 'module');
 		} else if (importMetaProperty === 'url') {
-			const importMetaUrlMechanism = (compact
-				? importMetaUrlMechanismsCompact
-				: importMetaUrlMechanisms)[format];
-			if (importMetaUrlMechanism) code.overwrite(parent.start, parent.end, importMetaUrlMechanism);
+			const importMetaUrlMechanism = importMetaUrlMechanisms[format];
+			if (importMetaUrlMechanism)
+				code.overwrite(parent.start, parent.end, importMetaUrlMechanism(_));
 			return true;
 		}
 
---
+++ test/form/samples/import-meta-url-compact/_config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	description: 'import.meta.url support in compact mode',
+	options: {
+		output: {
+			compact: true
+		}
+	}
+};
