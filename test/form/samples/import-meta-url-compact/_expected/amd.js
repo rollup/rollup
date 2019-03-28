ebcdb4d26f679f6b3ed6215442173cc2a8f5babe
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2019-03-28 13:23:11 +0100

    Merge compact and non-compact import.meta.url mechanisms

---
+++ test/form/samples/import-meta-url-compact/_expected/amd.js
@@ -0,0 +1,1 @@
+define(['module'],function(module){'use strict';console.log(new URL((typeof process!=='undefined'&&process.versions&&process.versions.node?'file:':'')+module.uri).href);});
---
+++ test/form/samples/import-meta-url-compact/_expected/umd.js
@@ -0,0 +1,1 @@
+(function(f){typeof define==='function'&&define.amd?define(f):f();}(function(){'use strict';console.log((typeof document!=='undefined'?document.currentScript&&document.currentScript.src||document.baseURI:new(typeof URL!=='undefined'?URL:require('ur'+'l').URL)('file:'+__filename).href));}));
