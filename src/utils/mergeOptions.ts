f659aaa35ed003c392ab54e5895bfd101cd64913
Guy Bedford <guybedford@gmail.com>
2019-03-29 18:09:11 +0200

    support Node-style format aliases

--- src/utils/mergeOptions.ts
+++ src/utils/mergeOptions.ts
@@ -229,7 +229,18 @@ function getOutputOptions(
 	command: GenericConfigObject = {}
 ): OutputOptions {
 	const getOption = createGetOption(config, command);
-	const format = getOption('format');
+	let format = getOption('format');
+
+	// Handle format aliases
+	switch (format) {
+		case 'esm':
+		case 'module':
+			format = 'es';
+			break;
+		case 'commonjs':
+			format = 'cjs';
+			break;
+	}
 
 	return {
 		amd: { ...config.amd, ...command.amd },
