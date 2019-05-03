5a5ccc8978c98a82a21245a33b394068d664d012
Lukas Taegert-Atkinson <lukastaegert@users.noreply.github.com>
2019-05-03 11:05:24 +0200

    Update dependencies (#2833)
    * Update dependencies
    
    * Transpile all dependencies
    
    * Only run tests against the bootstrap build on CI
    
    * Always do a test build before the bootstrap build
    
    * Remove --ignore-scripts for now
    
    * Add --ignore-scripts again
    
    * Also use the ci:test script on appveyor
    
    * Revert previous change

--- typings/declarations.d.ts
+++ typings/declarations.d.ts
@@ -7,7 +7,6 @@ declare module 'help.md' {
 
 // external libs
 declare module 'ansi-escapes';
-declare module 'pretty-ms';
 declare module 'pretty-bytes';
 declare module 'rollup-plugin-buble';
 declare module 'signal-exit';
