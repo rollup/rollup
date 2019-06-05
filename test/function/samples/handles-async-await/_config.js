f760d5864dbb0a839d693a78db16bfe49e6aa69e
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2019-06-05 15:25:41 +0200

    Move to form test

--- test/function/samples/handles-async-await/_config.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-
-module.exports = {
-	description: 'properly handles exporting async functions',
-	exports(exports) {
-		return exports.callback().then(response => assert.strictEqual(response, 42));
-	}
-};
