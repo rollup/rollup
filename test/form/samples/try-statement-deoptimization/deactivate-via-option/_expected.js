37b9e14e2a50eb2d1641b935842f93306142dc5c
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2019-06-10 07:05:30 +0200

    Properly include try statements for each pass when deoptimization is deactivated

--- test/form/samples/try-statement-deoptimization/deactivate-via-option/_expected.js
+++ test/form/samples/try-statement-deoptimization/deactivate-via-option/_expected.js
@@ -1,8 +1,17 @@
+function mutate(foo) {
+	foo.bar = 1;
+}
+
+const mutated = {};
+
 function test(callback) {
 	try {
 		callback();
+		mutate(mutated);
 	} catch {}
 }
 
 test(() => {
 });
+
+export { mutated };
