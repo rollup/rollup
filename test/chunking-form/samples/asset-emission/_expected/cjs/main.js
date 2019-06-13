2ccd084b611c3f3e633dc22f35d5b33a9473b418
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2019-06-13 16:45:22 +0200

    Fix URL resolution to work when the current script contains query parameters

--- test/chunking-form/samples/asset-emission/_expected/cjs/main.js
+++ test/chunking-form/samples/asset-emission/_expected/cjs/main.js
@@ -2,7 +2,7 @@
 
 var __chunk_1 = require('./nested/chunk.js');
 
-var logo = (typeof document === 'undefined' ? new (require('u' + 'rl').URL)('file:' + __dirname + '/assets/logo1-25253976.svg').href : new URL((document.currentScript && document.currentScript.src || document.baseURI) + '/../assets/logo1-25253976.svg').href);
+var logo = (typeof document === 'undefined' ? new (require('u' + 'rl').URL)('file:' + __dirname + '/assets/logo1-25253976.svg').href : new URL('assets/logo1-25253976.svg', document.currentScript && document.currentScript.src || document.baseURI).href);
 
 __chunk_1.showImage(logo);
 Promise.resolve(require('./nested/chunk2.js'));
--- test/chunking-form/samples/emit-chunk-worklet/_expected/cjs/main.js
+++ test/chunking-form/samples/emit-chunk-worklet/_expected/cjs/main.js
@@ -2,6 +2,6 @@
 
 var __chunk_1 = require('./chunks/chunk.js');
 
-CSS.paintWorklet.addModule((typeof document === 'undefined' ? new (require('u' + 'rl').URL)('file:' + __dirname + '/chunks/worklet.js').href : new URL((document.currentScript && document.currentScript.src || document.baseURI) + '/../chunks/worklet.js').href));
+CSS.paintWorklet.addModule((typeof document === 'undefined' ? new (require('u' + 'rl').URL)('file:' + __dirname + '/chunks/worklet.js').href : new URL('chunks/worklet.js', document.currentScript && document.currentScript.src || document.baseURI).href));
 
 document.body.innerHTML += `<h1 style="background-image: paint(vertical-lines);">color: ${__chunk_1.color}, size: ${__chunk_1.size}</h1>`;
