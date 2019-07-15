c559e0fb6a65de189f4d4e03ac18f9f45caafc31
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2019-07-15 18:35:19 +0200

    Remove solo test

--- test/cli/samples/allow-output-prefix/_config.js
+++ test/cli/samples/allow-output-prefix/_config.js
@@ -1,5 +1,4 @@
 module.exports = {
-	solo: true,
 	description: 'allows output options to be prefixed with "output."',
 	command: 'rollup main.js --output.format es --output.footer "console.log(\'Rollup!\')"'
 };
