d342951572495491f5548422974b2938674571a5
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2019-07-15 17:57:27 +0200

    Increase timeouts for CLI tests

--- test/cli/index.js
+++ test/cli/index.js
@@ -25,7 +25,7 @@ runTestSuiteWithSamples(
 
 				const command = 'node ' + path.resolve(__dirname, '../../bin') + path.sep + config.command;
 
-				const childProcess = exec(command, { timeout: 2000 }, (err, code, stderr) => {
+				const childProcess = exec(command, { timeout: 6000 }, (err, code, stderr) => {
 					if (err && !err.killed) {
 						if (config.error) {
 							const shouldContinue = config.error(err);
@@ -118,7 +118,7 @@ runTestSuiteWithSamples(
 					}
 				});
 			}
-		);
+		).timeout(10000);
 	},
 	() => process.chdir(cwd)
 );
