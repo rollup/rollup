b194d4a9774b63e20058fe5fb973980c0f54b100
Lukas Taegert-Atkinson <lukastaegert@users.noreply.github.com>
2019-07-16 07:11:50 +0200

    Include working coverage script (#2997)
    * Include working coverage script
    
    * Increase timeouts for CLI tests
    
    * Run coverage on CI
    
    * Fix typo
    
    * Remove solo test
    
    * Run codecov on CI
    
    * Simplify setup as it appears special instrumentation is not really necessary for nyc
    (and we could have had this much earlier...)

--- test/cli/index.js
+++ test/cli/index.js
@@ -25,7 +25,7 @@ runTestSuiteWithSamples(
 
 				const command = 'node ' + path.resolve(__dirname, '../../bin') + path.sep + config.command;
 
-				const childProcess = exec(command, { timeout: 2000 }, (err, code, stderr) => {
+				const childProcess = exec(command, { timeout: 25000 }, (err, code, stderr) => {
 					if (err && !err.killed) {
 						if (config.error) {
 							const shouldContinue = config.error(err);
@@ -118,7 +118,7 @@ runTestSuiteWithSamples(
 					}
 				});
 			}
-		);
+		).timeout(30000);
 	},
 	() => process.chdir(cwd)
 );
