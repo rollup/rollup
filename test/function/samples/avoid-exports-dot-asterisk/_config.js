5668b33fcdef0b80c461db7bea11dcc73c26e962
龙腾道 <LongTengDao@LongTengDao.com>
2019-08-06 12:05:31 +0800

    Update _config.js

--- test/function/samples/avoid-exports-dot-asterisk/_config.js
+++ test/function/samples/avoid-exports-dot-asterisk/_config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	description: 'avoid return or set module.exports to dot-asterisk style',
 	options: {
-		external: function () { return true; },
+		external: () => true,
 		output: {
 			format: 'cjs'
 		}
