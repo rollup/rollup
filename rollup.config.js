1d87f7a1f8901fb4ee0c25e2fa3ad9a42cf2f7a9
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2019-08-13 16:43:18 +0200

    Fix acorn import

--- rollup.config.js
+++ rollup.config.js
@@ -54,9 +54,9 @@ function addSheBang() {
 	};
 }
 
-const expectedAcornImport = "import acorn__default, { tokTypes, Parser } from 'acorn';";
+const expectedAcornImport = "import acorn__default, { Parser } from 'acorn';";
 const newAcornImport =
-	"import * as acorn__default from 'acorn';\nimport { tokTypes, Parser } from 'acorn';";
+	"import * as acorn__default from 'acorn';\nimport { Parser } from 'acorn';";
 
 // by default, rollup-plugin-commonjs will translate require statements as default imports
 // which can cause issues for secondary tools that use the ESM version of acorn
