7442d7e0c904e1246e4ef9777aed61e4db1a3cb3
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2019-08-26 17:13:56 +0200

    Improve coverage and replace some forEach loops

--- src/finalisers/shared/getExportBlock.ts
+++ src/finalisers/shared/getExportBlock.ts
@@ -36,7 +36,7 @@ export default function getExportBlock(
 		if (reexports && namedExportsMode) {
 			reexports.forEach(specifier => {
 				if (specifier.reexported === '*') {
-					if (!compact && exportBlock) exportBlock += '\n';
+					if (exportBlock) exportBlock += n;
 					if (specifier.needsLiveBinding) {
 						exportBlock +=
 							`Object.keys(${name}).forEach(function${_}(k)${_}{${n}` +
@@ -55,64 +55,61 @@ export default function getExportBlock(
 		}
 	});
 
-	dependencies.forEach(
-		({
-			name,
-			imports,
-			reexports,
-			isChunk,
-			namedExportsMode: depNamedExportsMode,
-			exportsNames
-		}) => {
-			if (reexports && namedExportsMode) {
-				reexports.forEach(specifier => {
-					if (specifier.imported === 'default' && !isChunk) {
-						if (exportBlock && !compact) exportBlock += '\n';
-						if (
-							exportsNames &&
-							(reexports.some(specifier =>
-								specifier.imported === 'default'
-									? specifier.reexported === 'default'
-									: specifier.imported !== '*'
-							) ||
-								(imports && imports.some(specifier => specifier.imported !== 'default')))
-						) {
-							exportBlock += `exports.${specifier.reexported}${_}=${_}${name}${
-								interop !== false ? '__default' : '.default'
-							};`;
-						} else {
-							exportBlock += `exports.${specifier.reexported}${_}=${_}${name};`;
-						}
-					} else if (specifier.imported !== '*') {
-						if (exportBlock && !compact) exportBlock += '\n';
-						const importName =
-							specifier.imported === 'default' && !depNamedExportsMode
-								? name
-								: `${name}.${specifier.imported}`;
-						exportBlock += specifier.needsLiveBinding
-							? `Object.defineProperty(exports,${_}'${specifier.reexported}',${_}{${n}` +
-							  `${t}enumerable:${_}true,${n}` +
-							  `${t}get:${_}function${_}()${_}{${n}` +
-							  `${t}${t}return ${importName};${n}${t}}${n}});`
-							: `exports.${specifier.reexported}${_}=${_}${importName};`;
-					} else if (specifier.reexported !== '*') {
-						if (exportBlock) exportBlock += n;
+	for (const {
+		name,
+		imports,
+		reexports,
+		isChunk,
+		namedExportsMode: depNamedExportsMode,
+		exportsNames
+	} of dependencies) {
+		if (reexports && namedExportsMode) {
+			for (const specifier of reexports) {
+				if (specifier.imported === 'default' && !isChunk) {
+					if (exportBlock) exportBlock += n;
+					if (
+						exportsNames &&
+						(reexports.some(specifier =>
+							specifier.imported === 'default'
+								? specifier.reexported === 'default'
+								: specifier.imported !== '*'
+						) ||
+							(imports && imports.some(specifier => specifier.imported !== 'default')))
+					) {
+						exportBlock += `exports.${specifier.reexported}${_}=${_}${name}${
+							interop !== false ? '__default' : '.default'
+						};`;
+					} else {
 						exportBlock += `exports.${specifier.reexported}${_}=${_}${name};`;
 					}
-				});
+				} else if (specifier.imported !== '*') {
+					if (exportBlock) exportBlock += n;
+					const importName =
+						specifier.imported === 'default' && !depNamedExportsMode
+							? name
+							: `${name}.${specifier.imported}`;
+					exportBlock += specifier.needsLiveBinding
+						? `Object.defineProperty(exports,${_}'${specifier.reexported}',${_}{${n}` +
+						  `${t}enumerable:${_}true,${n}` +
+						  `${t}get:${_}function${_}()${_}{${n}` +
+						  `${t}${t}return ${importName};${n}${t}}${n}});`
+						: `exports.${specifier.reexported}${_}=${_}${importName};`;
+				} else if (specifier.reexported !== '*') {
+					if (exportBlock) exportBlock += n;
+					exportBlock += `exports.${specifier.reexported}${_}=${_}${name};`;
+				}
 			}
 		}
-	);
+	}
 
-	exports.forEach(expt => {
+	for (const expt of exports) {
 		const lhs = `exports.${expt.exported}`;
 		const rhs = expt.local;
-		if (lhs === rhs) {
-			return;
+		if (lhs !== rhs) {
+			if (exportBlock) exportBlock += n;
+			exportBlock += `${lhs}${_}=${_}${rhs};`;
 		}
-		if (exportBlock && !compact) exportBlock += '\n';
-		exportBlock += `${lhs}${_}=${_}${rhs};`;
-	});
+	}
 
 	return exportBlock;
 }
