1d524f2c6781827a2fdcfc8987bd7a1b7d2205dd
Beknar Askarov <beknaraskarov@gmail.com>
2019-09-08 12:56:43 +0200

    fix: remove 'use strict'; from systemjs when strict=false (#3101)

---
+++ test/form/samples/strict-false/_expected/amd.js
@@ -0,0 +1,13 @@
+define(function () {
+
+    const localVariable = 'local';
+
+    try {
+        globalVariable = localVariable;
+    } catch (error) {
+        console.log('use strict; detected', error);
+
+        Function("g", "globalVariable = g")(localVariable);
+    }
+
+});
