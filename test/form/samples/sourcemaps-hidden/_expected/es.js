53229415d9e36e3570fb788285e17e6582fc1b8f
Rohit Mohan <rohitmohan96@gmail.com>
2019-09-18 08:28:14 +0530

    Added tests for hidden sourcemaps
    Signed-off-by: Rohit Mohan <rohitmohan96@gmail.com>

---
+++ test/form/samples/sourcemaps-hidden/_expected/es.js
@@ -0,0 +1,12 @@
+function foo () {
+	console.log( 'hello from foo.js' );
+}
+
+function bar () {
+	console.log( 'hello from bar.js' );
+}
+
+console.log( 'hello from main.js' );
+
+foo();
+bar();
---
+++ test/form/samples/sourcemaps-hidden/_expected/system.js
@@ -0,0 +1,21 @@
+System.register([], function () {
+	'use strict';
+	return {
+		execute: function () {
+
+			function foo () {
+				console.log( 'hello from foo.js' );
+			}
+
+			function bar () {
+				console.log( 'hello from bar.js' );
+			}
+
+			console.log( 'hello from main.js' );
+
+			foo();
+			bar();
+
+		}
+	};
+});
