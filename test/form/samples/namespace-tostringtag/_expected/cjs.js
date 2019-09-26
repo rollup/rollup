72852ef422cb992e1d2bca6ba08524ceb61f10b8
Kubilay Kahveci <mkahveci@bloomberg.net>
2019-09-26 15:37:38 +0100

    Update expected test fixtures

--- test/form/samples/namespace-tostringtag/_expected/cjs.js
+++ test/form/samples/namespace-tostringtag/_expected/cjs.js
@@ -2,14 +2,10 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-var self = {
+var self = /*#__PURE__*/Object.freeze({
+	[Symbol.toStringTag]: 'Module',
 	get p () { return p; }
-};
-if (typeof Symbol !== 'undefined' && Symbol.toStringTag)
-	Object.defineProperty(self, Symbol.toStringTag, { value: 'Module' });
-else
-	Object.defineProperty(self, 'toString', { value: function () { return '[object Module]' } });
-/*#__PURE__*/Object.freeze(self);
+});
 
 console.log(Object.keys(self));
 
--- test/form/samples/namespace-tostringtag/_expected/iife.js
+++ test/form/samples/namespace-tostringtag/_expected/iife.js
@@ -1,14 +1,10 @@
 var iife = (function (exports) {
 	'use strict';
 
-	var self = {
+	var self = /*#__PURE__*/Object.freeze({
+		[Symbol.toStringTag]: 'Module',
 		get p () { return p; }
-	};
-	if (typeof Symbol !== 'undefined' && Symbol.toStringTag)
-		Object.defineProperty(self, Symbol.toStringTag, { value: 'Module' });
-	else
-		Object.defineProperty(self, 'toString', { value: function () { return '[object Module]' } });
-	/*#__PURE__*/Object.freeze(self);
+	});
 
 	console.log(Object.keys(self));
 
