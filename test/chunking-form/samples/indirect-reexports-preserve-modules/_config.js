74e13e07a7b56c8effab5e298765d0678cdf1528
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2019-12-06 07:57:56 +0100

    Make sure that indirectly reexported modules also become chunk dependencies

---
+++ test/chunking-form/samples/indirect-reexports-preserve-modules/_config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	description: 'does not drop indirect reexports when preserving modules',
+	expectedWarnings: ['MIXED_EXPORTS'],
+	options: {
+		output: { name: 'bundle' },
+		preserveModules: true
+	}
+};
---
+++ test/chunking-form/samples/indirect-reexports-preserve-modules/_expected/amd/main.js
@@ -0,0 +1,10 @@
+define(['exports', './components/sub/index', './components/index'], function (exports, index, index$1) { 'use strict';
+
+
+
+	exports.foo = index.foo;
+	exports.baz = index$1.baz;
+
+	Object.defineProperty(exports, '__esModule', { value: true });
+
+});
