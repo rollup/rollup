2b56d7dee92718e31d3c44006866b9d6b3698dbe
Kari Ikonen <mr.kari.ikonen@gmail.com>
2019-12-07 22:25:51 +0200

    FIX support either NO_COLOR or FORCE_COLOR=0 to turn off color (#3272)
    * FIX support either NO_COLOR or FORCE_COLOR=0 to turn off color
    
    * Deactivate colors in CLI tests, test NO_COLOR option

--- test/cli/samples/code-splitting-named-inputs/_config.js
+++ test/cli/samples/code-splitting-named-inputs/_config.js
@@ -1,29 +1,20 @@
 const assert = require('assert');
 
-const COLOR = '\u001b[36m\u001b[1m';
-const STANDARD = '\u001b[22m\u001b[39m';
-
 module.exports = {
 	description: 'allows defining names via CLI',
 	command:
 		'rollup --entryFileNames [name]-[hash].js --input entry1=main1.js -i "Entry 2"="main 2.js" -i "main3.js"  -f es',
 	result(code) {
-		let color = '';
-		let standard = '';
-		if (code[1] === '\u001b') {
-			color = COLOR;
-			standard = STANDARD;
-		}
 		assert.equal(
 			code,
 			'\n' +
-				`${color}//→ entry1-b70571c1.js:${standard}\n` +
+				`//→ entry1-b70571c1.js:\n` +
 				"console.log('main1');\n" +
 				'\n' +
-				`${color}//→ Entry 2-cc781491.js:${standard}\n` +
+				`//→ Entry 2-cc781491.js:\n` +
 				"console.log('main2');\n" +
 				'\n' +
-				`${color}//→ main3-5e259623.js:${standard}\n` +
+				`//→ main3-5e259623.js:\n` +
 				"console.log('main3');\n"
 		);
 	}
--- test/cli/samples/warn-circular/_config.js
+++ test/cli/samples/warn-circular/_config.js
@@ -3,6 +3,7 @@ const { assertStderrIncludes } = require('../../../utils.js');
 module.exports = {
 	description: 'warns for circular dependencies',
 	command: 'rollup -c',
-	stderr: stderr =>
-		assertStderrIncludes(stderr, '(!) Circular dependency\n' + 'main.js -> dep.js -> main.js\n')
+	stderr(stderr) {
+		assertStderrIncludes(stderr, '(!) Circular dependency\nmain.js -> dep.js -> main.js\n');
+	}
 };
