85eae4f682998be28ede20bf6c0aba273dce1aa3
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2020-01-05 17:45:00 +0100

    fix systemjs

--- test/chunking-form/samples/mixed-synthetic-named-exports/_expected/system/main.js
+++ test/chunking-form/samples/mixed-synthetic-named-exports/_expected/system/main.js
@@ -1,4 +1,4 @@
-System.register([], function () {
+System.register([], function (exports) {
   'use strict';
   return {
     execute: function () {
@@ -7,7 +7,7 @@ System.register([], function () {
         fn: 42,
         hello: 'hola'
       };
-      const foo = 100;
+      const foo = exports('foo', 100);
 
       var ns = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.assign({
         __proto__: null,
@@ -18,6 +18,8 @@ System.register([], function () {
       console.log(d.fn);
       console.log(foo);
       console.log(ns);
+
+      exports('fn', d.fn);
 
     }
   };
