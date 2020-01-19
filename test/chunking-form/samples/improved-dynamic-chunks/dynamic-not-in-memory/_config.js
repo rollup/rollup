5230071cbb5aaefa46805322414eb3e3ede2564f
Tom Jenkinson <tjenkinson@users.noreply.github.com>
2020-01-19 11:34:48 +0000

    fix test description

--- test/chunking-form/samples/improved-dynamic-chunks/dynamic-not-in-memory/_config.js
+++ test/chunking-form/samples/improved-dynamic-chunks/dynamic-not-in-memory/_config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	description: 'keeps chunks separate when in memory for all dynamic imports',
+	description: 'keeps chunks separate when not in memory for all dynamic imports',
 	options: {
 		input: ['main1', 'main2', 'main3']
 	}
