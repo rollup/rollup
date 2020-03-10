a96f3445e9429b47fd369d6b30816cbae4e3f305
Kamran Ayub <kamran.ayub@gmail.com>
2020-03-10 09:53:16 -0500

    Update expected outputs

--- test/chunking-form/samples/preserve-modules-nested-barrel-es-export/_expected/cjs/main.js
+++ test/chunking-form/samples/preserve-modules-nested-barrel-es-export/_expected/cjs/main.js
@@ -5,5 +5,7 @@ Object.defineProperty(exports, '__esModule', { value: true });
 var index = require('./module-a/v1/index.js');
 var index$1 = require('./module-b/v1/index.js');
 
+
+
 exports.ModuleA_V1 = index;
 exports.ModuleB_V1 = index$1;
