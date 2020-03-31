dee436e7059e95134cb15c29d5b02d3ab805c0b0
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2020-03-31 22:08:27 +0200

    Fix tests

--- test/sourcemaps/samples/existing-external-sourcemaps-combined/_config.js
+++ test/sourcemaps/samples/existing-external-sourcemaps-combined/_config.js
@@ -1,7 +1,6 @@
 const assert = require('assert');
 
 module.exports = {
-	solo: true,
 	description: 'removes sourcemap comments',
 	async test(code) {
 		assert.strictEqual(code.indexOf('sourceMappingURL'), -1);
