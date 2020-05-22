6627dd3285012d553fdfa14fc97775290b90b3ab
Guy Bedford <guybedford@gmail.com>
2020-05-22 00:36:58 -0700

    fixup test

--- test/form/samples/optional-chaining/_config.js
+++ test/form/samples/optional-chaining/_config.js
@@ -1,9 +1,3 @@
 module.exports = {
-	description: 'supports URL externals',
-	options: {
-		external: ['https://external.com/external.js'],
-		output: {
-			globals: { 'https://external.com/external.js': 'external' }
-		}
-	}
+	description: 'supports optional chaining'
 };
