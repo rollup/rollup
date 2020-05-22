186f31d0acd8f98e74e73c470e2ce82382138cfd
Guy Bedford <guybedford@gmail.com>
2020-05-21 23:05:11 -0600

    fix "cannot read exports of undefined" error (#3576)
    * fix "cannot read exports of undefined" error
    
    * add test
    
    Co-authored-by: Lukas Taegert-Atkinson <lukastaegert@users.noreply.github.com>

---
+++ test/function/samples/reexport-ns/_config.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+
+module.exports = {
+	description: 'external namespace reexport',
+	options: {
+		external: ['external'],
+		output: {
+			namespaceToStringTag: true
+		}
+	},
+	exports(exports) {
+		assert.strictEqual(typeof exports.maths, 'object');
+		assert.strictEqual(exports[Symbol.toStringTag], 'Module');
+		assert.strictEqual(exports.maths.external, true);
+	}
+};
---
+++ test/function/samples/reexport-ns/square.js
@@ -0,0 +1,2 @@
+import * as m from 'external';
+export default m;
