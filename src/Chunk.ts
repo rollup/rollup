50d9eda719e583cfa51c72acea3df5903a23edc0
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2020-06-02 08:37:03 +0200

    Slightly improve logic

--- src/Chunk.ts
+++ src/Chunk.ts
@@ -1014,8 +1014,8 @@ export default class Chunk {
 		if (stripJsExtension && relativePath.endsWith('.js')) {
 			relativePath = relativePath.slice(0, -3);
 		}
-		if (relativePath === '..') relativePath = '../../' + basename(targetPath);
-		else if (relativePath === '') relativePath = '../' + basename(targetPath);
+		if (relativePath === '..') return '../../' + basename(targetPath);
+		if (relativePath === '') return '../' + basename(targetPath);
 		return relativePath.startsWith('../') ? relativePath : './' + relativePath;
 	}
 
