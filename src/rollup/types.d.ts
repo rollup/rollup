f37f04bf62a929dc24d132093e42c0cccf5d5e0c
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2020-06-08 06:38:02 +0200

    Fix test and types, polish documentation

--- src/rollup/types.d.ts
+++ src/rollup/types.d.ts
@@ -477,6 +477,10 @@ export type InputOption = string | string[] | { [entryAlias: string]: string };
 export type ManualChunksOption = { [chunkAlias: string]: string[] } | GetManualChunk;
 export type ModuleSideEffectsOption = boolean | 'no-external' | string[] | HasModuleSideEffects;
 export type PreserveEntrySignaturesOption = false | 'strict' | 'allow-extension';
+export type SourcemapPathTransformOption = (
+	relativeSourcePath: string,
+	sourcemapPath: string
+) => string;
 
 export interface InputOptions {
 	acorn?: Object;
@@ -568,7 +572,7 @@ export interface OutputOptions {
 	sourcemap?: boolean | 'inline' | 'hidden';
 	sourcemapExcludeSources?: boolean;
 	sourcemapFile?: string;
-	sourcemapPathTransform?: (sourcePath: string) => string;
+	sourcemapPathTransform?: SourcemapPathTransformOption;
 	strict?: boolean;
 	systemNullSetters?: boolean;
 }
@@ -610,7 +614,7 @@ export interface NormalizedOutputOptions {
 	sourcemap: boolean | 'inline' | 'hidden';
 	sourcemapExcludeSources: boolean;
 	sourcemapFile: string | undefined;
-	sourcemapPathTransform: ((sourcePath: string) => string) | undefined;
+	sourcemapPathTransform: SourcemapPathTransformOption;
 	strict: boolean;
 	systemNullSetters: boolean;
 }
---
+++ test/sourcemaps/samples/transform-full-source-paths/_config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const assert = require('assert');
+
+module.exports = {
+	description: 'provides the full source map path when transforming source maps',
+	options: {
+		output: {
+			name: 'myModule',
+			file: path.join(__dirname, '_actual/bundle.js'),
+			sourcemapPathTransform: (relativeSourcePath, sourcemapPath) =>
+				path.resolve(path.dirname(sourcemapPath), relativeSourcePath)
+		}
+	},
+	test(code, map) {
+		assert.deepEqual(map.sources, [path.resolve(__dirname, 'main.js')]);
+	}
+};
