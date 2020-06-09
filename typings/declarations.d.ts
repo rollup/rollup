fa43e954c7d40169ae209544bdd495193dfa0478
Bogdan Chadkin <trysound@yandex.ru>
2020-06-09 15:24:26 +0300

    Support numeric separator
    Ref https://github.com/rollup/rollup/issues/3605

--- typings/declarations.d.ts
+++ typings/declarations.d.ts
@@ -25,3 +25,8 @@ declare module 'acorn-export-ns-from' {
 	const plugin: (BaseParser: typeof acorn.Parser) => typeof acorn.Parser;
 	export default plugin;
 }
+
+declare module 'acorn-numeric-separator' {
+	const plugin: (BaseParser: typeof acorn.Parser) => typeof acorn.Parser;
+	export default plugin;
+}
