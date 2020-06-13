710735ac81dd4b66a4347dc61870b581e7150891
Guy Bedford <guybedford@gmail.com>
2020-06-13 00:48:47 -0700

    match newlines too

--- src/utils/escapeId.ts
+++ src/utils/escapeId.ts
@@ -1,4 +1,4 @@
-const quoteRegEx = /'/g;
+const quoteNewlineRegEx = /\'|\r\n?|\n|\u2028|\u2029/g;
 export function escapeId(id: string) {
-	return id.replace(quoteRegEx, "\\'");
+	return id.replace(quoteNewlineRegEx, "\\'");
 }
