07a07717b3c471119332b857e1202a0000003476
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2020-08-15 06:17:36 +0200

    Improve test

--- test/function/samples/simplified-nested-if/main.js
+++ test/function/samples/simplified-nested-if/main.js
@@ -1,22 +1,24 @@
+export let foo = 0;
+
 if (unknown)
-	for (var x = 1; x--; x > 0)
-		if (!unknown) console.log('effect');
+	for (var x = 1; x-- > 0; )
+		if (!unknown) foo++;
 		else 'Side effect free code to be dropped';
 else throw new Error("Shouldn't be reached");
 
 if (unknown)
-	for (var x = 1; x--; x > 0)
-		if ((console.log(), false)) console.log('effect');
+	for (var x = 1; x-- > 0; )
+		if (foo++, false) foo++;
 		else 'Side effect free code to be dropped';
 else throw new Error("Shouldn't be reached");
 
 if (unknown) {
-	for (var x = 1; x--; x > 0)
-		if (!unknown) console.log('effect');
+	for (var x = 1; x-- > 0; )
+		if (!unknown) foo++;
 		else 'Side effect free code to be dropped';
 } else throw new Error("Shouldn't be reached");
 
 if (unknown)
-	for (var x = 1; x--; x > 0)
-		if (!unknown) console.log('effect');
+	for (var x = 1; x-- > 0; )
+		if (!unknown) foo++;
 		else 'Side effect free code to be dropped';
