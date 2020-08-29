21fcec3b91c65738984fd0eb4bbdbb1ea2610a2c
Lukas Taegert-Atkinson <lukastaegert@users.noreply.github.com>
2020-08-29 08:33:12 +0200

    Also pass resolved ids to external if they use the object form (#3753)

---
+++ test/function/samples/external-resolved/main.js
@@ -0,0 +1,4 @@
+export {default as resolvedString} from 'resolve-string';
+export {default as resolvedExternal} from 'resolve-external';
+export {default as resolvedObject} from 'resolve-object';
+export {default as resolvedObjectExternal} from 'resolve-object-external';
