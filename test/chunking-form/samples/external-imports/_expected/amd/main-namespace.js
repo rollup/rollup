90327e51d7f511ca3f5549389957c0e3e905ccc2
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2020-09-18 06:23:10 +0200

    Slightly simplify namespace interop helpers

--- test/chunking-form/samples/external-imports/_expected/amd/main-namespace.js
+++ test/chunking-form/samples/external-imports/_expected/amd/main-namespace.js
@@ -1,24 +1,23 @@
 define(['external-all', 'external-default-namespace', 'external-named-namespace', 'external-namespace'], function (foo, quux, quux$1, bar) { 'use strict';
 
 	function _interopNamespace(e) {
-		if (e && e.__esModule) { return e; } else {
-			var n = Object.create(null);
-			if (e) {
-				Object.keys(e).forEach(function (k) {
-					if (k !== 'default') {
-						var d = Object.getOwnPropertyDescriptor(e, k);
-						Object.defineProperty(n, k, d.get ? d : {
-							enumerable: true,
-							get: function () {
-								return e[k];
-							}
-						});
-					}
-				});
-			}
-			n['default'] = e;
-			return Object.freeze(n);
+		if (e && e.__esModule) return e;
+		var n = Object.create(null);
+		if (e) {
+			Object.keys(e).forEach(function (k) {
+				if (k !== 'default') {
+					var d = Object.getOwnPropertyDescriptor(e, k);
+					Object.defineProperty(n, k, d.get ? d : {
+						enumerable: true,
+						get: function () {
+							return e[k];
+						}
+					});
+				}
+			});
 		}
+		n['default'] = e;
+		return Object.freeze(n);
 	}
 
 	var foo__namespace = /*#__PURE__*/_interopNamespace(foo);
--- test/chunking-form/samples/external-imports/_expected/cjs/main-namespace.js
+++ test/chunking-form/samples/external-imports/_expected/cjs/main-namespace.js
@@ -6,24 +6,23 @@ var quux$1 = require('external-named-namespace');
 var bar = require('external-namespace');
 
 function _interopNamespace(e) {
-	if (e && e.__esModule) { return e; } else {
-		var n = Object.create(null);
-		if (e) {
-			Object.keys(e).forEach(function (k) {
-				if (k !== 'default') {
-					var d = Object.getOwnPropertyDescriptor(e, k);
-					Object.defineProperty(n, k, d.get ? d : {
-						enumerable: true,
-						get: function () {
-							return e[k];
-						}
-					});
-				}
-			});
-		}
-		n['default'] = e;
-		return Object.freeze(n);
+	if (e && e.__esModule) return e;
+	var n = Object.create(null);
+	if (e) {
+		Object.keys(e).forEach(function (k) {
+			if (k !== 'default') {
+				var d = Object.getOwnPropertyDescriptor(e, k);
+				Object.defineProperty(n, k, d.get ? d : {
+					enumerable: true,
+					get: function () {
+						return e[k];
+					}
+				});
+			}
+		});
 	}
+	n['default'] = e;
+	return Object.freeze(n);
 }
 
 var foo__namespace = /*#__PURE__*/_interopNamespace(foo);
