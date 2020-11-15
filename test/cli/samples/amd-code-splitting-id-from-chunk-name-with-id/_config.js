881b932a6f8faf5ba23a2923e46b38fdfbd79089
Tom Jenkinson <tom@tjenkinson.me>
2020-11-15 15:52:07 +0000

    remove test from cli section

--- test/cli/samples/amd-code-splitting-id-from-chunk-name-with-id/_config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	description:
-		'sets AMD module ID for each chunk appended with chunk id when there is code splitting',
-	command: 'rollup -i main.js -f amd --amd.id "foo/[id]"'
-};
