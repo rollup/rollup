6e7116f1a7e78dc5ae18d427f6a1ed6c9009f579
intrnl <intrnl28+git@gmail.com>
2020-11-30 08:50:53 +0700

    always run closeBundle even on watch mode

--- src/rollup/rollup.ts
+++ src/rollup/rollup.ts
@@ -56,7 +56,7 @@ export async function rollupInternal(
 			err.watchFiles = watchFiles;
 		}
 		await graph.pluginDriver.hookParallel('buildEnd', [err]);
-		if (!watcher) await graph.pluginDriver.hookParallel('closeBundle', []);
+		await graph.pluginDriver.hookParallel('closeBundle', []);
 		throw err;
 	}
 
