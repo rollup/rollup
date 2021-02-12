808134262a50403d95d8da00e7b0275b853d4aa3
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2021-02-12 08:34:52 +0100

    Skip test on windows

--- test/cli/samples/validate/_config.js
+++ test/cli/samples/validate/_config.js
@@ -2,6 +2,7 @@ const { assertIncludes } = require('../../../utils.js');
 
 module.exports = {
 	description: 'use CLI --validate to test whether output is well formed',
+	skipIfWindows: true,
 	command: `rollup main.js --silent --outro 'console.log("end"); /*' -o _actual/out.js --validate`,
 	error: () => true,
 	stderr: stderr =>
