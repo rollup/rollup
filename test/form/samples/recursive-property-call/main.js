af34ea72c95b99151fedb3a8c2890633a1288525
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2021-03-28 07:57:57 +0200

    Improve coverage

--- test/form/samples/recursive-property-call/main.js
+++ test/form/samples/recursive-property-call/main.js
@@ -1,2 +1,9 @@
-var window = window;
-console.log(window.foo());
+var obj1 = obj1;
+console.log(obj1.foo());
+
+var obj2 = {foo: () => {}};
+var obj2 = {foo: log};
+obj2.foo();
+
+var obj3 = {obj: obj3}
+obj3.obj.obj.obj.obj.obj.obj.obj.obj.obj.foo()
