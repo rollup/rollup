8ed146284dfede208f6c9e68df842cda38a962ae
Ben Scott <ben.scott@shopify.com>
2021-05-12 11:43:28 -0700

    Add assetExtname replacement in entryFileNames
    This only has an effect when preserveModules is enaled.
    
    This allows consumers who want to customise the entry file name to
    easily implement the "only add an extension for non js/ts files" logic
    that rollup does if you don't specify entryFileNames.

--- src/Chunk.ts
+++ src/Chunk.ts
@@ -441,9 +441,7 @@ export default class Chunk {
 		if (isAbsolute(id)) {
 			const extension = extname(id);
 			const pattern = unsetOptions.has('entryFileNames')
-				? NON_ASSET_EXTENSIONS.includes(extension)
-					? '[name].js'
-					: '[name][extname].js'
+				? '[name][assetExtname].js'
 				: options.entryFileNames;
 			const currentDir = dirname(sanitizedId);
 			const fileName = renderNamePattern(
@@ -452,6 +450,7 @@ export default class Chunk {
 				{
 					ext: () => extension.substr(1),
 					extname: () => extension,
+					assetExtname: () => NON_ASSET_EXTENSIONS.includes(extension) ? '' : extension,
 					format: () => options.format as string,
 					name: () => this.getChunkName()
 				}
--- test/chunking-form/samples/preserve-modules-filename-pattern/_config.js
+++ test/chunking-form/samples/preserve-modules-filename-pattern/_config.js
@@ -1,10 +1,25 @@
+const path = require('path');
+
 module.exports = {
 	description: 'entryFileNames pattern supported in combination with preserveModules',
 	options: {
 		input: 'src/main.ts',
 		output: {
-			entryFileNames: 'entry-[name]-[format]-[ext][extname].js',
+			entryFileNames: 'entry-[name]-[format]-[ext][extname][assetExtname].js',
 			preserveModules: true
-		}
+		},
+		plugins: [
+			{
+				name: 'str-plugin',
+				transform(code, id) {
+					switch (path.extname(id)) {
+						case '.str':
+							return { code: `export default "${code.trim()}"` };
+						default:
+							return null;
+					}
+				}
+			}
+		]
 	}
 };
