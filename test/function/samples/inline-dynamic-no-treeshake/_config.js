27d560ed30438cbb1fae18d3fa131233eb744cf5
Lukas Taegert-Atkinson <lukastaegert@users.noreply.github.com>
2021-05-27 13:10:30 +0200

    Fix crash when using inlineDynamicImports with no-treeshake (#4104)

---
+++ test/function/samples/inline-dynamic-no-treeshake/_config.js
@@ -0,0 +1,29 @@
+const MAGIC_ENTRY = 'main';
+
+module.exports = {
+	description: 'handles inlining dynamic imports when treeshaking is disabled for modules (#4098)',
+	options: {
+		input: MAGIC_ENTRY,
+		output: {
+			inlineDynamicImports: true
+		},
+		plugins: [
+			{
+				name: 'magic-modules',
+				buildStart() {
+					this.emitFile({ type: 'chunk', id: './dep.js' });
+				},
+				async resolveId(source) {
+					if (source === MAGIC_ENTRY) {
+						return { id: source, moduleSideEffects: 'no-treeshake' };
+					}
+					return null;
+				},
+				async load(id) {
+					if (id !== MAGIC_ENTRY) return null;
+					return `import {foo} from './reexport.js';assert.strictEqual(foo, 1);`;
+				}
+			}
+		]
+	}
+};
