e40f83ebef9f2214c8f1b23d47351a566ddc164c
Masaharu TASHIRO <masatsr.kit@gmail.com>
2021-06-08 07:13:32 +0900

    fix: export `rollup.defineConfig` at Node.js runtime

--- src/node-entry.ts
+++ src/node-entry.ts
@@ -1,3 +1,3 @@
-export { default as rollup } from './rollup/rollup';
+export { default as rollup, defineConfig } from './rollup/rollup';
 export { default as watch } from './watch/watch-proxy';
 export { version as VERSION } from 'package.json';
