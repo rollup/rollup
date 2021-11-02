b2a5fd02744d18ce902c5d7075be42b61b520522
Daniel Roe <daniel@roe.dev>
2021-11-02 13:16:49 +0000

    fix: correct lint errors

--- src/utils/sanitizeFileName.ts
+++ src/utils/sanitizeFileName.ts
@@ -1,11 +1,12 @@
 // https://datatracker.ietf.org/doc/html/rfc2396
-const INVALID_CHAR_RE = /[?*:\x00-\x1f\x7f<>#"{}|\^[\]`]/g
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_RE = /[?*:\x00-\x1f\x7f<>#"{}|\\^[\]`]/g;
 
-export function sanitizeFileName (name: string): string {
-	const match = /^[a-z]:/i.exec(name)
-	const driveLetter = match ? match[0] : ''
+export function sanitizeFileName(name: string): string {
+	const match = /^[a-z]:/i.exec(name);
+	const driveLetter = match ? match[0] : '';
 
 	// A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
 	// Otherwise, avoid them because they can refer to NTFS alternate data streams.
-	return driveLetter + name.substr(driveLetter.length).replace(INVALID_CHAR_RE, '_')
+	return driveLetter + name.substr(driveLetter.length).replace(INVALID_CHAR_RE, '_');
 }
