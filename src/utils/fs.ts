2cea50771d575ebd796bef03a4c7773121c6fa0a
Daniel Nalborczyk <dnalborczyk@gmail.com>
2021-12-22 16:53:56 -0500

    inline function

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -3,13 +3,8 @@ import { dirname } from './path';
 
 export const { lstat, readdir, readFile, realpath } = fs;
 
-async function mkdirpath(path: string): Promise<void> {
-	const dir = dirname(path);
-	await fs.mkdir(dir, { recursive: true });
-}
-
 export async function writeFile(dest: string, data: string | Uint8Array): Promise<void> {
-	await mkdirpath(dest);
-
+	const dir = dirname(dest);
+	await fs.mkdir(dir, { recursive: true });
 	await fs.writeFile(dest, data);
 }
