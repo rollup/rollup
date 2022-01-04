0ceee1df3e1ffaf1698554abfe682250eb94ff0d
kzc <kzc@users.noreply.github.com>
2022-01-04 02:16:02 -0500

    Detect unfulfilled async hook actions and report error on exit (#4320)
    * Better user experience with buggy plugins that previously caused
      rollup to exit abruptly without any output, error or warning,
      and a zero process exit code incorrectly indicating success.
    
    Co-authored-by: Lukas Taegert-Atkinson <lukastaegert@users.noreply.github.com>

--- build-plugins/replace-browser-modules.ts
+++ build-plugins/replace-browser-modules.ts
@@ -3,6 +3,7 @@ import { Plugin } from 'rollup';
 
 const ID_CRYPTO = path.resolve('src/utils/crypto');
 const ID_FS = path.resolve('src/utils/fs');
+const ID_HOOKACTIONS = path.resolve('src/utils/hookActions');
 const ID_PATH = path.resolve('src/utils/path');
 const ID_RESOLVEID = path.resolve('src/utils/resolveId');
 
@@ -17,6 +18,8 @@ export default function replaceBrowserModules(): Plugin {
 						return path.resolve('browser/crypto.ts');
 					case ID_FS:
 						return path.resolve('browser/fs.ts');
+					case ID_HOOKACTIONS:
+						return path.resolve('browser/hookActions.ts');
 					case ID_PATH:
 						return path.resolve('browser/path.ts');
 					case ID_RESOLVEID:
