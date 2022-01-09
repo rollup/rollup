614c0f60be2028ccf92ae5569b6c9ac004775fd0
Daniel Nalborczyk <dnalborczyk@gmail.com>
2022-01-09 12:06:21 -0500

    check for performance object in alternative environments

--- browser/performance.ts
+++ browser/performance.ts
@@ -1,1 +1,10 @@
-export default performance;
+const global =
+	typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : {};
+
+export default 'performance' in global
+	? performance
+	: {
+			now(): 0 {
+				return 0;
+			}
+	  };
