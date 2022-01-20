64c0d06bfa92f90e4b6474be63f79ba1ebcf3d0b
Daniel Nalborczyk <dnalborczyk@gmail.com>
2022-01-20 12:28:16 -0500

    Revert "Revert "fix: mark fsevents as external""
    This reverts commit 2ef6c5b3c567c273d9acbc0588f5de961206a69d.

--- rollup.config.ts
+++ rollup.config.ts
@@ -79,6 +79,7 @@ const nodePlugins = [
 export default (command: Record<string, unknown>): RollupOptions | RollupOptions[] => {
 	const { collectLicenses, writeLicense } = getLicenseHandler();
 	const commonJSBuild: RollupOptions = {
+		external: ['fsevents'],
 		input: {
 			'loadConfigFile.js': 'cli/run/loadConfigFile.ts',
 			'rollup.js': 'src/node-entry.ts'
