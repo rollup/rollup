5f4d04fd57ba411710b00b686cc815f09d27df01
dnalborczyk <dnalborczyk@gmail.com>
2022-02-09 01:06:31 -0500

    refactor: add @types/estree explicitly, fix dynamic type imports (#4390)
    * chore: add @types/estree explicitly
    
    * bump
    
    * fix type imports
    
    * refactor: simplify NodeWithFieldDefinition union type definition
    
    * order nit
    
    Co-authored-by: Lukas Taegert-Atkinson <lukastaegert@users.noreply.github.com>

--- typings/declarations.d.ts
+++ typings/declarations.d.ts
@@ -6,156 +6,93 @@ declare module 'help.md' {
 
 // external libs
 declare module 'rollup-plugin-string' {
-	export const string: import('rollup').PluginImpl;
+	import type { PluginImpl } from 'rollup';
+
+	export const string: PluginImpl;
 }
 
 declare module 'is-reference' {
+	import type * as estree from 'estree';
+
 	export default function is_reference(
 		node: NodeWithFieldDefinition,
 		parent: NodeWithFieldDefinition
 	): boolean;
+
 	export type Node =
-		| import('estree').Identifier
-		| import('estree').SimpleLiteral
-		| import('estree').RegExpLiteral
-		| import('estree').Program
-		| import('estree').FunctionDeclaration
-		| import('estree').FunctionExpression
-		| import('estree').ArrowFunctionExpression
-		| import('estree').SwitchCase
-		| import('estree').CatchClause
-		| import('estree').VariableDeclarator
-		| import('estree').ExpressionStatement
-		| import('estree').BlockStatement
-		| import('estree').EmptyStatement
-		| import('estree').DebuggerStatement
-		| import('estree').WithStatement
-		| import('estree').ReturnStatement
-		| import('estree').LabeledStatement
-		| import('estree').BreakStatement
-		| import('estree').ContinueStatement
-		| import('estree').IfStatement
-		| import('estree').SwitchStatement
-		| import('estree').ThrowStatement
-		| import('estree').TryStatement
-		| import('estree').WhileStatement
-		| import('estree').DoWhileStatement
-		| import('estree').ForStatement
-		| import('estree').ForInStatement
-		| import('estree').ForOfStatement
-		| import('estree').VariableDeclaration
-		| import('estree').ClassDeclaration
-		| import('estree').ThisExpression
-		| import('estree').ArrayExpression
-		| import('estree').ObjectExpression
-		| import('estree').YieldExpression
-		| import('estree').UnaryExpression
-		| import('estree').UpdateExpression
-		| import('estree').BinaryExpression
-		| import('estree').AssignmentExpression
-		| import('estree').LogicalExpression
-		| import('estree').MemberExpression
-		| import('estree').ConditionalExpression
-		| import('estree').SimpleCallExpression
-		| import('estree').NewExpression
-		| import('estree').SequenceExpression
-		| import('estree').TemplateLiteral
-		| import('estree').TaggedTemplateExpression
-		| import('estree').ClassExpression
-		| import('estree').MetaProperty
-		| import('estree').AwaitExpression
-		| import('estree').ImportExpression
-		| import('estree').ChainExpression
-		| import('estree').Property
-		| import('estree').AssignmentProperty
-		| import('estree').Super
-		| import('estree').TemplateElement
-		| import('estree').SpreadElement
-		| import('estree').ObjectPattern
-		| import('estree').ArrayPattern
-		| import('estree').RestElement
-		| import('estree').AssignmentPattern
-		| import('estree').ClassBody
-		| import('estree').MethodDefinition
-		| import('estree').ImportDeclaration
-		| import('estree').ExportNamedDeclaration
-		| import('estree').ExportDefaultDeclaration
-		| import('estree').ExportAllDeclaration
-		| import('estree').ImportSpecifier
-		| import('estree').ImportDefaultSpecifier
-		| import('estree').ImportNamespaceSpecifier
-		| import('estree').ExportSpecifier;
+		| estree.ArrayExpression
+		| estree.ArrayPattern
+		| estree.ArrowFunctionExpression
+		| estree.AssignmentExpression
+		| estree.AssignmentPattern
+		| estree.AssignmentProperty
+		| estree.AwaitExpression
+		| estree.BinaryExpression
+		| estree.BlockStatement
+		| estree.BreakStatement
+		| estree.CatchClause
+		| estree.ChainExpression
+		| estree.ClassBody
+		| estree.ClassDeclaration
+		| estree.ClassExpression
+		| estree.ConditionalExpression
+		| estree.ContinueStatement
+		| estree.DebuggerStatement
+		| estree.DoWhileStatement
+		| estree.EmptyStatement
+		| estree.ExportAllDeclaration
+		| estree.ExportDefaultDeclaration
+		| estree.ExportNamedDeclaration
+		| estree.ExportSpecifier
+		| estree.ExpressionStatement
+		| estree.ForInStatement
+		| estree.ForOfStatement
+		| estree.ForStatement
+		| estree.FunctionDeclaration
+		| estree.FunctionExpression
+		| estree.IfStatement
+		| estree.Identifier
+		| estree.ImportDeclaration
+		| estree.ImportDefaultSpecifier
+		| estree.ImportExpression
+		| estree.ImportNamespaceSpecifier
+		| estree.ImportSpecifier
+		| estree.LabeledStatement
+		| estree.LogicalExpression
+		| estree.MemberExpression
+		| estree.MetaProperty
+		| estree.MethodDefinition
+		| estree.NewExpression
+		| estree.ObjectExpression
+		| estree.ObjectPattern
+		| estree.Program
+		| estree.Property
+		| estree.RegExpLiteral
+		| estree.RestElement
+		| estree.ReturnStatement
+		| estree.SequenceExpression
+		| estree.SimpleCallExpression
+		| estree.SimpleLiteral
+		| estree.SpreadElement
+		| estree.Super
+		| estree.SwitchCase
+		| estree.SwitchStatement
+		| estree.TaggedTemplateExpression
+		| estree.TemplateElement
+		| estree.TemplateLiteral
+		| estree.ThisExpression
+		| estree.ThrowStatement
+		| estree.TryStatement
+		| estree.UnaryExpression
+		| estree.UpdateExpression
+		| estree.VariableDeclaration
+		| estree.VariableDeclarator
+		| estree.WhileStatement
+		| estree.WithStatement
+		| estree.YieldExpression;
+
 	export type NodeWithFieldDefinition =
-		| import('estree').Identifier
-		| import('estree').SimpleLiteral
-		| import('estree').RegExpLiteral
-		| import('estree').Program
-		| import('estree').FunctionDeclaration
-		| import('estree').FunctionExpression
-		| import('estree').ArrowFunctionExpression
-		| import('estree').SwitchCase
-		| import('estree').CatchClause
-		| import('estree').VariableDeclarator
-		| import('estree').ExpressionStatement
-		| import('estree').BlockStatement
-		| import('estree').EmptyStatement
-		| import('estree').DebuggerStatement
-		| import('estree').WithStatement
-		| import('estree').ReturnStatement
-		| import('estree').LabeledStatement
-		| import('estree').BreakStatement
-		| import('estree').ContinueStatement
-		| import('estree').IfStatement
-		| import('estree').SwitchStatement
-		| import('estree').ThrowStatement
-		| import('estree').TryStatement
-		| import('estree').WhileStatement
-		| import('estree').DoWhileStatement
-		| import('estree').ForStatement
-		| import('estree').ForInStatement
-		| import('estree').ForOfStatement
-		| import('estree').VariableDeclaration
-		| import('estree').ClassDeclaration
-		| import('estree').ThisExpression
-		| import('estree').ArrayExpression
-		| import('estree').ObjectExpression
-		| import('estree').YieldExpression
-		| import('estree').UnaryExpression
-		| import('estree').UpdateExpression
-		| import('estree').BinaryExpression
-		| import('estree').AssignmentExpression
-		| import('estree').LogicalExpression
-		| import('estree').MemberExpression
-		| import('estree').ConditionalExpression
-		| import('estree').SimpleCallExpression
-		| import('estree').NewExpression
-		| import('estree').SequenceExpression
-		| import('estree').TemplateLiteral
-		| import('estree').TaggedTemplateExpression
-		| import('estree').ClassExpression
-		| import('estree').MetaProperty
-		| import('estree').AwaitExpression
-		| import('estree').ImportExpression
-		| import('estree').ChainExpression
-		| import('estree').Property
-		| import('estree').AssignmentProperty
-		| import('estree').Super
-		| import('estree').TemplateElement
-		| import('estree').SpreadElement
-		| import('estree').ObjectPattern
-		| import('estree').ArrayPattern
-		| import('estree').RestElement
-		| import('estree').AssignmentPattern
-		| import('estree').ClassBody
-		| import('estree').MethodDefinition
-		| import('estree').ImportDeclaration
-		| import('estree').ExportNamedDeclaration
-		| import('estree').ExportDefaultDeclaration
-		| import('estree').ExportAllDeclaration
-		| import('estree').ImportSpecifier
-		| import('estree').ImportDefaultSpecifier
-		| import('estree').ImportNamespaceSpecifier
-		| import('estree').ExportSpecifier
+		| Node
 		| {
 				computed: boolean;
 				type: 'FieldDefinition';
