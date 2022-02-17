f67c404d5411fecfca16ba87c3336305c4c80a77
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2022-02-17 06:42:12 +0100

    Test errors in shouldTransform are handled correctly

---
+++ test/function/samples/plugin-error-should-transform/_config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const acorn = require('acorn');
+
+const code = 'export default 42;\n';
+const ID_MAIN = path.join(__dirname, 'main.js');
+
+module.exports = {
+	description: 'errors in shouldTransformCachedModule abort the build',
+	options: {
+		cache: {
+			modules: [
+				{
+					id: ID_MAIN,
+					ast: acorn.parse(code, {
+						ecmaVersion: 6,
+						sourceType: 'module'
+					}),
+					code,
+					dependencies: [],
+					dynamicDependencies: [],
+					originalCode: code,
+					resolvedIds: {},
+					sourcemapChain: [],
+					transformDependencies: []
+				}
+			]
+		},
+		plugins: [
+			{
+				name: 'test',
+				shouldTransformCachedModule() {
+					throw new Error('broken');
+				}
+			}
+		]
+	},
+	error: {
+		code: 'PLUGIN_ERROR',
+		hook: 'shouldTransformCachedModule',
+		message: 'broken',
+		plugin: 'test',
+		watchFiles: [ID_MAIN]
+	}
+};
