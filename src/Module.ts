f7434c3453969463d600ca56ed974fc930764888
Daniel Nalborczyk <dnalborczyk@gmail.com>
2022-02-18 20:23:28 -0500

    refactor: remove unnecessary array assignment

--- src/Module.ts
+++ src/Module.ts
@@ -464,8 +464,6 @@ export default class Module {
 		if (this.transitiveReexports) {
 			return this.transitiveReexports;
 		}
-		// to avoid infinite recursion when using circular `export * from X`
-		this.transitiveReexports = [];
 
 		const reexports = new Set(this.reexportDescriptions.keys());
 
