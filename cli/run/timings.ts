c3dbca62f55152afe829de205fb17c297058ef2e
Daniel Nalborczyk <dnalborczyk@gmail.com>
2022-02-19 20:35:01 -0500

    refactor: use object.entries over .keys

--- cli/run/timings.ts
+++ cli/run/timings.ts
@@ -3,10 +3,9 @@ import type { SerializedTimings } from '../../src/rollup/types';
 import { bold, underline } from '../../src/utils/colors';
 
 export function printTimings(timings: SerializedTimings): void {
-	Object.keys(timings).forEach(label => {
+	Object.entries(timings).forEach(([label, [time, memory, total]]) => {
 		const appliedColor =
 			label[0] === '#' ? (label[1] !== '#' ? underline : bold) : (text: string) => text;
-		const [time, memory, total] = timings[label];
 		const row = `${label}: ${time.toFixed(0)}ms, ${prettyBytes(memory)} / ${prettyBytes(total)}`;
 		console.info(appliedColor(row));
 	});
