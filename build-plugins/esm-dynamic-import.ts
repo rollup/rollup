b107be00afc39f639c804cc4923485c43736c4d5
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2022-02-27 06:50:10 +0100

    Fix plugin name

--- build-plugins/esm-dynamic-import.ts
+++ build-plugins/esm-dynamic-import.ts
@@ -1,6 +1,6 @@
 import type { Plugin } from 'rollup';
 
-export default function addBinShebangAndEsmImport(): Plugin {
+export default function esmDynamicImport(): Plugin {
 	let importFound = false;
 	return {
 		generateBundle() {
