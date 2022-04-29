5a47a1c6c48977215d18f16805049cc2b8623c2e
Harris Miller <harrismillerconsulting@gmail.com>
2022-04-29 17:16:04 -0600

    need guidance

--- test/cli/samples/watch/watch-event-hooks/_config.js
+++ test/cli/samples/watch/watch-event-hooks/_config.js
@@ -2,6 +2,23 @@ const { assertIncludes } = require('../../../../utils.js');
 
 module.exports = {
 	description: 'onStart event hoot shell command executes correctly',
-	command: 'rollup -cw --watch.onStart="echo onStart"',
-	stdout: stdout => assertIncludes(stdout, 'onStart')
+	command:
+		'rollup -cw --watch --watch.onStart "echo start" --watch.onBundleStart "echo bundleStart" --watch.onBundleEnd "echo bundleEnd" --watch.onEnd "echo onEnd" --watch.onError "echo onError"',
+	stderr(stderr) {
+		assertIncludes(
+			stderr,
+			`watch.onStart $ echo start
+start
+bundles main.js → _actual...
+watch.onBundleStart $ echo bundleStart
+bundleStart
+created _actual in 16ms
+watch.onBundleEnd $ echo bundleEnd
+bundleEnd
+watch.onEnd $ echo onEnd
+onEnd
+`
+		);
+		// assert.strictEqual(stderr.slice(0, 12), `${CLEAR_SCREEN}${UNDERLINE}rollup`);
+	}
 };
