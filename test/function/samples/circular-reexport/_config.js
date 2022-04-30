2bbdc8e136d68a1512984ccd025f69bb78135f3b
Lukas Taegert-Atkinson <lukastaegert@users.noreply.github.com>
2022-04-30 07:43:53 +0200

    Throw proper error when indirectly reexporting a recursive binding (#4472)
    * Throw proper error when indirectly reexporting a recursive binding
    
    * Fix unrelated typo

---
+++ test/function/samples/circular-reexport/_config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const ID_MAIN = path.join(__dirname, 'main.js');
+
+module.exports = {
+	description: 'throws proper error for circular reexports',
+	error: {
+		code: 'CIRCULAR_REEXPORT',
+		id: ID_MAIN,
+		message: '"foo" cannot be exported from main.js as it is a reexport that references itself.',
+		watchFiles: [ID_MAIN]
+	}
+};
