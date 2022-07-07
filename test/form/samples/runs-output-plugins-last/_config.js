e28dc261fa732e4cef0b7c96a1e3a6890bbbb8ab
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-07-07 06:53:15 -0400

    [v3.0] Run output plugins last (#3846)
    * fix: run output plugins last
    
    * Add test
    
    Co-authored-by: Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>

---
+++ test/form/samples/runs-output-plugins-last/_config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	description: 'runs output plugins last',
+	options: {
+		plugins: [
+			{
+				name: 'input',
+				renderChunk(code) {
+					return `/* input */\n${code}\n/* input */`;
+				}
+			}
+		],
+		output: {
+			plugins: [
+				{
+					name: 'output',
+					renderChunk(code) {
+						return `/* output */\n${code}\n/* output */`;
+					}
+				}
+			]
+		}
+	}
+};
