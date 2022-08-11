4ab4a86921f72d44b837b464dc2176d18c3cada9
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2022-08-11 06:51:43 +0200

    Try modified test

--- test/chunking-form/samples/preserve-modules-root/_config.js
+++ test/chunking-form/samples/preserve-modules-root/_config.js
@@ -2,11 +2,20 @@ const commonjs = require('@rollup/plugin-commonjs');
 const resolve = require('@rollup/plugin-node-resolve').default;
 
 module.exports = {
+	solo: true,
 	description: 'confirm preserveModulesRoot restructures src appropriately',
 	expectedWarnings: ['MIXED_EXPORTS'],
 	options: {
 		input: ['src/under-build.js', 'src/below/module.js'],
 		plugins: [
+			{
+				name: 'convert-slashes',
+				// This simulates converted slashes as used by e.g. Vite
+				async resolveId(source, importer, options) {
+					const resolved = await this.resolve(source, importer, { ...options, skipSelf: true });
+					return { ...resolved, id: resolved.id.replace(/\\/g, '/') };
+				}
+			},
 			resolve({
 				moduleDirectories: ['custom_modules']
 			}),
