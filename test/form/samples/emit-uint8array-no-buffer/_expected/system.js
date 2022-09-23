8015be75da6c0861998eb093fb040d0b17679b20
Lukas Taegert-Atkinson <lukastaegert@users.noreply.github.com>
2022-09-23 06:41:53 +0200

    [v3.0] Improve performance of asset emissions (#4644)
    BREAKING CHANGES: Typed array sources will no longer be deduplicated

--- test/form/samples/emit-uint8array-no-buffer/_expected/system.js
+++ test/form/samples/emit-uint8array-no-buffer/_expected/system.js
@@ -3,17 +3,13 @@ System.register([], (function (exports, module) {
 	return {
 		execute: (function () {
 
-			var asset1a = new URL('assets/asset-8e3dd2ea', module.meta.url).href;
+			var asset1 = new URL('assets/asset-8e3dd2ea', module.meta.url).href;
 
-			var asset1b = new URL('assets/asset-8e3dd2ea', module.meta.url).href;
+			var asset2 = new URL('assets/asset-75590fc1', module.meta.url).href;
 
-			var asset2a = new URL('assets/asset-75590fc1', module.meta.url).href;
+			var asset99 = new URL('assets/asset-60cc5dc9', module.meta.url).href;
 
-			var asset2b = new URL('assets/asset-75590fc1', module.meta.url).href;
-
-			var asset99a = new URL('assets/asset-60cc5dc9', module.meta.url).href;
-
-			console.log(asset1a, asset1b, asset2a, asset2b, asset99a);
+			console.log(asset1, asset2, asset99);
 
 		})
 	};
