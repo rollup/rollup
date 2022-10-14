8e729c9a6b2e85b8d3098114ada7b3151130c44d
三咲智子 <sxzz@sxzz.moe>
2022-10-14 13:18:03 +0800

    refactor: remove iife

--- src/watch/watch-proxy.ts
+++ src/watch/watch-proxy.ts
@@ -1,5 +1,5 @@
 import { handleError } from '../../cli/logging';
-import type { RollupOptions, RollupWatcher } from '../rollup/types';
+import type { MaybeArray, RollupOptions, RollupWatcher } from '../rollup/types';
 import { ensureArray } from '../utils/ensureArray';
 import { error, errorInvalidOption } from '../utils/error';
 import { mergeOptions } from '../utils/options/mergeOptions';
@@ -9,24 +9,26 @@ import { loadFsEvents } from './fsevents-importer';
 export default function watch(configs: RollupOptions[] | RollupOptions): RollupWatcher {
 	const emitter = new WatchEmitter() as RollupWatcher;
 
-	(async () => {
-		const optionsList = await Promise.all(ensureArray(configs).map(config => mergeOptions(config)));
-		const watchOptionsList = optionsList.filter(config => config.watch !== false);
-		if (watchOptionsList.length === 0) {
-			return error(
-				errorInvalidOption(
-					'watch',
-					'watch',
-					'there must be at least one config where "watch" is not set to "false"'
-				)
-			);
-		}
-		await loadFsEvents();
-		const { Watcher } = await import('./watch');
-		new Watcher(watchOptionsList, emitter);
-	})().catch(error => {
+	watchInternal(configs, emitter).catch(error => {
 		handleError(error);
 	});
 
 	return emitter;
+}
+
+async function watchInternal(configs: MaybeArray<RollupOptions>, emitter: RollupWatcher) {
+	const optionsList = await Promise.all(ensureArray(configs).map(config => mergeOptions(config)));
+	const watchOptionsList = optionsList.filter(config => config.watch !== false);
+	if (watchOptionsList.length === 0) {
+		return error(
+			errorInvalidOption(
+				'watch',
+				'watch',
+				'there must be at least one config where "watch" is not set to "false"'
+			)
+		);
+	}
+	await loadFsEvents();
+	const { Watcher } = await import('./watch');
+	new Watcher(watchOptionsList, emitter);
 }
