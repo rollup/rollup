1a89a4f26d7912620b9f5450787479652513095a
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2022-11-01 06:19:44 +0100

    Improve coverage

--- test/function/samples/class-name-conflict2/expression.js
+++ test/function/samples/class-name-conflict2/expression.js
@@ -1,9 +1,12 @@
 import { bar as bar$ } from './bar.js';
 
 {
-  let bar = class extends bar$ {
-  };
+	let bar = class extends bar$ {
+		static test() {
+			assert.ok(bar.base);
+		}
+	};
 
-  assert.strictEqual(bar.name, 'bar');
-  assert.ok(bar.base);
+	assert.strictEqual(bar.name, 'bar');
+	bar.test();
 }
