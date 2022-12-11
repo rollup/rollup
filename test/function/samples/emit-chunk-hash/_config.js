a0d4d69092484a0879377e16425b737ec541ae55
Lukas Taegert-Atkinson <lukastaegert@users.noreply.github.com>
2022-12-11 16:22:32 +0100

    provide hashed file name when using this.getFileName in generateBundle (#4747)

---
+++ test/function/samples/emit-chunk-hash/_config.js
@@ -0,0 +1,20 @@
+const assert = require('node:assert');
+let referenceId;
+
+module.exports = {
+	description: 'gives access to the hashed filed name via this.getFileName in generateBundle',
+	options: {
+		input: 'main',
+		output: {
+			chunkFileNames: '[name]-[hash].js'
+		},
+		plugins: {
+			buildStart() {
+				referenceId = this.emitFile({ type: 'chunk', id: 'emitted' });
+			},
+			generateBundle() {
+				assert.strictEqual(this.getFileName(referenceId), 'emitted-38bdd9b2.js');
+			}
+		}
+	}
+};
