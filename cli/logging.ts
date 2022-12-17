554ba9f0840bf59de2dc2e98fb6bdd1b293a3413
Shinigami <chrissi92@hotmail.de>
2022-12-17 06:36:26 +0100

    fix: RollupLog cause allow unknown (#4757)
    * fix: RollupLog cause allow unknown
    
    * fix access to cause.name
    
    Co-authored-by: Lukas Taegert-Atkinson <lukastaegert@users.noreply.github.com>

--- cli/logging.ts
+++ cli/logging.ts
@@ -8,7 +8,7 @@ export const stderr = (...parameters: readonly unknown[]) =>
 	process.stderr.write(`${parameters.join('')}\n`);
 
 export function handleError(error: RollupError, recover = false): void {
-	const name = error.name || error.cause?.name;
+	const name = error.name || (error.cause as Error)?.name;
 	const nameSection = name ? `${name}: ` : '';
 	const pluginSection = error.plugin ? `(plugin ${error.plugin}) ` : '';
 	const message = `${pluginSection}${nameSection}${error.message}`;
