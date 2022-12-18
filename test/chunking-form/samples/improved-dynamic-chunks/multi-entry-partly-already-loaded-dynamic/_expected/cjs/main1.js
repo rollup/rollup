5c3ae7a6c6af8c8c7feeb6181195d67c10581f59
TrickyPi <530257315@qq.com>
2022-12-18 22:28:03 +0800

    test: regression amd and cjs test

--- test/chunking-form/samples/improved-dynamic-chunks/multi-entry-partly-already-loaded-dynamic/_expected/cjs/main1.js
+++ test/chunking-form/samples/improved-dynamic-chunks/multi-entry-partly-already-loaded-dynamic/_expected/cjs/main1.js
@@ -7,5 +7,5 @@ const value1 = 'shared1';
 console.log('main1', value1, dep2.value2);
 Promise.resolve().then(function () { return require('./generated-dynamic1.js'); });
 
-exports.value2 = dep2.value2;
 exports.value1 = value1;
+exports.value2 = dep2.value2;
