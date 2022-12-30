5e8141b8a1f58ab9117209eadbda75bd27b553b2
Daniel Nalborczyk <dnalborczyk@gmail.com>
2022-12-30 17:52:11 -0500

    Use Set and builtin-modules package

--- src/finalisers/shared/warnOnBuiltins.ts
+++ src/finalisers/shared/warnOnBuiltins.ts
@@ -1,30 +1,22 @@
+import builtinModules from 'builtin-modules/static';
 import type { ChunkDependency } from '../../Chunk';
 import type { RollupWarning } from '../../rollup/types';
 import { errorMissingNodeBuiltins } from '../../utils/error';
 
-const builtins = {
-	assert: 1,
-	buffer: 1,
-	console: 1,
-	constants: 1,
-	domain: 1,
-	events: 1,
-	http: 1,
-	https: 1,
-	os: 1,
-	path: 1,
-	process: 1,
-	punycode: 1,
-	querystring: 1,
-	stream: 1,
-	string_decoder: 1,
-	timers: 1,
-	tty: 1,
-	url: 1,
-	util: 1,
-	vm: 1,
-	zlib: 1
-};
+const nodeBuiltins: ReadonlySet<string> = new Set([
+	...builtinModules,
+	'assert/strict',
+	'dns/promises',
+	'fs/promises',
+	'path/posix',
+	'path/win32',
+	'readline/promises',
+	'stream/consumers',
+	'stream/promises',
+	'stream/web',
+	'timers/promises',
+	'util/types'
+]);
 
 export default function warnOnBuiltins(
 	warn: (warning: RollupWarning) => void,
@@ -32,7 +24,7 @@ export default function warnOnBuiltins(
 ): void {
 	const externalBuiltins = dependencies
 		.map(({ importPath }) => importPath)
-		.filter(importPath => importPath in builtins || importPath.startsWith('node:'));
+		.filter(importPath => nodeBuiltins.has(importPath) || importPath.startsWith('node:'));
 
 	if (externalBuiltins.length === 0) return;
 
