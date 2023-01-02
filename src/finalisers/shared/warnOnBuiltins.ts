023ed0f01f2d2211962f042ea9290cda1e5734e2
dnalborczyk <dnalborczyk@gmail.com>
2023-01-02 04:23:47 -0500

    Use Set and builtin-modules package (#4781)
    * Use Set and builtin-modules package
    
    * Ad comment
    
    * Make parameter readonly
    
    Co-authored-by: Lukas Taegert-Atkinson <lukastaegert@users.noreply.github.com>

--- src/finalisers/shared/warnOnBuiltins.ts
+++ src/finalisers/shared/warnOnBuiltins.ts
@@ -1,38 +1,32 @@
+import builtinModules from 'builtin-modules/static';
 import type { ChunkDependency } from '../../Chunk';
 import type { RollupWarning } from '../../rollup/types';
 import { errorMissingNodeBuiltins } from '../../utils/error';
 
-const builtins = {
-	assert: 1,
-	buffer: 1,
-	console: 1,
-	constants: 1,
-	domain: 1,
-	events: 1,
-	http: 1,
-	https: 1,
-	os: 1,
-	path: 1,
-	process: 1,
-	punycode: 1,
-	querystring: 1,
-	stream: 1,
-	string_decoder: 1,
-	timers: 1,
-	tty: 1,
-	url: 1,
-	util: 1,
-	vm: 1,
-	zlib: 1
-};
+const nodeBuiltins: ReadonlySet<string> = new Set([
+	...builtinModules,
+	// TODO
+	// remove once builtin-modules includes PR: https://github.com/sindresorhus/builtin-modules/pull/17
+	'assert/strict',
+	'dns/promises',
+	'fs/promises',
+	'path/posix',
+	'path/win32',
+	'readline/promises',
+	'stream/consumers',
+	'stream/promises',
+	'stream/web',
+	'timers/promises',
+	'util/types'
+]);
 
 export default function warnOnBuiltins(
 	warn: (warning: RollupWarning) => void,
-	dependencies: ChunkDependency[]
+	dependencies: readonly ChunkDependency[]
 ): void {
 	const externalBuiltins = dependencies
 		.map(({ importPath }) => importPath)
-		.filter(importPath => importPath in builtins || importPath.startsWith('node:'));
+		.filter(importPath => nodeBuiltins.has(importPath) || importPath.startsWith('node:'));
 
 	if (externalBuiltins.length === 0) return;
 
