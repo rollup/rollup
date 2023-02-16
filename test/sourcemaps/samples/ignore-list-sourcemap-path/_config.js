1ba8d4e977830a143314631cd3485bb60492aa64
Benedikt Meurer <bmeurer@google.com>
2023-02-16 21:05:09 +0100

    Introduce `sourcemapIgnoreList` predicate. (#4848)
    This adds a new `sourcemapIgnoreList` config predicate that works
    similar to the existing `sourcemapPathTransform`, and can be provided to
    mark certain source files as ignore-listed when generating source maps.
    
    This is useful for libraries and frameworks that provide code that is
    not relevant to the debugging experience of the customer. Chrome
    DevTools already supports this new `x_google_ignoreList` field in the
    source maps[^1].
    
    Doc: https://goo.gle/devtools-ignoreList-adoption
    
    [^1]: https://developer.chrome.com/blog/devtools-better-angular-debugging

---
+++ test/sourcemaps/samples/ignore-list-sourcemap-path/_config.js
@@ -0,0 +1,21 @@
+const assert = require('node:assert');
+const path = require('node:path');
+
+module.exports = {
+	description: 'correctly passes source map path',
+	options: {
+		output: {
+			name: 'myModule',
+			file: path.resolve(__dirname, '_actual/output.js'),
+			sourcemapIgnoreList: (relativeSourcePath, sourcemapPath) =>
+				path.basename(sourcemapPath) === 'output.js.map' &&
+				path.basename(path.dirname(sourcemapPath)) === '_actual' &&
+				path.basename(relativeSourcePath) === 'main.js',
+			sourcemapPathTransform: relativeSourcePath => path.basename(relativeSourcePath)
+		}
+	},
+	test(code, map) {
+		assert.deepEqual(map.sources, ['main.js']);
+		assert.deepEqual(map.x_google_ignoreList, [0]);
+	}
+};
