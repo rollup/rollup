04797a223e3b35ae74be0fe2ab08e3d17a27f0f7
Daniel Nalborczyk <dnalborczyk@gmail.com>
2023-02-28 22:27:07 -0500

    Use Map

--- build-plugins/replace-browser-modules.ts
+++ build-plugins/replace-browser-modules.ts
@@ -4,15 +4,15 @@ import type { Plugin } from 'rollup';
 
 const resolve = (path: string) => fileURLToPath(new URL(`../${path}`, import.meta.url));
 
-export const resolutions = {
-	[resolve('src/utils/crypto')]: resolve('browser/src/crypto.ts'),
-	[resolve('src/utils/fs')]: resolve('browser/src/fs.ts'),
-	[resolve('src/utils/hookActions')]: resolve('browser/src/hookActions.ts'),
-	[resolve('src/utils/path')]: resolve('browser/src/path.ts'),
-	[resolve('src/utils/performance')]: resolve('browser/src/performance.ts'),
-	[resolve('src/utils/process')]: resolve('browser/src/process.ts'),
-	[resolve('src/utils/resolveId')]: resolve('browser/src/resolveId.ts')
-};
+export const resolutions: ReadonlyMap<string, string> = new Map([
+	[resolve('src/utils/crypto'), resolve('browser/src/crypto.ts')],
+	[resolve('src/utils/fs'), resolve('browser/src/fs.ts')],
+	[resolve('src/utils/hookActions'), resolve('browser/src/hookActions.ts')],
+	[resolve('src/utils/path'), resolve('browser/src/path.ts')],
+	[resolve('src/utils/performance'), resolve('browser/src/performance.ts')],
+	[resolve('src/utils/process'), resolve('browser/src/process.ts')],
+	[resolve('src/utils/resolveId'), resolve('browser/src/resolveId.ts')]
+]);
 
 export default function replaceBrowserModules(): Plugin {
 	return {
@@ -20,9 +20,8 @@ export default function replaceBrowserModules(): Plugin {
 		resolveId(source, importer) {
 			if (importer && source[0] === '.') {
 				const resolved = join(dirname(importer), source);
-				if (resolutions[resolved]) {
-					return resolutions[resolved];
-				}
+
+				return resolutions.get(resolved);
 			}
 		}
 	};
