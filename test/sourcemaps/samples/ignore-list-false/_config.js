3d9d107c519b6e8bc44e36b554576ba427c30d95
Benedikt Meurer <bmeurer@google.com>
2023-03-01 19:22:57 +0100

    feat: mark files in a `node_modules` as ignore-listed by default (#4877)
    Following up on the addition of the `sourcemapIgnoreList` output option,
    this adds a default which marks all files within a `node_modules` folder
    as ignore-listed. This way most projects using rollup will need no
    additional configuration regarding the ignore-listing and automatically
    benefit.
    
    This also allows to specify `false` for the `sourcemapIgnoreList` output
    option, which completely disables the generation of an ignore-list.
    
    Doc: https://goo.gle/devtools-ignoreList-adoption

---
+++ test/sourcemaps/samples/ignore-list-false/_config.js
@@ -0,0 +1,19 @@
+const assert = require('node:assert');
+const path = require('node:path');
+
+module.exports = {
+	description:
+		'accepts false for `sourcemapIgnoreList` to disable the default ignore-listing of node_modules',
+	options: {
+		output: {
+			name: 'myModule',
+			file: path.resolve(__dirname, '_actual/bundle.js'),
+			sourcemapPathTransform: sourcePath => path.basename(sourcePath),
+			sourcemapIgnoreList: false
+		}
+	},
+	test(code, map) {
+		assert.deepEqual(map.sources, ['lib.js', 'main.js']);
+		assert.strictEqual(map.x_google_ignoreList, undefined);
+	}
+};
