de255540b9070fef6a37fcb711f4b913cd2ac319
Lukas Taegert-Atkinson <lukastaegert@users.noreply.github.com>
2023-03-02 21:41:39 +0100

    Fix `npm run dev` (#4890)

--- build-plugins/replace-browser-modules.ts
+++ build-plugins/replace-browser-modules.ts
@@ -4,24 +4,33 @@ import type { Plugin } from 'rollup';
 
 const resolve = (path: string) => fileURLToPath(new URL(`../${path}`, import.meta.url));
 
-export const resolutions: ReadonlyMap<string, string> = new Map([
-	[resolve('src/utils/crypto'), resolve('browser/src/crypto.ts')],
-	[resolve('src/utils/fs'), resolve('browser/src/fs.ts')],
-	[resolve('src/utils/hookActions'), resolve('browser/src/hookActions.ts')],
-	[resolve('src/utils/path'), resolve('browser/src/path.ts')],
-	[resolve('src/utils/performance'), resolve('browser/src/performance.ts')],
-	[resolve('src/utils/process'), resolve('browser/src/process.ts')],
-	[resolve('src/utils/resolveId'), resolve('browser/src/resolveId.ts')]
-]);
+const REPLACED_MODULES = [
+	'crypto',
+	'fs',
+	'hookActions',
+	'path',
+	'performance',
+	'process',
+	'resolveId'
+];
+
+export const resolutions: ReadonlyMap<string, string> = new Map(
+	REPLACED_MODULES.flatMap(module => {
+		const originalId = resolve(`src/utils/${module}`);
+		const replacementId = resolve(`browser/src//${module}.ts`);
+		return [
+			[originalId, replacementId],
+			[`${originalId}.ts`, replacementId]
+		];
+	})
+);
 
 export default function replaceBrowserModules(): Plugin {
 	return {
 		name: 'replace-browser-modules',
 		resolveId(source, importer) {
 			if (importer && source[0] === '.') {
-				const resolved = join(dirname(importer), source);
-
-				return resolutions.get(resolved);
+				return resolutions.get(join(dirname(importer), source));
 			}
 		}
 	};
