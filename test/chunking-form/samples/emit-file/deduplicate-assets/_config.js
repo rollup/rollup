aaff781bb3fb31d32ad54657fe2f1eb7dfb63db6
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2023-03-23 06:36:14 +0100

    Only set asset names when finalizing

--- test/chunking-form/samples/emit-file/deduplicate-assets/_config.js
+++ test/chunking-form/samples/emit-file/deduplicate-assets/_config.js
@@ -1,3 +1,14 @@
+const assert = require('node:assert');
+let string1Id,
+	string2Id,
+	stringSameSourceId,
+	stringSameAsBufferId,
+	otherStringId,
+	bufferId,
+	bufferSameSourceId,
+	sameBufferAsStringId,
+	otherBufferId;
+
 module.exports = {
 	description: 'deduplicates asset that have the same source',
 	options: {
@@ -5,36 +16,44 @@ module.exports = {
 		plugins: {
 			buildStart() {
 				// emit 'string' source in a random order
-				this.emitFile({ type: 'asset', name: 'stringSameSource.txt', source: 'string' });
-				this.emitFile({ type: 'asset', name: 'string2.txt', source: 'string' });
-				this.emitFile({ type: 'asset', name: 'string1.txt', source: 'string' });
-				this.emitFile({
+				stringSameSourceId = this.emitFile({
 					type: 'asset',
-					name: 'sameStringAsBuffer.txt',
+					name: 'stringSameSource.txt',
+					source: 'string'
+				});
+				string2Id = this.emitFile({ type: 'asset', name: 'string2.txt', source: 'string' });
+				string1Id = this.emitFile({ type: 'asset', name: 'string1.txt', source: 'string' });
+				stringSameAsBufferId = this.emitFile({
+					type: 'asset',
+					name: 'stringSameAsBuffer.txt',
 					source: Buffer.from('string') // Test cross Buffer/string deduplication
 				});
 
 				// Different string source
-				this.emitFile({ type: 'asset', name: 'otherString.txt', source: 'otherString' });
+				otherStringId = this.emitFile({
+					type: 'asset',
+					name: 'otherString.txt',
+					source: 'otherString'
+				});
 
 				const bufferSource = () => Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]);
-				this.emitFile({
+				bufferId = this.emitFile({
 					type: 'asset',
 					name: 'buffer.txt',
 					source: bufferSource()
 				});
-				this.emitFile({
+				bufferSameSourceId = this.emitFile({
 					type: 'asset',
 					name: 'bufferSameSource.txt',
 					source: bufferSource()
 				});
-				this.emitFile({
+				sameBufferAsStringId = this.emitFile({
 					type: 'asset',
 					name: 'sameBufferAsString.txt',
 					source: bufferSource().toString() // Test cross Buffer/string deduplication
 				});
 				// Different buffer source
-				this.emitFile({
+				otherBufferId = this.emitFile({
 					type: 'asset',
 					name: 'otherBuffer.txt',
 					source: Buffer.from('otherBuffer')
@@ -48,6 +67,41 @@ module.exports = {
 					source: bufferSource()
 				});
 				return null;
+			},
+			generateBundle() {
+				assert.strictEqual(this.getFileName(string1Id), 'assets/string1-473287f8.txt', 'string1');
+				assert.strictEqual(this.getFileName(string2Id), 'assets/string1-473287f8.txt', 'string2');
+				assert.strictEqual(
+					this.getFileName(stringSameSourceId),
+					'assets/string1-473287f8.txt',
+					'stringSameSource'
+				);
+				assert.strictEqual(
+					this.getFileName(stringSameAsBufferId),
+					'assets/string1-473287f8.txt',
+					'stringSameAsBuffer'
+				);
+				assert.strictEqual(
+					this.getFileName(otherStringId),
+					'assets/otherString-e296c1ca.txt',
+					'otherString'
+				);
+				assert.strictEqual(this.getFileName(bufferId), 'assets/buffer-d0ca8c2a.txt', 'buffer');
+				assert.strictEqual(
+					this.getFileName(bufferSameSourceId),
+					'assets/buffer-d0ca8c2a.txt',
+					'bufferSameSource'
+				);
+				assert.strictEqual(
+					this.getFileName(sameBufferAsStringId),
+					'assets/buffer-d0ca8c2a.txt',
+					'sameBufferAsString'
+				);
+				assert.strictEqual(
+					this.getFileName(otherBufferId),
+					'assets/otherBuffer-e8d9b528.txt',
+					'otherBuffer'
+				);
 			}
 		}
 	}
