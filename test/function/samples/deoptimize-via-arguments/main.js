567a9c223e96e0a615e9a8c01173233c354ef7b8
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2023-04-28 22:03:58 +0200

    Ensure arguments are deoptimized when arguments variable is used

---
+++ test/function/samples/deoptimize-via-arguments/main.js
@@ -0,0 +1,17 @@
+function mutate(a) {
+	arguments[0].x = true;
+	arguments[1].x = true;
+}
+
+var obj1 = {
+	x: false
+};
+
+var obj2 = {
+	x: false
+};
+
+mutate(obj1, obj2);
+
+assert.ok(obj1.x ? true : false);
+assert.ok(obj2.x ? true : false);
