32767f43b67920440cd1fcb5eb4aaf619b5a1bf5
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2023-04-28 07:53:43 +0200

    Fix REPL in dev

--- build-plugins/replace-browser-modules.ts
+++ build-plugins/replace-browser-modules.ts
@@ -1,6 +1,6 @@
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import type { Plugin } from 'rollup';
+import type { Plugin } from 'vite';
 
 const resolve = (path: string) => fileURLToPath(new URL(`../${path}`, import.meta.url));
 
@@ -14,10 +14,10 @@ const REPLACED_MODULES = [
 	'resolveId'
 ];
 
-export const resolutions: ReadonlyMap<string, string> = new Map(
+const resolutions: ReadonlyMap<string, string> = new Map(
 	REPLACED_MODULES.flatMap(module => {
 		const originalId = resolve(`src/utils/${module}`);
-		const replacementId = resolve(`browser/src//${module}.ts`);
+		const replacementId = resolve(`browser/src/${module}.ts`);
 		return [
 			[originalId, replacementId],
 			[`${originalId}.ts`, replacementId]
@@ -27,11 +27,18 @@ export const resolutions: ReadonlyMap<string, string> = new Map(
 
 export default function replaceBrowserModules(): Plugin {
 	return {
+		apply: 'serve',
+		enforce: 'pre',
 		name: 'replace-browser-modules',
 		resolveId(source, importer) {
 			if (importer && source[0] === '.') {
 				return resolutions.get(join(dirname(importer), source));
 			}
+		},
+		transformIndexHtml(html) {
+			// Unfortunately, picomatch sneaks as a dedendency into the dev bundle.
+			// This fixes an error.
+			return html.replace('</head>', '<script>window.process={}</script></head>');
 		}
 	};
 }
