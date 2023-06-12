b1341bf9cd719670a670905e0308418b37621d70
Anthony Fu <anthonyfu117@hotmail.com>
2023-06-12 06:19:07 +0200

    fix: `__NO_SIDE_EFFECTS__` annotation for async function (#5031)
    * fix: `__NO_SIDE_EFFECTS__` annonation for async function
    
    * chore: update
    
    * chore: update

--- test/form/samples/no-side-effects-function-declaration/functions.js
+++ test/form/samples/no-side-effects-function-declaration/functions.js
@@ -51,6 +51,42 @@ export const fnF = (args) => {
   return args
 }
 
+// #__NO_SIDE_EFFECTS__
+export async function fnG(args) {
+  console.log(args)
+  return args
+}
+
+/**
+ * #__NO_SIDE_EFFECTS__
+ */ 
+export const fnH = async (args) => {
+  console.log(args)
+  return args
+}
+
+export const fnI = /*#__NO_SIDE_EFFECTS__*/ async (args) => {
+  console.log(args)
+  return args
+}
+
+
+/**
+ * #__NO_SIDE_EFFECTS__
+ */ 
+export function * fnJ(args) {
+  console.log(args)
+  return args
+}
+
+/**
+ * #__NO_SIDE_EFFECTS__
+ */ 
+export async function * fnK(args) {
+  console.log(args)
+  return args
+}
+
 /*#__NO_SIDE_EFFECTS__*/ 
 export default function fnDefault(args) {
   console.log(args)
--- test/form/samples/no-side-effects-function-declaration/main.js
+++ test/form/samples/no-side-effects-function-declaration/main.js
@@ -1,4 +1,4 @@
-import fnDefault, { fnPure, fnEffects, fnA, fnB, fnC, fnD, fnE, fnF, fnAlias, fnFromSub } from './functions'
+import fnDefault, { fnPure, fnEffects, fnA, fnB, fnC, fnD, fnE, fnF, fnI, fnG, fnJ, fnK, fnAlias, fnFromSub } from './functions'
 
 const pure = fnPure(1)
 const effects = fnEffects(2)
@@ -9,6 +9,10 @@ const c = fnC(3)
 const d = fnD(4)
 const e = fnE(5)
 const f = fnF(6)
+const g = fnG(7)
+const i = fnI(8)
+const j = fnJ(9)
+const k = fnK(10)
 
 const defaults = fnDefault(3)
 const alias = fnAlias(6)
