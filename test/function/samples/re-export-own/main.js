fac40fe90425a286898af50b6cdd8a8aa05bdc3d
龙腾道 <LongTengDao@LongTengDao.com>
2023-07-02 08:09:19 +0800

    Update main.js

--- test/function/samples/re-export-own/main.js
+++ test/function/samples/re-export-own/main.js
@@ -1,3 +1,3 @@
 import * as mod from './mod.js';
 
-assert.strictEqual(typeof Object.getOwnProperty(mod, 'name').get, 'function');
+assert.strictEqual(typeof Object.getOwnPropertyDescriptor(mod, 'name').get, 'function');
