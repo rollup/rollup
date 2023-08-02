3e6a17af73ecd4c02034cbcaa8e73df69c067add
TrickyPi <530257315@qq.com>
2023-08-02 16:20:01 +0800

    add more comments to silence the linter

--- browser/src/initWasm.ts
+++ browser/src/initWasm.ts
@@ -1,5 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 // eslint-disable-next-line import/no-unresolved
 import init from '../wasm/bindings_wasm';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 // eslint-disable-next-line import/no-unresolved
 import wasm from '../wasm/bindings_wasm_bg.wasm';
 
--- browser/src/native.ts
+++ browser/src/native.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 // eslint-disable-next-line import/no-unresolved
 import { parse } from '../wasm/bindings_wasm.js';
 
