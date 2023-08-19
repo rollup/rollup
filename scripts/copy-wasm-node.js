5c783b3984678c612a3ddad6b41c14f74d8e8103
TrickyPi <530257315@qq.com>
2023-08-19 23:13:26 +0800

    Add Node WASM files to Native packages for StackBlitz and similar use cases

---
+++ scripts/copy-wasm-node.js
@@ -0,0 +1,15 @@
+import fs from 'node:fs/promises';
+import { resolve } from 'node:path';
+
+const WASM_NODE_DIR_NAME = 'artifacts/wasm-node';
+const NATIVES_DIR_NAME = 'npm';
+
+async function copy() {
+	const directories = await fs.readdir(NATIVES_DIR_NAME);
+	for (const dir of directories) {
+		const target = resolve(NATIVES_DIR_NAME, dir, WASM_NODE_DIR_NAME);
+		fs.cp(WASM_NODE_DIR_NAME, target, { recursive: true });
+	}
+}
+
+copy();
