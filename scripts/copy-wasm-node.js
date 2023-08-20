7bf85c72b5d8be186c6c4878e655ab17b369fd70
TrickyPi <530257315@qq.com>
2023-08-20 15:17:11 +0800

    Fix copy-wasm-node.js

--- scripts/copy-wasm-node.js
+++ scripts/copy-wasm-node.js
@@ -1,14 +1,13 @@
 import fs from 'node:fs/promises';
 import { resolve } from 'node:path';
 
-const WASM_NODE_DIR_NAME = 'artifacts/bindings-wasm-node/wasm-node';
 const NATIVES_DIR_NAME = 'npm';
 
 async function copy() {
 	const directories = await fs.readdir(NATIVES_DIR_NAME);
 	for (const dir of directories) {
-		const target = resolve(NATIVES_DIR_NAME, dir, WASM_NODE_DIR_NAME);
-		fs.cp(WASM_NODE_DIR_NAME, target, { recursive: true });
+		const target = resolve(NATIVES_DIR_NAME, dir, 'wasm-node');
+		fs.cp('artifacts/bindings-wasm-node/wasm-node', target, { recursive: true });
 	}
 }
 
