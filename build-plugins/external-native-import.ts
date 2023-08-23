1e181739021411f69f388056000ba92eb653b551
TrickyPi <530257315@qq.com>
2023-08-23 11:32:21 +0800

    Fix entension for native import

--- build-plugins/external-native-import.ts
+++ build-plugins/external-native-import.ts
@@ -6,9 +6,10 @@ export function externalNativeImport(): Plugin {
 		async resolveId(id, importer) {
 			if (id.includes('/native')) {
 				const resolved = await this.resolve(id, importer!, { skipSelf: true });
+				const resolvedId = resolved!.id;
 				return {
 					external: 'relative',
-					id: `${resolved!.id}.js`
+					id: resolvedId.endsWith('.js') ? resolvedId : `${resolvedId}.js`
 				};
 			}
 		}
