d0a6abf866ce3c27fdcad76616d09b0b87763149
TrickyPi <530257315@qq.com>
2023-08-24 14:45:03 +0800

    Tweak getReadStringFunction

--- src/utils/getReadStringFunction.ts
+++ src/utils/getReadStringFunction.ts
@@ -1,13 +1,7 @@
 export default function getReadStringFunction(
 	astBuffer: Buffer | Uint8Array
 ): (start: number, length: number) => string {
-	let bufferConstructor = null;
-	try {
-		bufferConstructor = Buffer;
-	} catch {
-		/* regardless of error */
-	}
-	if (bufferConstructor && astBuffer instanceof bufferConstructor) {
+	if (typeof Buffer !== 'undefined' && astBuffer instanceof Buffer) {
 		return function readString(start, length) {
 			return astBuffer.toString('utf8', start, start + length);
 		};
