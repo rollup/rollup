accd66a4247dfdb4d7f2605b399d12d001e5a3ed
XiaoPi <530257315@qq.com>
2023-09-24 13:26:09 +0800

    [v4.0] feat: Do not watch files anymore if their content is returned by the load hook (#5150)
    * feat: Do not watch files anymore if their content is returned by the load hook
    
    * tweak test

--- test/browser/samples/missing-dependency-resolution/_config.js
+++ test/browser/samples/missing-dependency-resolution/_config.js
@@ -12,7 +12,6 @@ console.log(foo);`
 		code: 'NO_FS_IN_BROWSER',
 		message:
 			'Cannot access the file system (via "path.resolve") when using the browser build of Rollup. Make sure you supply a plugin with custom resolveId and load hooks to Rollup.',
-		url: 'https://rollupjs.org/plugin-development/#a-simple-example',
-		watchFiles: ['main']
+		url: 'https://rollupjs.org/plugin-development/#a-simple-example'
 	}
 });
--- test/function/samples/load-returns-string-or-null/_config.js
+++ test/function/samples/load-returns-string-or-null/_config.js
@@ -1,6 +1,3 @@
-const path = require('node:path');
-const ID_MAIN = path.join(__dirname, 'main.js');
-
 module.exports = defineTest({
 	description: 'throws error if load returns something wacky',
 	options: {
@@ -15,7 +12,6 @@ module.exports = defineTest({
 	},
 	error: {
 		code: 'BAD_LOADER',
-		watchFiles: [ID_MAIN],
 		message:
 			'Error loading "main.js": plugin load hook should return a string, a { code, map } object, or nothing/null.'
 	}
