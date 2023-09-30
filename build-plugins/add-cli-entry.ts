7a73ff22cfd9eb5f05f90ac095283ea9147733a5
TrickyPi <530257315@qq.com>
2023-09-30 20:15:52 +0800

    Remove the shebang insertion during cli build

--- build-plugins/add-cli-entry.ts
+++ build-plugins/add-cli-entry.ts
@@ -1,6 +1,5 @@
 import { chmod } from 'node:fs/promises';
 import { resolve } from 'node:path';
-import MagicString from 'magic-string';
 import type { Plugin } from 'rollup';
 
 const CLI_CHUNK = 'bin/rollup';
@@ -16,14 +15,6 @@ export default function addCliEntry(): Plugin {
 			});
 		},
 		name: 'add-cli-entry',
-		renderChunk(code, chunkInfo) {
-			if (chunkInfo.fileName === CLI_CHUNK) {
-				const magicString = new MagicString(code);
-				magicString.prepend('#!/usr/bin/env node\n\n');
-				return { code: magicString.toString(), map: magicString.generateMap({ hires: true }) };
-			}
-			return null;
-		},
 		writeBundle({ dir }) {
 			return chmod(resolve(dir!, CLI_CHUNK), '755');
 		}
