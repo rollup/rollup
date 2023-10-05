cade24fbf3390a31734301a1f803b24e34b6cedd
Lukas Taegert-Atkinson <lukastaegert@users.noreply.github.com>
2023-10-05 12:33:51 +0200

    [v4.0] Ensure we support new import attribute "with" syntax (#5168)
    * Update dependencies
    
    * Use new import attribute syntax in test files

--- test/form/samples/import-attributes/plugin-attributes-resolveid/main.js
+++ test/form/samples/import-attributes/plugin-attributes-resolveid/main.js
@@ -1,9 +1,9 @@
-import { a } from 'a' assert { type: 'a', extra: 'extra' };
-import * as b from 'b' assert { type: 'b' };
-export { c } from 'c' assert { type: 'c' };
-export * from 'd' assert { type: 'd' };
+import { a } from 'a' with { type: 'a', extra: 'extra' };
+import * as b from 'b' with { type: 'b' };
+export { c } from 'c' with { type: 'c' };
+export * from 'd' with { type: 'd' };
 import 'e';
 
 console.log(a, b, d);
-import('f', { assert: { type: 'f' } });
+import('f', { with: { type: 'f' } });
 import('g');
--- test/function/samples/import-assertions/warn-assertion-conflicts/_config.js
+++ test/function/samples/import-assertions/warn-assertion-conflicts/_config.js
@@ -11,11 +11,11 @@ module.exports = defineTest({
 			code: 'INCONSISTENT_IMPORT_ATTRIBUTES',
 			frame: `
 1: import './other.js';
-2: import 'external' assert { type: 'foo' };
-3: import 'external' assert { type: 'bar' };
+2: import 'external' with { type: 'foo' };
+3: import 'external' with { type: 'bar' };
    ^
 4: import 'external';
-5: import('external', { assert: { type: 'baz' } });`,
+5: import('external', { with: { type: 'baz' } });`,
 			id: ID_MAIN,
 			loc: {
 				column: 0,
@@ -24,17 +24,17 @@ module.exports = defineTest({
 			},
 			message:
 				'Module "main.js" tried to import "external" with "type": "bar" attributes, but it was already imported elsewhere with "type": "foo" attributes. Please ensure that import attributes for the same module are always consistent.',
-			pos: 63
+			pos: 61
 		},
 		{
 			code: 'INCONSISTENT_IMPORT_ATTRIBUTES',
 			frame: `
-2: import 'external' assert { type: 'foo' };
-3: import 'external' assert { type: 'bar' };
+2: import 'external' with { type: 'foo' };
+3: import 'external' with { type: 'bar' };
 4: import 'external';
    ^
-5: import('external', { assert: { type: 'baz' } });
-6: import './dep.js' assert { type: 'foo' };`,
+5: import('external', { with: { type: 'baz' } });
+6: import './dep.js' with { type: 'foo' };`,
 			id: ID_MAIN,
 			loc: {
 				column: 0,
@@ -43,7 +43,7 @@ module.exports = defineTest({
 			},
 			message:
 				'Module "main.js" tried to import "external" with no attributes, but it was already imported elsewhere with "type": "foo" attributes. Please ensure that import attributes for the same module are always consistent.',
-			pos: 105
+			pos: 101
 		},
 		{
 			code: 'INCONSISTENT_IMPORT_ATTRIBUTES',
