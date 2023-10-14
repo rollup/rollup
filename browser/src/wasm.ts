0c43e1f1bb43aa5165260f91baaa079e5479a917
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2023-10-15 01:39:08 +0900

    chore: fix lint

--- browser/src/wasm.ts
+++ browser/src/wasm.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line import/no-unresolved
 export { parse, xxhash_base64_url as xxhashBase64Url } from '../../wasm/bindings_wasm.js';
 
+// eslint-disable-next-line import/no-unresolved
 import { parse } from '../../wasm/bindings_wasm.js';
 export async function parseAsync(
 	code: string,
