9ad519c1430258fe423200ca720589e65ce7c2bc
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2023-10-15 14:32:29 +0900

    fix: set js_name for xxhash_base64_url function from wasm

--- browser/src/wasm.ts
+++ browser/src/wasm.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/no-unresolved
-export { parse, xxhash_base64_url as xxhashBase64Url } from '../../wasm/bindings_wasm.js';
+export { parse, xxhashBase64Url } from '../../wasm/bindings_wasm.js';
