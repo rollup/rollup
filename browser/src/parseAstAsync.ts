265836df547f4fb2a1dff308df1b2fcb058441aa
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2023-10-20 17:05:35 +0200

    Avoid race condition when emitting implicit entry points

--- browser/src/parseAstAsync.ts
+++ browser/src/parseAstAsync.ts
@@ -2,8 +2,7 @@ import type { AstNode, ParseAst } from '../../src/rollup/types';
 import { parseAst } from '../../src/utils/parseAst';
 
 // We could also do things in a web worker here, but that would make bundling
-// the
-// browser build much more complicated
+// the browser build much more complicated
 const parseAstAsync = async (
 	code: Parameters<ParseAst>[0],
 	options?: Parameters<ParseAst>[1]
