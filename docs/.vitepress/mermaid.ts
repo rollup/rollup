bdb03d0a8b7d1338056d6889c8d653606c4b271d
Lukas Taegert-Atkinson <lukastaegert@users.noreply.github.com>
2023-11-26 13:08:53 +0100

    Debug deployed graphs (#5272)
    * Add logging
    
    * Fix graph generation

--- docs/.vitepress/mermaid.ts
+++ docs/.vitepress/mermaid.ts
@@ -12,6 +12,7 @@ const graphsDirectory = new URL('graphs/', import.meta.url);
 
 const mermaidRegExp = /^```mermaid\n([\S\s]*?)\n```/gm;
 const greaterThanRegExp = /&gt;/g;
+const svgIdRegExp = /my-svg/g;
 const styleTagRegExp = /<style>[\S\s]*?<\/style>/gm;
 const configFileURL = new URL('mermaid.config.json', import.meta.url);
 const puppeteerConfigFileURL = new URL('puppeteer-config.json', import.meta.url);
@@ -22,7 +23,7 @@ export function renderMermaidGraphsPlugin(): Plugin {
 		.then(files => new Set(files.filter(name => name.endsWith('.svg'))));
 	const existingGraphsByName = new Map<string, Promise<string>>();
 
-	async function renderGraph(codeBlock: string, outFile: string) {
+	async function renderGraph(codeBlock: string, outFile: string, hash: string) {
 		const existingGraphFileNames = await existingGraphFileNamesPromise;
 		const outFileURL = new URL(outFile, graphsDirectory);
 		if (!existingGraphFileNames.has(outFile)) {
@@ -42,13 +43,23 @@ export function renderMermaidGraphsPlugin(): Plugin {
 		// Styles need to be placed top-level, so we extract them and then
 		// prepend them, separated with a line-break
 		const extractedStyles: string[] = [];
+		let hasReplacedId = false;
+		const replacementId = `mermaid-${hash}`;
 		const baseGraph = outFileContent
 			// We need to replace some HTML entities
 			.replace(greaterThanRegExp, '>')
+			// First, we replace the default id with a unique, hash-based one
+			.replace(svgIdRegExp, () => {
+				hasReplacedId = true;
+				return replacementId;
+			})
 			.replace(styleTagRegExp, styleTag => {
 				extractedStyles.push(styleTag);
 				return '';
 			});
+		if (!hasReplacedId) {
+			throw new Error('Could not find expected id "my-svg"');
+		}
 		console.log('Extracted styles from mermaid chart:', extractedStyles.length);
 		return `${extractedStyles.join('')}\n${baseGraph}`;
 	}
@@ -66,9 +77,10 @@ export function renderMermaidGraphsPlugin(): Plugin {
 				}
 				await Promise.all(
 					mermaidCodeBlocks.map(async (codeBlock, index) => {
-						const outFile = `${createHash('sha256').update(codeBlock).digest('base64url')}.svg`;
+						const hash = createHash('sha256').update(codeBlock).digest('hex').slice(0, 8);
+						const outFile = `mermaid-${hash}.svg`;
 						if (!existingGraphsByName.has(outFile)) {
-							existingGraphsByName.set(outFile, renderGraph(codeBlock, outFile));
+							existingGraphsByName.set(outFile, renderGraph(codeBlock, outFile, hash));
 						}
 						renderedGraphs[index] = await existingGraphsByName.get(outFile)!;
 					})
