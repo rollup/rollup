1a15d6ae7a4b2d206f0005ed4df1a7cad5dd5184
XiaoPi <530257315@qq.com>
2023-12-25 22:59:00 +0800

    Remove rollup-plugin-thatworks from devDeps

--- test/cli/samples/config-mjs-plugins/nested/plugin.mjs
+++ test/cli/samples/config-mjs-plugins/nested/plugin.mjs
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'url';
 
 export default () => ({
 	transform(code) {
-		return `console.log('${relative(process.cwd(), fileURLToPath(import.meta.url)).replace(
+		return `#!/usr/bin/env node\nconsole.log('${relative(process.cwd(), fileURLToPath(import.meta.url)).replace(
 			'\\',
 			'/'
 		)}');\n${code}`;
--- test/cli/samples/config-mjs-plugins/rollup.config.mjs
+++ test/cli/samples/config-mjs-plugins/rollup.config.mjs
@@ -1,10 +1,9 @@
 import replace from '@rollup/plugin-replace';
-import { shebang } from 'rollup-plugin-thatworks';
 import nestedPlugin from './nested/plugin.mjs';
 import plugin from './plugin.mjs';
 
 export default {
 	input: 'main.js',
 	output: { format: 'cjs', exports: 'auto' },
-	plugins: [shebang(), replace({ preventAssignment: true, ANSWER: 42 }), plugin(), nestedPlugin()]
+	plugins: [replace({ preventAssignment: true, ANSWER: 42 }), plugin(), nestedPlugin()]
 };
