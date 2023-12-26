cdb6204f06d1e66dc9465be883b7b00dcce4222a
龙腾道 <LongTengDao@LongTengDao.com>
2023-12-26 18:08:30 +0800

    handling __proto__ key

--- test/function/samples/handling-__proto__-key/main.js
+++ test/function/samples/handling-__proto__-key/main.js
@@ -1,4 +1,5 @@
 import * as module from './module.js';
 
-assert.equal(module.__proto__, 0);
-assert.equal({ ...module }.__proto__, 0);
+assert.equal(module.__proto__, 1);
+assert.equal(Object.freeze(module).__proto__, 1);
+assert.equal({ ...module }.__proto__, 1);
