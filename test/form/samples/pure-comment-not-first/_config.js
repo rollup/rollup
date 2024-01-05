4c19872d9794eef78dcb1cfb6e731a97790fef39
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2024-01-05 07:48:13 +0100

    Handles pure annotations that are separated by a comment

---
+++ test/form/samples/pure-comment-not-first/_config.js
@@ -0,0 +1,3 @@
+module.exports = defineTest({
+	description: 'recognizes pure comments when there are other comments in between'
+});
---
+++ test/form/samples/pure-comment-not-first/main.js
@@ -0,0 +1,2 @@
+const four = /* #__PURE__ */ /* foo */ add(2, 2);
+export const five = /* #__PURE__ */ /* foo */ add(2, 3);
