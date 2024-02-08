04b62876f3e22f687be4101e38471445a8b8ee7d
XiaoPi <530257315@qq.com>
2024-02-08 14:03:17 +0800

    Adapt error function in the Module class

--- test/function/samples/catch-rust-panic/_config.js
+++ test/function/samples/catch-rust-panic/_config.js
@@ -5,13 +5,13 @@ module.exports = defineTest({
 	description: 'Catch Rust panics and then throw them in Node',
 	error: {
 		cause: {
-			code: 'UNIMPLEMENTED_ERROR',
-			message: 'not implemented: Cannot convert Prop::Assign'
+			code: 'PARSE_ERROR',
+			message: 'not implemented: Cannot convert Prop::Assign',
+			pos: undefined
 		},
 		code: 'PARSE_ERROR',
 		id: MAIN_ID,
 		message: 'not implemented: Cannot convert Prop::Assign',
-		pos: undefined,
 		watchFiles: [MAIN_ID]
 	}
 });
