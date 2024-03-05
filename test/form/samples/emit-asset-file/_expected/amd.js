ce6cb93098850a46fa242e37b74a919e99a5de28
翠 / green <green@sapphi.red>
2024-03-05 14:40:36 +0900

    fix: escape ids in `import.meta.ROLLUP_FILE_URL_referenceId` correctly (#5404)

--- test/form/samples/emit-asset-file/_expected/amd.js
+++ test/form/samples/emit-asset-file/_expected/amd.js
@@ -2,6 +2,8 @@ define(['require'], (function (require) { 'use strict';
 
 	var logo = new URL(require.toUrl('./assets/logo-zDlmrXar.svg'), document.baseURI).href;
 
+	var logoReverse = new URL(require.toUrl('./assets/logo_reverse\'-DbGK2oiS.svg'), document.baseURI).href;
+
 	function showImage(url) {
 		console.log(url);
 		if (typeof document !== 'undefined') {
@@ -12,5 +14,6 @@ define(['require'], (function (require) { 'use strict';
 	}
 
 	showImage(logo);
+	showImage(logoReverse);
 
 }));
