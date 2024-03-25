1c91f75083ae4f1cfe143c183059d39a5ed05bd8
liuly <me@liuly.moe>
2024-03-25 20:44:12 +0800

    fix: test indent

--- test/form/samples/tree-shake-literal-parameter/top-level-export/_expected.js
+++ test/form/samples/tree-shake-literal-parameter/top-level-export/_expected.js
@@ -1,7 +1,7 @@
 function foo(x) {
-    // The exported function might also be called with "false"
-    if (x) console.log('true');
-    else console.log('false');
+	// The exported function might also be called with "false"
+	if (x) console.log('true');
+	else console.log('false');
 }
 
 foo(true);
