db7f1572f1bc3ddb07880bb1a22409857447a359
XiaoPi <530257315@qq.com>
2024-03-25 22:29:38 +0800

    feat: support using keyword

---
+++ test/form/samples/support-using-keyword/_expected.js
@@ -0,0 +1,15 @@
+const logClean = function () {
+	return {
+		[Symbol.dispose]() {
+			console.log('clean in sync');
+		},
+		[Symbol.asyncDispose]() {
+			console.log('clean in async');
+		}
+	};
+};
+
+async function foo() {
+	using a = logClean();	await using b = logClean();}
+
+foo();
