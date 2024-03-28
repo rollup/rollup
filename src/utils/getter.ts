fe57366b2d03c44c56761dd18ab1229684ebe948
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2024-03-28 06:48:29 +0100

    Remove safeguards as we are converting a hand-picked list

--- src/utils/getter.ts
+++ src/utils/getter.ts
@@ -3,16 +3,14 @@ export function cacheObjectGetters<T, K extends PropertyKey = keyof T>(
 	getterProperties: K[]
 ) {
 	for (const property of getterProperties) {
-		const propertyGetter = Object.getOwnPropertyDescriptor(object, property)?.get;
-		if (propertyGetter) {
-			Object.defineProperty(object, property, {
-				get: () => {
-					const value = propertyGetter.call(object);
-					// This replaces the getter with a fixed value for subsequent calls
-					Object.defineProperty(object, property, { value });
-					return value;
-				}
-			});
-		}
+		const propertyGetter = Object.getOwnPropertyDescriptor(object, property)!.get!;
+		Object.defineProperty(object, property, {
+			get() {
+				const value = propertyGetter.call(object);
+				// This replaces the getter with a fixed value for subsequent calls
+				Object.defineProperty(object, property, { value });
+				return value;
+			}
+		});
 	}
 }
