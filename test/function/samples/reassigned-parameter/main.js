4bbe1a3d349d631760a1a8f82c352be8c14dc355
liuly <me@liuly.moe>
2024-04-22 23:07:03 +0800

    fix: function parameter reassigned update (#5482)
    fix: reassigned

--- test/function/samples/reassigned-parameter/main.js
+++ test/function/samples/reassigned-parameter/main.js
@@ -1,7 +1,21 @@
 function f(a) {
-  assert.equal(a ? 'OK' : 'FAIL', 'OK');
-  a = false;
-  assert.equal(a ? 'FAIL' : 'OK', 'OK');
+	assert.equal(a ? 'OK' : 'FAIL', 'OK');
+	a = false;
+	assert.equal(a ? 'FAIL' : 'OK', 'OK');
 }
 
-f(true)
+f(true);
+
+function g(array) {
+	if (array === null) {
+		array = [];
+	}
+
+	if (array) {
+		return 'OK';
+	}
+	return array;
+}
+
+assert.equal(g(null), 'OK');
+
