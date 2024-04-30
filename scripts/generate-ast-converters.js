f8f166ca47652db234a3bbfdbe3bdd376045ba29
Lukas Taegert-Atkinson <lukastaegert@users.noreply.github.com>
2024-04-30 09:38:28 +0200

    Auto generate node index (#5504)
    * Auto-generate node index
    
    * Auto-generate node types

--- scripts/generate-ast-converters.js
+++ scripts/generate-ast-converters.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
-import './generate-rust-constants.js';
+import './generate-buffer-parsers.js';
 import './generate-buffer-to-ast.js';
 import './generate-child-node-keys.js';
-import './generate-buffer-parsers.js';
+import './generate-node-index.js';
+import './generate-node-types.js';
+import './generate-rust-constants.js';
 import './generate-string-constants.js';
