fc1fc331f1f003143dd70b5adfda2e01d4d29380
Alberto Diaz <alberto.diazdorado@tngtech.com>
2024-05-03 11:31:18 +0200

    Start implementing 'convert_ts_type_annotation'

--- rust/parse_ast/src/convert_ast/converter.rs
+++ rust/parse_ast/src/convert_ast/converter.rs
@@ -1,23 +1,7 @@
 use swc_atoms::JsWord;
 use swc_common::Span;
 use swc_ecma_ast::{
-  ArrayLit, ArrayPat, ArrowExpr, AssignExpr, AssignOp, AssignPat, AssignPatProp, AssignTarget,
-  AssignTargetPat, AwaitExpr, BigInt, BinExpr, BinaryOp, BindingIdent, BlockStmt, BlockStmtOrExpr,
-  Bool, BreakStmt, CallExpr, Callee, CatchClause, Class, ClassDecl, ClassExpr, ClassMember,
-  ClassMethod, ClassProp, ComputedPropName, CondExpr, Constructor, ContinueStmt, DebuggerStmt,
-  Decl, DefaultDecl, DoWhileStmt, EmptyStmt, ExportAll, ExportDecl, ExportDefaultDecl,
-  ExportDefaultExpr, ExportNamedSpecifier, ExportSpecifier, Expr, ExprOrSpread, ExprStmt, FnExpr,
-  ForHead, ForInStmt, ForOfStmt, ForStmt, Function, GetterProp, Ident, IfStmt, ImportDecl,
-  ImportDefaultSpecifier, ImportNamedSpecifier, ImportSpecifier, ImportStarAsSpecifier,
-  KeyValuePatProp, KeyValueProp, LabeledStmt, Lit, MemberExpr, MemberProp, MetaPropExpr,
-  MetaPropKind, MethodKind, MethodProp, ModuleDecl, ModuleExportName, ModuleItem, NamedExport,
-  NewExpr, Null, Number, ObjectLit, ObjectPat, ObjectPatProp, OptCall, OptChainBase, OptChainExpr,
-  ParamOrTsParamProp, ParenExpr, Pat, PrivateMethod, PrivateName, PrivateProp, Program, Prop,
-  PropName, PropOrSpread, Regex, RestPat, ReturnStmt, SeqExpr, SetterProp, SimpleAssignTarget,
-  SpreadElement, StaticBlock, Stmt, Str, Super, SuperProp, SuperPropExpr, SwitchCase, SwitchStmt,
-  TaggedTpl, ThisExpr, ThrowStmt, Tpl, TplElement, TryStmt, TsType, TsTypeAnn, UnaryExpr, UnaryOp,
-  UpdateExpr, UpdateOp, UsingDecl, VarDecl, VarDeclKind, VarDeclOrExpr, VarDeclarator, WhileStmt,
-  YieldExpr,
+  ArrayLit, ArrayPat, ArrowExpr, AssignExpr, AssignOp, AssignPat, AssignPatProp, AssignTarget, AssignTargetPat, AwaitExpr, BigInt, BinExpr, BinaryOp, BindingIdent, BlockStmt, BlockStmtOrExpr, Bool, BreakStmt, CallExpr, Callee, CatchClause, Class, ClassDecl, ClassExpr, ClassMember, ClassMethod, ClassProp, ComputedPropName, CondExpr, Constructor, ContinueStmt, DebuggerStmt, Decl, DefaultDecl, DoWhileStmt, EmptyStmt, ExportAll, ExportDecl, ExportDefaultDecl, ExportDefaultExpr, ExportNamedSpecifier, ExportSpecifier, Expr, ExprOrSpread, ExprStmt, FnExpr, ForHead, ForInStmt, ForOfStmt, ForStmt, Function, GetterProp, Ident, IfStmt, ImportDecl, ImportDefaultSpecifier, ImportNamedSpecifier, ImportSpecifier, ImportStarAsSpecifier, KeyValuePatProp, KeyValueProp, LabeledStmt, Lit, MemberExpr, MemberProp, MetaPropExpr, MetaPropKind, MethodKind, MethodProp, ModuleDecl, ModuleExportName, ModuleItem, NamedExport, NewExpr, Null, Number, ObjectLit, ObjectPat, ObjectPatProp, OptCall, OptChainBase, OptChainExpr, ParamOrTsParamProp, ParenExpr, Pat, PrivateMethod, PrivateName, PrivateProp, Program, Prop, PropName, PropOrSpread, Regex, RestPat, ReturnStmt, SeqExpr, SetterProp, SimpleAssignTarget, SpreadElement, StaticBlock, Stmt, Str, Super, SuperProp, SuperPropExpr, SwitchCase, SwitchStmt, TaggedTpl, ThisExpr, ThrowStmt, Tpl, TplElement, TryStmt, TsKeywordType, TsType, TsTypeAnn, UnaryExpr, UnaryOp, UpdateExpr, UpdateOp, UsingDecl, VarDecl, VarDeclKind, VarDeclOrExpr, VarDeclarator, WhileStmt, YieldExpr
 };
 
 use crate::convert_ast::annotations::{AnnotationKind, AnnotationWithType};
@@ -1033,10 +1017,10 @@ impl<'a> AstConverter<'a> {
     );
   }
 
-  fn convert_type_annotation(&self, type_annotation: &TsTypeAnn) {
+  fn convert_type_annotation(&mut self, type_annotation: &TsTypeAnn) {
     match &*type_annotation.type_ann {
-      TsType::TsKeywordType(_) => {
-        unimplemented!("TsKeywordType")
+      TsType::TsKeywordType(keyword_type) => {
+        self.convert_ts_keyword_type(keyword_type);
       }
       TsType::TsThisType(_) => {
         unimplemented!("TsThisType")
@@ -3086,6 +3070,17 @@ impl<'a> AstConverter<'a> {
     }
     // end
     self.add_end(end_position, &try_statement.span);
+  }
+
+  fn convert_ts_keyword_type(&mut self, keyword_type: &TsKeywordType) {
+    let end_position = self.add_type_and_start(
+      &TYPE_TS_TYPE_ANNOTATION,
+      &keyword_type.span,
+      TS_TYPE_ANNOTATION_RESERVED_BYTES,
+      false,
+    );
+    // end
+    self.add_end(end_position, &keyword_type.span);
   }
 
   fn convert_unary_expression(&mut self, unary_expression: &UnaryExpr) {
