740d2e48d769d9d7f2c063287ffcdb54c3306976
Burkhard Blobel <burkhard.blobel@tngtech.com>
2024-05-07 21:06:11 +0200

    Restructure convert_type_annotation: add type annotation info outside of match

--- rust/parse_ast/src/convert_ast/converter.rs
+++ rust/parse_ast/src/convert_ast/converter.rs
@@ -1049,19 +1049,18 @@ impl<'a> AstConverter<'a> {
   }
 
   fn convert_type_annotation(&mut self, type_annotation: &TsTypeAnn) {
+    let end_position = self.add_type_and_start(
+      &TYPE_TS_TYPE_ANNOTATION,
+      &type_annotation.span,
+      TS_TYPE_ANNOTATION_RESERVED_BYTES,
+      false,
+    );
+    
+    // typeAnnotation
+    self.update_reference_position(end_position + TS_TYPE_ANNOTATION_TYPE_ANNOTATION_OFFSET);
     match &*type_annotation.type_ann {
       TsType::TsKeywordType(keyword_type) => {
-        let end_position = self.add_type_and_start(
-          &TYPE_TS_TYPE_ANNOTATION,
-          &type_annotation.span,
-          TS_TYPE_ANNOTATION_RESERVED_BYTES,
-          false,
-        );
-        // typeAnnotation
-        self.update_reference_position(end_position + TS_TYPE_ANNOTATION_TYPE_ANNOTATION_OFFSET);
         self.convert_ts_keyword_type_kind(&keyword_type.kind, &keyword_type.span);
-        // end
-        self.add_end(end_position, &type_annotation.span);
       }
       TsType::TsThisType(_) => {
         unimplemented!("TsThisType")
@@ -1121,6 +1120,8 @@ impl<'a> AstConverter<'a> {
         unimplemented!("TsImportType")
       }
     }
+    // end
+    self.add_end(end_position, &type_annotation.span);
   }
 
   fn convert_ts_keyword_type_kind(&mut self, keyword_type_kind: &TsKeywordTypeKind, span: &Span) {
