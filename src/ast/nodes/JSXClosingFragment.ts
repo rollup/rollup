58a786b99035549e5b077b644ad9cb04e295c307
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2024-05-12 06:56:47 +0200

    Support fragments

--- src/ast/nodes/JSXClosingFragment.ts
+++ src/ast/nodes/JSXClosingFragment.ts
@@ -1,6 +1,15 @@
+import type MagicString from 'magic-string';
+import type { NormalizedJsxOptions } from '../../rollup/types';
 import type * as NodeType from './NodeType';
 import { NodeBase } from './shared/Node';
 
 export default class JSXClosingFragment extends NodeBase {
 	type!: NodeType.tJSXClosingFragment;
+
+	render(code: MagicString): void {
+		const { preserve } = this.scope.context.options.jsx as NormalizedJsxOptions;
+		if (!preserve) {
+			code.overwrite(this.start, this.end, ')', { contentOnly: true });
+		}
+	}
 }
