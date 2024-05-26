116dbca153e4735856b7e489d35d9ebb7902f526
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2024-05-26 07:11:23 +0200

    Improve handling for spread and rest elements

--- rust/parse_ast/src/ast_nodes/rest_element.rs
+++ rust/parse_ast/src/ast_nodes/rest_element.rs
@@ -7,15 +7,16 @@ use crate::convert_ast::converter::AstConverter;
 
 impl<'a> AstConverter<'a> {
   pub fn store_rest_element(&mut self, rest_pattern: &RestPat) {
-    let end_position = self.add_type_and_explicit_start(
+    let end_position = self.add_type_and_start(
       &TYPE_REST_ELEMENT,
-      rest_pattern.dot3_token.lo.0 - 1,
+      &rest_pattern.dot3_token,
       REST_ELEMENT_RESERVED_BYTES,
+      false,
     );
     // argument
     self.update_reference_position(end_position + REST_ELEMENT_ARGUMENT_OFFSET);
     self.convert_pattern(&rest_pattern.arg);
     // end
-    self.add_explicit_end(end_position, rest_pattern.span.hi.0 - 1);
+    self.add_end(end_position, &rest_pattern.span);
   }
 }
