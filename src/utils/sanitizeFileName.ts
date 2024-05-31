38113286fb17fbe9300efa6609019c9fc0d0c151
baseballyama <xydybaseball@gmail.com>
2024-05-31 16:37:23 +0900

    refactor: We recognize that decodeURIComponent exists in most of environment

--- src/utils/sanitizeFileName.ts
+++ src/utils/sanitizeFileName.ts
@@ -4,9 +4,6 @@ const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g;
 const DRIVE_LETTER_REGEX = /^[a-z]:/i;
 const PERCENT_REGEX = /%/g;
 
-const decodeURIComponentSafe =
-	typeof decodeURIComponent === 'undefined' ? undefined : decodeURIComponent;
-
 export function sanitizeFileName(name: string): string {
 	// A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
 	// Otherwise, avoid them because they can refer to NTFS alternate data streams.
@@ -14,15 +11,11 @@ export function sanitizeFileName(name: string): string {
 	const driveLetter = match ? match[0] : '';
 	let path = name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '_');
 
-	// Replace % with _ if there is invalid escape sequence
+	// Replace % with _ if % is used as an invalid escape sequence
 	if (path.includes('%')) {
-		if (decodeURIComponentSafe) {
-			try {
-				decodeURIComponentSafe(path);
-			} catch {
-				path = path.replace(PERCENT_REGEX, '_');
-			}
-		} else {
+		try {
+			decodeURIComponent(path);
+		} catch {
 			path = path.replace(PERCENT_REGEX, '_');
 		}
 	}
