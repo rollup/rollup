aa43dedf257096e9da343296bdb605030eabaa6f
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2024-11-20 09:01:53 +0100

    Only rely on stable property paths when deoptimizing member expressions
    Literal values may be deoptimized later. At that point, we would otherwise need
    to deoptimize again. For hasEffects, we can use dynamic literal values as those
    checks are repeatedly executed.
    
    Resolves #5735

--- test/form/samples/side-effects-prototype-assignments/main.js
+++ test/form/samples/side-effects-prototype-assignments/main.js
@@ -10,6 +10,5 @@ V6Engine.prototype.toString = function () { return 'V6'; };
 function IgnoredEngine () {}
 
 IgnoredEngine.prototype.toString = function () { return 'IGNORED'; };
-IgnoredEngine.prototype[ 'to' + 'String' ] = function () { return 'IGNORED'; };
 
 console.log( new V8Engine().toString() );
