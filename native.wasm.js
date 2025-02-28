03bb7e452d1ded7cce08dd765c864d9e8b377284
XiaoPi <530257315@qq.com>
2025-02-28 22:25:07 +0800

    Add jsx parameter to parseAsync in native.wasm.js

--- native.wasm.js
+++ native.wasm.js
@@ -6,8 +6,8 @@ const {
 } = require('./wasm-node/bindings_wasm.js');
 
 exports.parse = parse;
-exports.parseAsync = async (code, allowReturnOutsideFunction, _signal) =>
-	parse(code, allowReturnOutsideFunction);
+exports.parseAsync = async (code, allowReturnOutsideFunction, jsx, _signal) =>
+	parse(code, allowReturnOutsideFunction, jsx);
 exports.xxhashBase64Url = xxhashBase64Url;
 exports.xxhashBase36 = xxhashBase36;
 exports.xxhashBase16 = xxhashBase16;
