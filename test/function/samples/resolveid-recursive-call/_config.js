7d2c1e69279cdb79a3021931dd443c49115f9b6a
younggglcy <younggglcy@gmail.com>
2025-05-03 21:33:29 +0800

    test: add tests

---
+++ test/function/samples/resolveid-recursive-call/_config.js
@@ -0,0 +1,30 @@
+module.exports = defineTest({
+	description:
+		'skipSelf: true option in resolveId hook option should skip the plugin if it has been called before with the same id and importer, see #5768 for more details',
+	options: {
+		plugins: [
+			{
+				name: 'r1',
+				resolveId(id) {
+					console.log('r1', id);
+					const importer = 'foo';
+					return this.resolve(id, importer) ?? 'success';
+				},
+				load(id) {
+					if (id === 'success') {
+						return { code: 'export default 1' };
+					}
+				}
+			},
+			{
+				name: 'r2',
+				resolveId(id) {
+					console.log('r2', id);
+					const importer = 'bar';
+					return this.resolve(id, importer);
+				}
+			}
+		]
+	},
+	solo: true
+});
