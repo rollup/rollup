b38adfd1c39a37260e2f645277d73c6cacc83b7d
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2025-06-06 10:21:33 +0200

    Debug pipeline

--- test/cli/index.js
+++ test/cli/index.js
@@ -23,12 +23,13 @@ runTestSuiteWithSamples(
 	'cli',
 	path.resolve(__dirname, 'samples'),
 	/**
+	 * @param {string} directory
 	 * @param {import('../types').TestConfigCli} config
 	 */
 	(directory, config) => {
-		(config.skip ? it.skip : config.solo ? it.only : it)(
-			path.basename(directory) + ': ' + config.description,
-			async () => {
+		for (let run = 0; run < (config.repeat || 1); run++) {
+			const description = `${path.basename(directory)}: ${config.description}${run > 0 ? ` (run ${run + 1})` : ''}`;
+			(config.skip ? it.skip : config.solo ? it.only : it)(description, async () => {
 				process.chdir(config.cwd || directory);
 				try {
 					await runTest(config);
@@ -39,8 +40,8 @@ runTestSuiteWithSamples(
 					}
 					throw error;
 				}
-			}
-		).timeout(80_000);
+			}).timeout(80_000);
+		}
 	},
 	() => process.chdir(cwd)
 );
--- test/cli/samples/watch/watch-event-hooks-error/_config.js
+++ test/cli/samples/watch/watch-event-hooks-error/_config.js
@@ -1,6 +1,8 @@
 const { assertIncludes } = require('../../../../testHelpers.js');
 
 module.exports = defineTest({
+	solo: true,
+	repeat: 20,
 	description: 'onError event hook shell commands write to stderr',
 	spawnScript: 'wrapper.js',
 	spawnArgs: ['-cw', '--watch.onError', 'echo error'],
