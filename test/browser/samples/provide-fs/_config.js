63491d69ce0b37d00e95c4e9587c3c24ebfd8c3f
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2025-06-09 18:43:50 +0200

    Add tests for browser build

---
+++ test/browser/samples/provide-fs/_config.js
@@ -0,0 +1,18 @@
+const { Volume } = require('memfs');
+
+const vol = Volume.fromJSON({
+	main: "console.log('Hello, Rollup!');"
+});
+
+module.exports = defineTest({
+	description: 'allows to provide an in-memory fs via option',
+	options: {
+		fs: vol.promises,
+		plugins: {
+			name: 'test',
+			resolveId(source) {
+				return source;
+			}
+		}
+	}
+});
