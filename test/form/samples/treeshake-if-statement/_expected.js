0d4a4f7e6dbee223485dd1656a6cbb6b32cdec1c
ahabhgk <ahabhgk@gmail.com>
2025-07-01 13:03:00 +0800

    more tests

--- test/form/samples/treeshake-if-statement/_expected.js
+++ test/form/samples/treeshake-if-statement/_expected.js
@@ -6,4 +6,8 @@ if (typeof parseInt === 'function' ?? 'a' === 'a') {
 	assert.ok(true);
 }
 
-if (!(unknownGlobal ? "asdf" : true)) ;
+if (!(unknownGlobal ? 'asdf' : true)) ;
+
+if (unknownGlobal ? 0 : false) ;
+
+if (!(unknownGlobal ? 'a' === 'a' : true)) ;
--- test/form/samples/treeshake-if-statement/main.js
+++ test/form/samples/treeshake-if-statement/main.js
@@ -10,6 +10,14 @@ if (typeof parseInt === 'function' ?? 'a' === 'a') {
 	assert.ok(true);
 }
 
-if (!(unknownGlobal ? "asdf" : true)) {
-	assert.ok(false)
+if (!(unknownGlobal ? 'asdf' : true)) {
+	assert.ok(false);
+}
+
+if (unknownGlobal ? 0 : false) {
+	assert.ok(false);
+}
+
+if (!(unknownGlobal ? 'a' === 'a' : true)) {
+	assert.ok(false);
 }
