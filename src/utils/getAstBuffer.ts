bc295bcf11d5cfe6ab1e0a53ac316d0a0c9d3362
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2025-07-06 19:26:23 +0200

    Include string and list conversion

--- src/utils/getAstBuffer.ts
+++ src/utils/getAstBuffer.ts
@@ -1,21 +1,63 @@
-export type AstBuffer = Uint32Array & { convertString: (position: number) => string };
+export type AstBuffer = Uint32Array & { convertString: ConvertString };
+type ConvertString = (position: number) => string;
+
+export type AstBufferForWriting = Uint32Array & {
+	addStringToBuffer: AddStringToBuffer;
+	position: number;
+};
+
+type AddStringToBuffer = (text: string, referencePosition: number) => AstBufferForWriting;
 
 export function getAstBuffer(astBuffer: Buffer | Uint8Array): AstBuffer {
 	const array = new Uint32Array(astBuffer.buffer);
-	let convertString: (position: number) => string;
+	let convertString: ConvertString;
 	if (typeof Buffer !== 'undefined' && astBuffer instanceof Buffer) {
-		convertString = (position: number) => {
-			const length = array[position++];
-			const bytePosition = position << 2;
+		convertString = position => {
+			const length = array[position];
+			const bytePosition = (position + 1) << 2;
 			return astBuffer.toString('utf8', bytePosition, bytePosition + length);
 		};
 	} else {
 		const textDecoder = new TextDecoder();
-		convertString = (position: number) => {
-			const length = array[position++];
-			const bytePosition = position << 2;
+		convertString = position => {
+			const length = array[position];
+			const bytePosition = (position + 1) << 2;
 			return textDecoder.decode(astBuffer.subarray(bytePosition, bytePosition + length));
 		};
 	}
 	return Object.assign(array, { convertString });
+}
+
+export function createAstBuffer(uInt32Size: number): AstBufferForWriting {
+	const buffer = new Uint32Array(uInt32Size) as AstBufferForWriting;
+	let addStringToBuffer: AddStringToBuffer;
+	if (typeof Buffer !== 'undefined') {
+		const byteBuffer = Buffer.from(buffer.buffer);
+		addStringToBuffer = (text, referencePosition) => {
+			const insertPosition = buffer.position;
+			buffer[referencePosition] = insertPosition;
+			const textBytePosition = (insertPosition + 1) << 2;
+			const encodedLength = byteBuffer.write(text, textBytePosition);
+			buffer[insertPosition] = encodedLength;
+			// Round up to the next 4-byte boundary
+			buffer.position = (textBytePosition + encodedLength + 3) >> 2;
+			return buffer;
+		};
+	} else {
+		const encoder = new TextEncoder();
+		const byteArray = new Uint8Array(buffer.buffer);
+		addStringToBuffer = (text, referencePosition) => {
+			const insertPosition = buffer.position;
+			buffer[referencePosition] = insertPosition;
+			const textBytePosition = (insertPosition + 1) << 2;
+			const encoded = encoder.encode(text);
+			const encodedLength = encoded.length;
+			byteArray.set(encoded, textBytePosition);
+			buffer[insertPosition] = encodedLength;
+			// Round up to the next 4-byte boundary
+			buffer.position = (textBytePosition + encodedLength + 3) >> 2;
+			return buffer;
+		};
+	}
+	return Object.assign(buffer, { addStringToBuffer });
 }
--- src/utils/polymorphicNodesToBuffer.ts
+++ src/utils/polymorphicNodesToBuffer.ts
@@ -13,27 +13,27 @@ import {
 
 export const serializeExpressionStatement: NodeSerializer<
 	ast.ExpressionStatement | ast.Directive
-> = (node, buffer, position) => {
+> = (node, buffer) => {
 	return 'directive' in node
-		? serializeDirective(node, buffer, position)
-		: serializeExpressionStatementNode(node, buffer, position);
+		? serializeDirective(node, buffer)
+		: serializeExpressionStatementNode(node, buffer);
 };
 
-export const serializeLiteral: NodeSerializer<ast.Literal> = (node, buffer, position) => {
+export const serializeLiteral: NodeSerializer<ast.Literal> = (node, buffer) => {
 	switch (typeof node.value) {
 		case 'object':
 			if (node.value === null) {
-				return serializeLiteralNull(node, buffer, position);
+				return serializeLiteralNull(node, buffer);
 			}
-			return serializeLiteralRegExp(node as ast.LiteralRegExp, buffer, position);
+			return serializeLiteralRegExp(node as ast.LiteralRegExp, buffer);
 		case 'boolean':
-			return serializeLiteralBoolean(node as ast.LiteralBoolean, buffer, position);
+			return serializeLiteralBoolean(node as ast.LiteralBoolean, buffer);
 		case 'number':
-			return serializeLiteralNumber(node as ast.LiteralNumber, buffer, position);
+			return serializeLiteralNumber(node as ast.LiteralNumber, buffer);
 		case 'string':
-			return serializeLiteralString(node as ast.LiteralString, buffer, position);
+			return serializeLiteralString(node as ast.LiteralString, buffer);
 		case 'bigint':
-			return serializeLiteralBigInt(node as ast.LiteralBigInt, buffer, position);
+			return serializeLiteralBigInt(node as ast.LiteralBigInt, buffer);
 		default: {
 			/* istanbul ignore next */
 			throw new Error(`Unexpected node value type for Literal: ${typeof node.value}`);
