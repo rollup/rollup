5818e2780709a9afedc7b15de085a0b8fea1de93
Mohammad Bagher Abiyat <37929992+Aslemammad@users.noreply.github.com>
2025-07-14 10:01:19 +0330

    update some tests to reflect the reserved used names set

--- test/chunking-form/samples/preserve-modules-root/_expected/amd/below/module2.js
+++ test/chunking-form/samples/preserve-modules-root/_expected/amd/below/module2.js
@@ -1,17 +1,17 @@
 define(['exports', '../custom_modules/@my-scope/my-base-pkg/index'], (function (exports, index) { 'use strict';
 
-  var module;
+  var module$1;
   var hasRequiredModule;
 
   function requireModule () {
-  	if (hasRequiredModule) return module;
+  	if (hasRequiredModule) return module$1;
   	hasRequiredModule = 1;
   	const base2 = index.__require();
 
-  	module = {
+  	module$1 = {
   	  base2,
   	};
-  	return module;
+  	return module$1;
   }
 
   exports.__require = requireModule;
