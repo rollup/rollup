ffacdad42303d10158afc25bb013267189789ae1
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2025-07-14 06:28:13 +0200

    Deduplicate fixed strings

--- rust/parse_ast/src/ast_nodes/assignment_expression.rs
+++ rust/parse_ast/src/ast_nodes/assignment_expression.rs
@@ -1,10 +1,10 @@
 use swc_ecma_ast::{AssignExpr, AssignOp};
 
 use crate::convert_ast::converter::string_constants::{
-  STRING_ADDASSIGN, STRING_ANDASSIGN, STRING_ASSIGN, STRING_BITANDASSIGN, STRING_BITORASSIGN,
-  STRING_BITXORASSIGN, STRING_DIVASSIGN, STRING_EXPASSIGN, STRING_LSHIFTASSIGN, STRING_MODASSIGN,
-  STRING_MULASSIGN, STRING_NULLISHASSIGN, STRING_ORASSIGN, STRING_RSHIFTASSIGN, STRING_SUBASSIGN,
-  STRING_ZEROFILLRSHIFTASSIGN,
+  STRING_ANDASSIGN, STRING_ASSIGN, STRING_BITANDASSIGN, STRING_BITORASSIGN, STRING_BITXORASSIGN,
+  STRING_DIVIDEASSIGN, STRING_EXPONENTASSIGN, STRING_LSHIFTASSIGN, STRING_MINUSASSIGN,
+  STRING_MODULOASSIGN, STRING_MULTIPLYASSIGN, STRING_NULLISHASSIGN, STRING_ORASSIGN,
+  STRING_PLUSASSIGN, STRING_RSHIFTASSIGN, STRING_ZEROFILLRSHIFTASSIGN,
 };
 use crate::convert_ast::converter::AstConverter;
 use crate::store_assignment_expression;
@@ -16,18 +16,18 @@ impl AstConverter<'_> {
       span => assignment_expression.span,
       operator => match assignment_expression.op {
         AssignOp::Assign => &STRING_ASSIGN,
-        AssignOp::AddAssign => &STRING_ADDASSIGN,
-        AssignOp::SubAssign => &STRING_SUBASSIGN,
-        AssignOp::MulAssign => &STRING_MULASSIGN,
-        AssignOp::DivAssign => &STRING_DIVASSIGN,
-        AssignOp::ModAssign => &STRING_MODASSIGN,
+        AssignOp::AddAssign => &STRING_PLUSASSIGN,
+        AssignOp::SubAssign => &STRING_MINUSASSIGN,
+        AssignOp::MulAssign => &STRING_MULTIPLYASSIGN,
+        AssignOp::DivAssign => &STRING_DIVIDEASSIGN,
+        AssignOp::ModAssign => &STRING_MODULOASSIGN,
         AssignOp::LShiftAssign => &STRING_LSHIFTASSIGN,
         AssignOp::RShiftAssign => &STRING_RSHIFTASSIGN,
         AssignOp::ZeroFillRShiftAssign => &STRING_ZEROFILLRSHIFTASSIGN,
         AssignOp::BitOrAssign => &STRING_BITORASSIGN,
         AssignOp::BitXorAssign => &STRING_BITXORASSIGN,
         AssignOp::BitAndAssign => &STRING_BITANDASSIGN,
-        AssignOp::ExpAssign => &STRING_EXPASSIGN,
+        AssignOp::ExpAssign => &STRING_EXPONENTASSIGN,
         AssignOp::AndAssign => &STRING_ANDASSIGN,
         AssignOp::OrAssign => &STRING_ORASSIGN,
         AssignOp::NullishAssign => &STRING_NULLISHASSIGN,
