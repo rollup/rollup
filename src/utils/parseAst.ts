84a639f9d8a3f32413d9c437136acbf44fbb6edb
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2025-07-17 06:46:59 +0200

    Expose converter and serializer

--- src/utils/parseAst.ts
+++ src/utils/parseAst.ts
@@ -1,14 +1,30 @@
 import { parse, parseAsync } from '../../native';
-import type { ParseAst, ParseAstAsync } from '../rollup/types';
-import { convertProgram } from './bufferToAst';
+import type {
+	ast,
+	ConvertAstToBuffer,
+	ConvertBufferToAst,
+	ParseAst,
+	ParseAstAsync
+} from '../rollup/types';
+import { serializeAst } from './astToBuffer';
+import { convertAst } from './bufferToAst';
 import { getAstBuffer } from './getAstBuffer';
 
 export const parseAst: ParseAst = (
 	input,
 	{ allowReturnOutsideFunction = false, jsx = false } = {}
-) => convertProgram(getAstBuffer(parse(input, allowReturnOutsideFunction, jsx)));
+) => convertAst(0, getAstBuffer(parse(input, allowReturnOutsideFunction, jsx))) as ast.Program;
 
 export const parseAstAsync: ParseAstAsync = async (
 	input,
 	{ allowReturnOutsideFunction = false, jsx = false, signal } = {}
-) => convertProgram(getAstBuffer(await parseAsync(input, allowReturnOutsideFunction, jsx, signal)));
+) =>
+	convertAst(
+		0,
+		getAstBuffer(await parseAsync(input, allowReturnOutsideFunction, jsx, signal))
+	) as ast.Program;
+
+export const convertBufferToAst: ConvertBufferToAst = (buffer, position = 0) =>
+	convertAst(position, getAstBuffer(buffer));
+
+export const convertAstToBuffer: ConvertAstToBuffer = node => serializeAst(node) as Buffer;
