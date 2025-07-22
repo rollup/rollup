60441be2f8b7182354a5b9b63639bc2026b501af
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2025-07-22 06:43:24 +0200

    Auto-extend buffer

--- src/utils/getAstBuffer.ts
+++ src/utils/getAstBuffer.ts
@@ -1,13 +1,15 @@
 export type AstBuffer = Uint32Array & { convertString: ConvertString };
 type ConvertString = (position: number) => string;
 
-export type AstBufferForWriting = Uint32Array & {
-	addStringToBuffer: AddStringToBuffer;
-	position: number;
-	byteBuffer: Uint8Array | Buffer;
-};
+export type AstBufferForWriting<T = Uint8Array | Buffer> = Uint32Array & AstBufferForWritingApi<T>;
 
-type AddStringToBuffer = (text: string, referencePosition: number) => AstBufferForWriting;
+interface AstBufferForWritingApi<T = Uint8Array | Buffer> {
+	addStringToBuffer: (text: string, referencePosition: number) => AstBufferForWriting<T>;
+	byteBuffer: T;
+	position: number;
+	reserve: (size: number) => AstBufferForWriting<T>;
+	toOutput: () => T;
+}
 
 export function getAstBuffer(byteBuffer: Buffer | Uint8Array): AstBuffer {
 	const array = new Uint32Array(byteBuffer.buffer);
@@ -29,36 +31,83 @@ export function getAstBuffer(byteBuffer: Buffer | Uint8Array): AstBuffer {
 	return Object.assign(array, { convertString });
 }
 
-export function createAstBuffer(uInt32Size: number): AstBufferForWriting {
-	const buffer = new Uint32Array(uInt32Size) as AstBufferForWriting;
-	let addStringToBuffer: AddStringToBuffer;
-	if (typeof Buffer !== 'undefined') {
-		const byteBuffer = (buffer.byteBuffer = Buffer.from(buffer.buffer));
-		addStringToBuffer = (text, referencePosition) => {
+export function createAstBufferUint8(uInt32Size: number): AstBufferForWriting<Uint8Array> {
+	const buffer = new Uint32Array(uInt32Size) as AstBufferForWriting<Uint8Array>;
+	const encoder = new TextEncoder();
+	return Object.assign(buffer, {
+		addStringToBuffer(text, referencePosition) {
 			const insertPosition = buffer.position;
 			buffer[referencePosition] = insertPosition;
-			const textBytePosition = (insertPosition + 1) << 2;
-			const encodedLength = byteBuffer.write(text, textBytePosition);
-			buffer[insertPosition] = encodedLength;
-			// Round up to the next 4-byte boundary
-			buffer.position = (textBytePosition + encodedLength + 3) >> 2;
-			return buffer;
-		};
-	} else {
-		const encoder = new TextEncoder();
-		const byteBuffer = (buffer.byteBuffer = new Uint8Array(buffer.buffer));
-		addStringToBuffer = (text, referencePosition) => {
-			const insertPosition = buffer.position;
-			buffer[referencePosition] = insertPosition;
-			const textBytePosition = (insertPosition + 1) << 2;
 			const encoded = encoder.encode(text);
-			const encodedLength = encoded.length;
-			byteBuffer.set(encoded, textBytePosition);
-			buffer[insertPosition] = encodedLength;
+			const encodedByteLength = encoded.length;
+			// Round up to the next 4-byte boundary, add 1 for the length
+			const reservedSize = 1 + ((encodedByteLength + 3) >> 2);
+			const extendedBuffer = buffer.reserve(reservedSize);
+			extendedBuffer[insertPosition] = encodedByteLength;
+			extendedBuffer.byteBuffer.set(encoded, (insertPosition + 1) << 2);
+			return extendedBuffer;
+		},
+		byteBuffer: new Uint8Array(buffer.buffer),
+		position: 0,
+		reserve(size) {
+			const newPosition = buffer.position + size;
+			if (newPosition <= buffer.length) {
+				buffer.position = newPosition;
+				return buffer;
+			}
+			const newBuffer = createAstBufferUint8(newPosition * 2);
+			newBuffer.set(buffer);
+			newBuffer.position = newPosition;
+			return newBuffer;
+		},
+		toOutput() {
+			const byteSize = buffer.position << 2;
+			return buffer.byteBuffer.slice(0, byteSize);
+		}
+	} satisfies AstBufferForWritingApi<Uint8Array>);
+}
+
+export function createAstBufferNode(uInt32Size: number): AstBufferForWriting<Buffer> {
+	const buffer = new Uint32Array(uInt32Size) as AstBufferForWriting<Buffer>;
+	return Object.assign(buffer, {
+		addStringToBuffer(text, referencePosition) {
+			let extendedBuffer = buffer;
+			const requiredSize = buffer.position + 1 + ((text.length * 3 + 3) >> 2);
+			if (requiredSize > buffer.length) {
+				// Reserve more space if needed
+				extendedBuffer = createAstBufferNode(requiredSize * 2);
+				buffer.byteBuffer.copy(extendedBuffer.byteBuffer, 0, 0, buffer.position << 2);
+				extendedBuffer.position = buffer.position;
+			}
+			const insertPosition = extendedBuffer.position;
+			extendedBuffer[referencePosition] = insertPosition;
+			const textBytePosition = (insertPosition + 1) << 2;
+			const encodedLength = extendedBuffer.byteBuffer.write(text, textBytePosition);
+			extendedBuffer[insertPosition] = encodedLength;
 			// Round up to the next 4-byte boundary
-			buffer.position = (textBytePosition + encodedLength + 3) >> 2;
-			return buffer;
-		};
-	}
-	return Object.assign(buffer, { addStringToBuffer, position: 0 });
+			extendedBuffer.position = (textBytePosition + encodedLength + 3) >> 2;
+			return extendedBuffer;
+		},
+		byteBuffer: Buffer.from(buffer.buffer),
+		position: 0,
+		reserve(size) {
+			const newPosition = buffer.position + size;
+			if (newPosition <= buffer.length) {
+				buffer.position = newPosition;
+				return buffer;
+			}
+			const newBuffer = createAstBufferNode(newPosition * 2);
+			buffer.byteBuffer.copy(newBuffer.byteBuffer, 0, 0, buffer.position << 2);
+			newBuffer.position = newPosition;
+			return newBuffer;
+		},
+		toOutput() {
+			const byteSize = buffer.position << 2;
+			// The "slow" version will allocate exactly the needed size and is preferable
+			// for long-term memory usage.
+			const truncatedBuffer = Buffer.allocUnsafeSlow(byteSize);
+			buffer.byteBuffer.copy(truncatedBuffer, 0, 0, byteSize);
+			return truncatedBuffer;
+		}
+	} satisfies AstBufferForWritingApi<Buffer>);
 }
