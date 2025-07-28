ff916527bbb9f85f64651d0e3e5cce8c17f1cfe4
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2025-07-28 19:07:11 +0300

    [feat-enable-workspace-packages-in-ts-config-files] Add test case which simulates situation where rollup.config.ts imports package which looks like external, but is actually part of workspace. The test fails with expected error message now.

---
+++ test/cli/samples/config-ts-with-workspace-refs/packages/bundle/rollup.config.ts
@@ -0,0 +1,3 @@
+import * as workspaceReference from "@rollup-tests/referenced"; // Reference to workspace package in ../referenced directory.
+
+export default workspaceReference.MY_CUSTOM_OPTIONS;
---
+++ test/cli/samples/config-ts-with-workspace-refs/packages/bundle/src/bundlable-code.js
@@ -0,0 +1,1 @@
+console.log("Just some JS code");
