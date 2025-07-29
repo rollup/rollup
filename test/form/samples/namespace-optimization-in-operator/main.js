96e0158474b051ab607559efa4ac55ac3b8c2a51
TrickyPi <530257315@qq.com>
2025-07-29 22:26:09 +0800

    Improve coverage

--- test/form/samples/namespace-optimization-in-operator/main.js
+++ test/form/samples/namespace-optimization-in-operator/main.js
@@ -1,4 +1,5 @@
 import * as foo from './foo';
 
-if ('d' in foo) console.log(foo.d())
-if ('c' in foo) console.log(foo.c())
+if ('d' in foo) console.log(foo.d());
+if ('c' in foo) console.log(foo.c());
+console.log('c' in foo);
