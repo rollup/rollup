b98f75319ffb78756b0e702a8fedd06873f11eb9
Mohammad Bagher Abiyat <37929992+Aslemammad@users.noreply.github.com>
2025-08-05 18:37:44 +0330

    update some tests to respect reserved names and reserved used names

--- test/form/samples/protect-cjs-globals/_expected/amd.js
+++ test/form/samples/protect-cjs-globals/_expected/amd.js
@@ -1,15 +1,15 @@
 define(['exports'], (function (exports) { 'use strict';
 
 	const exports$1 = 1;
-	const require = 2;
-	const module = 3;
-	const __filename = 4;
-	const __dirname = 5;
+	const require$1 = 2;
+	const module$1 = 3;
+	const __filename$1 = 4;
+	const __dirname$1 = 5;
 
-	exports.__dirname = __dirname;
-	exports.__filename = __filename;
+	exports.__dirname = __dirname$1;
+	exports.__filename = __filename$1;
 	exports.exports = exports$1;
-	exports.module = module;
-	exports.require = require;
+	exports.module = module$1;
+	exports.require = require$1;
 
 }));
