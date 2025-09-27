70baa48aff1d21e3796da257a1ea8b51f8e29df2
TrickyPi <530257315@qq.com>
2025-09-27 13:12:42 +0800

    Replace no-effect awaiting import() expressions with 0

--- test/chunking-form/samples/dynamic-imports-without-side-effects/_expected/es/main.js
+++ test/chunking-form/samples/dynamic-imports-without-side-effects/_expected/es/main.js
@@ -1,7 +1,10 @@
 // removed
+// replace with await 0
+await 0;
+await 0;
 
 // retained
 import('./generated-sub2.js');
-await import('./generated-sub2.js');
 import('external2');
+await import('./generated-sub2.js');
 await import('external2');
--- test/chunking-form/samples/dynamic-imports-without-side-effects/_expected/system/main.js
+++ test/chunking-form/samples/dynamic-imports-without-side-effects/_expected/system/main.js
@@ -4,11 +4,14 @@ System.register([], (function (exports, module) {
 		execute: (async function () {
 
 			// removed
+			// replace with await 0
+			await 0;
+			await 0;
 
 			// retained
 			module.import('./generated-sub2.js');
-			await module.import('./generated-sub2.js');
 			module.import('external2');
+			await module.import('./generated-sub2.js');
 			await module.import('external2');
 
 		})
