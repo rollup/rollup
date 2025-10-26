b94040422d4db316e02e8795d11158ca47fff26c
Service account user <service_account_group_7248550_7944357a7f4ae38247fac3b716764774@noreply.gitlab.com>
2025-10-26 06:49:19 +0100

    Disable format shuffling for globals test

--- test/form/samples/deconflict-format-specific-globals/_config.js
+++ test/form/samples/deconflict-format-specific-globals/_config.js
@@ -1,5 +1,9 @@
 module.exports = defineTest({
 	description: 'deconflicts format specific globals',
+	// Due to deconflicted variable name caching, there will be more deconflicted
+	// names in "es" or "system" if they render after one of the other formats.
+	// This is fine, but we need to disable shuffling to keep the test stable.
+	shuffleFormats: false,
 	options: {
 		external: 'external',
 		output: {
