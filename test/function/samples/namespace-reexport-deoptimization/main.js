rollup/rollup

commit 15a6f73006c664963d5edac0139bb84b9a3a3067

Fix prop inclusion with try-catch-deoptimization (#5842)
* Test that argument inclusion works correctly when using try-catch-deopt

* Refine call argument inclusion

- When call arguments are included, do not call "include" on the "this" argument
  again, as this will already be included at the call site. This allows to make
  the inclusion logic more straightforward
- create shared helper to do that
- if a call/new/tagged template expression is called with recursive inclusion,
  do not specifically include the arguments. Instead, if an identifier is
  included recursively, then include an unknown path of the variable.

* Refine inclusion and deoptimization logic for reassigned member expressions

src/ast/nodes/MemberExpression.ts
@@ -44,6 +44,7 @@ import { getChainElementLiteralValueAtPath } from './shared/chainElements';
 import {
 	deoptimizeInteraction,
 	type ExpressionEntity,
+	includeInteraction,
 	type LiteralValueOrUnknown,
 	UNKNOWN_RETURN_EXPRESSION,
 	UnknownValue
@@ -375,6 +376,16 @@ export default class MemberExpression
 		}
 	}
 
+	includeNodeAsAssignmentTarget(context: InclusionContext) {
+		this.included = true;
+		if (!this.assignmentDeoptimized) this.applyAssignmentDeoptimization();
+		if (this.variable) {
+			this.scope.context.includeVariableInModule(this.variable, EMPTY_PATH, context);
+		} else if (!this.isUndefined) {
+			this.object.includePath([this.propertyKey], context);
+		}
+	}
+
 	includePath(path: ObjectPath, context: InclusionContext): void {
 		if (!this.included) this.includeNode(context);
 		if (this.variable) {
@@ -397,21 +408,17 @@ export default class MemberExpression
 		includeChildrenRecursively: IncludeChildren,
 		deoptimizeAccess: boolean
 	): void {
-		if (!this.assignmentDeoptimized) this.applyAssignmentDeoptimization();
-		if (deoptimizeAccess) {
-			this.include(context, includeChildrenRecursively);
-		} else {
-			if (!this.included) this.includeNode(context);
-			this.object.include(context, includeChildrenRecursively);
-			this.property.include(context, includeChildrenRecursively);
-		}
+		if (!this.included) this.includeNodeAsAssignmentTarget(context);
+		if (deoptimizeAccess && !this.deoptimized) this.applyDeoptimizations();
+		this.object.include(context, includeChildrenRecursively);
+		this.property.include(context, includeChildrenRecursively);
 	}
 
-	includeCallArguments(context: InclusionContext, interaction: NodeInteractionCalled): void {
+	includeCallArguments(interaction: NodeInteractionCalled, context: InclusionContext): void {
 		if (this.variable) {
-			this.variable.includeCallArguments(context, interaction);
+			this.variable.includeCallArguments(interaction, context);
 		} else {
-			super.includeCallArguments(context, interaction);
+			includeInteraction(interaction, context);
 		}
 	}
 

test/function/samples/object-expression-treeshaking/try-statement-return-properties/_config.js
@@ -1,0 +1,8 @@
+module.exports = defineTest({
+	description: 'includes properties of return statements in try statements',
+	options: {
+		treeshake: {
+			tryCatchDeoptimization: true
+		}
+	}
+});

test/function/samples/object-expression-treeshaking/try-statement-return-properties/main.js
@@ -1,0 +1,14 @@
+const options = {
+	bar: () => 'bar',
+	car: () => 'car'
+};
+
+assert.strictEqual(qux(options), 'car');
+
+function qux({ bar, car }) {
+	try {
+		return car();
+	} catch {
+		return undefined;
+	}
+}

src/ast/nodes/BreakStatement.ts
@@ -1,7 +1,12 @@
 import { type HasEffectsContext, type InclusionContext } from '../ExecutionContext';
 import type Identifier from './Identifier';
 import type * as NodeType from './NodeType';
-import { doNotDeoptimize, onlyIncludeSelfNoDeoptimize, StatementBase } from './shared/Node';
+import {
+	doNotDeoptimize,
+	type IncludeChildren,
+	onlyIncludeSelfNoDeoptimize,
+	StatementBase
+} from './shared/Node';
 
 export default class BreakStatement extends StatementBase {
 	declare label: Identifier | null;
@@ -19,10 +24,10 @@ export default class BreakStatement extends StatementBase {
 		return false;
 	}
 
-	include(context: InclusionContext): void {
+	include(context: InclusionContext, includeChildrenRecursively: IncludeChildren): void {
 		this.included = true;
 		if (this.label) {
-			this.label.include(context);
+			this.label.include(context, includeChildrenRecursively);
 			context.includedLabels.add(this.label.name);
 		} else {
 			context.hasBreak = true;

src/ast/nodes/CallExpression.ts
@@ -114,11 +114,7 @@ export default class CallExpression
 	include(context: InclusionContext, includeChildrenRecursively: IncludeChildren): void {
 		if (!this.included) this.includeNode(context);
 		if (includeChildrenRecursively) {
-			this.callee.include(context, true);
-			for (const argument of this.arguments) {
-				argument.includePath(UNKNOWN_PATH, context);
-				argument.include(context, true);
-			}
+			super.include(context, true);
 			if (
 				includeChildrenRecursively === INCLUDE_PARAMETERS &&
 				this.callee instanceof Identifier &&
@@ -127,16 +123,8 @@ export default class CallExpression
 				this.callee.variable.markCalledFromTryStatement();
 			}
 		} else {
-			// If the callee is a member expression and does not have a variable, its
-			// object will already be included via the first argument of the
-			// interaction in includeCallArguments. Including it again can lead to
-			// severe performance problems.
-			if (this.callee instanceof MemberExpression && !this.callee.variable) {
-				this.callee.property.include(context, false);
-			} else {
-				this.callee.include(context, false);
-			}
-			this.callee.includeCallArguments(context, this.interaction);
+			this.callee.include(context, false);
+			this.callee.includeCallArguments(this.interaction, context);
 		}
 	}
 

src/ast/nodes/ConditionalExpression.ts
@@ -164,13 +164,13 @@ export default class ConditionalExpression extends NodeBase implements Deoptimiz
 		}
 	}
 
-	includeCallArguments(context: InclusionContext, interaction: NodeInteractionCalled): void {
+	includeCallArguments(interaction: NodeInteractionCalled, context: InclusionContext): void {
 		const usedBranch = this.getUsedBranch();
 		if (usedBranch) {
-			usedBranch.includeCallArguments(context, interaction);
+			usedBranch.includeCallArguments(interaction, context);
 		} else {
-			this.consequent.includeCallArguments(context, interaction);
-			this.alternate.includeCallArguments(context, interaction);
+			this.consequent.includeCallArguments(interaction, context);
+			this.alternate.includeCallArguments(interaction, context);
 		}
 	}
 

src/ast/nodes/ContinueStatement.ts
@@ -1,7 +1,12 @@
 import { type HasEffectsContext, type InclusionContext } from '../ExecutionContext';
 import type Identifier from './Identifier';
 import type * as NodeType from './NodeType';
-import { doNotDeoptimize, onlyIncludeSelfNoDeoptimize, StatementBase } from './shared/Node';
+import {
+	doNotDeoptimize,
+	type IncludeChildren,
+	onlyIncludeSelfNoDeoptimize,
+	StatementBase
+} from './shared/Node';
 
 export default class ContinueStatement extends StatementBase {
 	declare label: Identifier | null;
@@ -19,10 +24,10 @@ export default class ContinueStatement extends StatementBase {
 		return false;
 	}
 
-	include(context: InclusionContext): void {
+	include(context: InclusionContext, includeChildrenRecursively: IncludeChildren): void {
 		this.included = true;
 		if (this.label) {
-			this.label.include(context);
+			this.label.include(context, includeChildrenRecursively);
 			context.includedLabels.add(this.label.name);
 		} else {
 			context.hasContinue = true;

src/ast/nodes/LabeledStatement.ts
@@ -44,7 +44,7 @@ export default class LabeledStatement extends StatementBase {
 		context.includedLabels = new Set<string>();
 		this.body.include(context, includeChildrenRecursively);
 		if (includeChildrenRecursively || context.includedLabels.has(this.label.name)) {
-			this.label.include(context);
+			this.label.include(context, includeChildrenRecursively);
 			context.includedLabels.delete(this.label.name);
 			context.brokenFlow = brokenFlow;
 		}

commit ece352eaadbbdf05ec7d4687a4e25eb84a743311

fix: include namespace variable paths during try-catch deoptimization
Fixes regression introduced in #5842 (v4.34.7).

Add a check in `MemberExpression.include` to call
`this.variable.includePath(UNKNOWN_PATH, context)` when
`includeChildrenRecursively` is true, ensuring namespace properties are
preserved.

This mirrors the change made to `IdentifierBase.include` in #5842, which
added the same check. `MemberExpression` was missing this handling for
its namespace variable.

src/ast/nodes/MemberExpression.ts
@@ -367,6 +367,9 @@ export default class MemberExpression
 		if (!this.included) this.includeNode(context);
 		this.object.include(context, includeChildrenRecursively);
 		this.property.include(context, includeChildrenRecursively);
+		if (includeChildrenRecursively) {
+			this.variable?.includePath(UNKNOWN_PATH, context);
+		}
 	}
 
 	includeNode(context: InclusionContext) {

test/function/samples/namespace-import-deoptimization/_config.js
@@ -1,0 +1,3 @@
+module.exports = defineTest({
+	description: 'includes namespace import properties accessed in try-catch callbacks'
+});

test/function/samples/namespace-import-deoptimization/dep.js
@@ -1,0 +1,1 @@
+export const foo = { bar: 'baz' };

test/function/samples/namespace-import-deoptimization/main.js
@@ -1,0 +1,13 @@
+import * as ns from './dep.js';
+
+function runInTryCatch(fn) {
+	try {
+		return fn();
+	} catch {}
+}
+
+function getBar(obj) {
+	return obj.bar;
+}
+
+assert.strictEqual(runInTryCatch(() => getBar(ns.foo)), 'baz');

test/function/samples/namespace-reexport-deoptimization/_config.js
@@ -1,0 +1,3 @@
+module.exports = defineTest({
+	description: 'includes namespace reexport properties accessed in try-catch callbacks'
+});

test/function/samples/namespace-reexport-deoptimization/dep.js
@@ -1,0 +1,1 @@
+export * as ns from './values.js';

test/function/samples/namespace-reexport-deoptimization/main.js
@@ -1,0 +1,13 @@
+import { ns } from './dep.js';
+
+function runInTryCatch(fn) {
+	try {
+		return fn();
+	} catch {}
+}
+
+function getBar(obj) {
+	return obj.bar;
+}
+
+assert.strictEqual(runInTryCatch(() => getBar(ns.foo)), 'baz');

test/function/samples/namespace-reexport-deoptimization/values.js
@@ -1,0 +1,1 @@
+export const foo = { bar: 'baz' };