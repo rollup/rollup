dc22f16b004de83e9ec9324a2c947aefd420bfc1
一笑 <530257315@qq.com>
2026-01-30 13:21:32 +0800

    [v5.0] Use "with" as the keyword of import attributes by default (#6248)

--- test/form/samples/import-attributes/attribute-shapes/_expected.js
+++ test/form/samples/import-attributes/attribute-shapes/_expected.js
@@ -1,4 +1,4 @@
-import('external-a', { assert: { type: 'json' } });
+import('external-a', { with: { type: 'json' } });
 import('external-b', { with: {} });
 import('external-c', { ...{} });
 import('external-d', {});
