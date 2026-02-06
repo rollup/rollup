aec703b73587950052501455dad4667f4cf1c10f
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2026-02-06 16:22:02 +0100

    Add nodeIds to JavaScript

--- scripts/generate-ast-converters.ts
+++ scripts/generate-ast-converters.ts
@@ -7,6 +7,7 @@ import './generate-buffer-parsers.js';
 import './generate-buffer-to-ast.js';
 import './generate-buffer-to-lazy-ast.js';
 import './generate-child-node-keys.js';
+import './generate-node-ids.js';
 import './generate-node-types.js';
 import './generate-node-unions.js';
 import './generate-rust-constants.js';
