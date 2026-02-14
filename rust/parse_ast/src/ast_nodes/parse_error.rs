957c67e25a21e2fcfa7d675e3df1c2c9d6c8db3b
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2026-02-14 16:00:39 +0100

    Make parseAndWalk throw PARSE_ERROR on invalid syntax
    This makes parseAndWalk behavior consistent with this.parse(), which also
    throws PARSE_ERROR when encountering invalid syntax.

--- rust/parse_ast/src/ast_nodes/parse_error.rs
+++ rust/parse_ast/src/ast_nodes/parse_error.rs
@@ -1,18 +1,17 @@
 use crate::convert_ast::converter::ast_constants::{
-  PARSE_ERROR_MESSAGE_OFFSET, PARSE_ERROR_RESERVED_BYTES, TYPE_PARSE_ERROR,
+    PARSE_ERROR_MESSAGE_OFFSET, PARSE_ERROR_RESERVED_BYTES, TYPE_PARSE_ERROR,
 };
-use crate::convert_ast::converter::update_reference_position;
+use crate::convert_ast::converter::{convert_string, update_reference_position};
 
-pub(crate) fn get_parse_error_buffer(error_buffer: &[u8], utf_16_pos: &u32) -> Vec<u8> {
+pub(crate) fn write_parse_error(buffer: &mut Vec<u8>, utf_16_pos: u32, message: &str) {
   // type
-  let mut buffer = TYPE_PARSE_ERROR.to_vec();
+  buffer.extend_from_slice(&TYPE_PARSE_ERROR);
   // start
   buffer.extend_from_slice(&utf_16_pos.to_ne_bytes());
   // end
   let end_position = buffer.len();
   buffer.resize(end_position + PARSE_ERROR_RESERVED_BYTES, 0);
-  // message, the string is already converted to a buffer via convert_string
-  update_reference_position(&mut buffer, end_position + PARSE_ERROR_MESSAGE_OFFSET);
-  buffer.extend_from_slice(&error_buffer[4..]);
-  buffer
+  // message
+  update_reference_position(buffer, end_position + PARSE_ERROR_MESSAGE_OFFSET);
+  convert_string(buffer, message);
 }
