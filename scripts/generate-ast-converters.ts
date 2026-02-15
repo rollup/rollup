19a4dbd9b28acb4f2f4fb4535219aebd4d0e188b
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2026-02-15 07:52:32 +0100

    Add test we do not forget to extend the parseAndWalk tests as needed
    Those tests cover both the parsing and the walking logic.

--- scripts/generate-ast-converters.ts
+++ scripts/generate-ast-converters.ts
@@ -13,3 +13,37 @@ import './generate-node-types.js';
 import './generate-node-unions.js';
 import './generate-rust-constants.js';
 import './generate-string-constants.js';
+
+// Check if we have sufficient test coverage if new nodes are added
+import { readdirSync } from 'node:fs';
+import { astNodeNamesWithFieldOrder } from './ast-types.js';
+
+const testDirFromRoot = 'test/function/samples/parse-and-walk/';
+const parseAndWalkTestDir = new URL(`../${testDirFromRoot}`, import.meta.url);
+const tests = new Set(readdirSync(parseAndWalkTestDir));
+
+const astNodeTypes = new Set(
+	astNodeNamesWithFieldOrder
+		.map(({ name }) => name)
+		.filter(name => name !== 'PanicError' && name !== 'ParseError')
+);
+
+const missingTests: string[] = [];
+for (const astType of astNodeTypes) {
+	if (!tests.has(`parse-${astType}`)) {
+		missingTests.push(astType);
+	}
+}
+
+if (missingTests.length > 0) {
+	console.error(
+		`❌ Missing parseAndWalk tests in "${testDirFromRoot}" for the following AST node types:`
+	);
+	for (const astType of missingTests) {
+		console.error(`   - ${astType}`);
+	}
+	console.error(
+		`\nPlease add test cases in test/function/samples/parse-and-walk/parse-<NodeType>/\n`
+	);
+	process.exit(1);
+}
