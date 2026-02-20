dd982e16056f785ddc816ad0ee8659513df3a07b
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2026-02-20 18:52:58 +0100

    Validate bundle stays within output dir
    When a file would leave the output dir, an error is thrown.

---
+++ test/function/samples/file-name-double-dot-prefix-is-valid/_config.js
@@ -0,0 +1,26 @@
+const assert = require('node:assert');
+
+module.exports = defineTest({
+	description: 'allows file names that start with ".." but are not path traversal sequences',
+	options: {
+		plugins: [
+			{
+				name: 'test',
+				generateBundle(_options, bundle) {
+					bundle['..foo.js'] = {
+						type: 'asset',
+						fileName: '..foo.js',
+						name: undefined,
+						needsCodeReference: false,
+						names: [],
+						originalFileNames: [],
+						source: 'content'
+					};
+				}
+			}
+		]
+	},
+	code(result) {
+		assert.ok(result !== null && result !== undefined, 'bundle was generated successfully');
+	}
+});
