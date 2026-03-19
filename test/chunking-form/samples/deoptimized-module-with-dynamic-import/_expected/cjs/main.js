444b03a05ba584d9b3651983a1d2d9f9bbd01c01
João Brilhante <joao.brilhante@outlook.com>
2026-03-19 01:16:54 +0100

    fix: fix chunk assignment for deoptimized module with dynamic import

---
+++ test/chunking-form/samples/deoptimized-module-with-dynamic-import/_expected/cjs/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const value = 42;
+
+console.log(value);
+
+var cjs = /*#__PURE__*/Object.freeze({
+	__proto__: null
+});
+
+exports.cjs = cjs;
---
+++ test/chunking-form/samples/deoptimized-module-with-dynamic-import/_expected/es/main.js
@@ -0,0 +1,9 @@
+const value = 42;
+
+console.log(value);
+
+var cjs = /*#__PURE__*/Object.freeze({
+	__proto__: null
+});
+
+export { cjs as c };
