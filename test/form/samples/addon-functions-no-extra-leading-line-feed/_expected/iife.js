799f62856188c05f3e49f2045af23c48c8c2c2a2
TrickyPi <530257315@qq.com>
2026-06-02 14:58:10 +0800

    Add a regression test

---
+++ test/form/samples/addon-functions-no-extra-leading-line-feed/_expected/iife.js
@@ -0,0 +1,18 @@
+/* banner-first iife.js */
+/* banner-second iife.js */
+(function () {
+	'use strict';
+
+	/* intro-first iife.js */
+
+	/* intro-second iife.js */
+
+	console.log('main');
+
+	/* outro-first iife.js */
+
+	/* outro-second iife.js */
+
+})();
+/* footer-first iife.js */
+/* footer-second iife.js */
