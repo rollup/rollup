e546f284646167bb70752acede566272b59432c7
一笑 <530257315@qq.com>
2026-06-03 15:09:31 +0800

    Tweaks the code
    Co-authored-by: Lukas Taegert-Atkinson <lukastaegert@users.noreply.github.com>

--- src/utils/addons.ts
+++ src/utils/addons.ts
@@ -9,9 +9,8 @@ export interface Addons {
 	outro: string;
 }
 
-const concatSeparator = (out: string, next: string) => (next ? `${out}${out && '\n'}${next}` : out);
-const concatDblSeparator = (out: string, next: string) =>
-	next ? `${out}${out && '\n\n'}${next}` : out;
+const concatSeparator = (out: string, next: string) => [out, next].filter(Boolean).join('\n');
+const concatDblSeparator = (out: string, next: string) => [out, next].filter(Boolean).join('\n\n');
 
 export async function createAddons(
 	options: NormalizedOutputOptions,
